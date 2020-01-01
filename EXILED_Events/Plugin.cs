82e02bb189140805e12f4066f7f96ee60eeabe64
galaxy119 <amathor929@gmail.com>
2020-01-01 14:23:31 -0800

    Fix harmony patching on reloads

--- EXILED_Events/Plugin.cs
+++ EXILED_Events/Plugin.cs
@@ -30,6 +30,7 @@ namespace EXILED
 		public static bool WarheadLockPatchDisable;
 		public static bool GrenadeThrownPatchDisable;
 		private EventHandlers handlers;
+		private int patchFixer = 0;
 		
 		public override void OnEnable()
 		{
@@ -42,7 +43,8 @@ namespace EXILED
 			Debug("Patching..");
 			try
 			{
-				instance = HarmonyInstance.Create("exiled.patches");
+				patchFixer++;
+				instance = HarmonyInstance.Create($"exiled.patches{patchFixer}");
 				instance.PatchAll();
 			}
 			catch (Exception e)
