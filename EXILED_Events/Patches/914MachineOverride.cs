e087e460d56548eb492c8a4e47b95d35beed6e5f
Kade <KadePcGames@gmail.com>
2020-01-19 18:37:24 -0800

    914UpgradeEvent

---
+++ EXILED_Events/Patches/914MachineOverride.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXILED.Patches
+{
+    [HarmonyPatch(typeof(Scp914.Scp914Machine), "ProcessItems")]
+    public class _914MachineOverride
+    {
+        public void Postfix(Scp914.Scp914Machine __instance)
+        {
+            if (EventPlugin.NineFourteenMachinePatchDisable)
+                return;
+            try
+            {
+                Events.InvokeSCP914Upgrade(__instance, __instance.players, __instance.items, __instance.knobState);
+            }
+            catch (Exception e)
+            {
+                Plugin.Error($"SCP914Upgrade Patch error: {e}");
+                return;
+            }
+        }
+    }
+}
