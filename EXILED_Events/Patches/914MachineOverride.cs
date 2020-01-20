03988c30db4659503d7ac2fc807d7c7215d3e424
galaxy119 <amathor929@gmail.com>
2020-01-20 09:27:56 -0800

    Fixed issue with 914 event

--- EXILED_Events/Patches/914MachineOverride.cs
+++ EXILED_Events/Patches/914MachineOverride.cs
@@ -15,45 +15,45 @@ namespace EXILED.Patches
     {
         public static bool Prefix(Scp914.Scp914Machine __instance)
         {
-            if (!NetworkServer.active)
-                return true;
-            Collider[] colliderArray = Physics.OverlapBox(__instance.intake.position, __instance.inputSize / 2f);
-            __instance.players.Clear();
-            __instance.items.Clear();
-            foreach (Collider collider in colliderArray)
-            {
-                CharacterClassManager component1 = collider.GetComponent<CharacterClassManager>();
-                if (component1 != null)
-                {
-                    __instance.players.Add(component1);
-                }
-                else
-                {
-                    Pickup component2 = collider.GetComponent<Pickup>();
-                    if (component2 != null)
-                        __instance.items.Add(component2);
-                }
-            }
-
-            List<Pickup> pickups = __instance.items;
-            bool allowUpgrade = true;
-            
-            Events.InvokeScp914Upgrade(__instance, __instance.players, ref pickups, __instance.knobState, ref allowUpgrade);
-            
-            __instance.items.Clear();
-            foreach (Pickup p in pickups)
-                __instance.items.Add(p);
             try
             {
-                __instance.MoveObjects(__instance.items, __instance.players);
-            }
-            finally
-            {
-                if (allowUpgrade)
-                    __instance.UpgradeObjects(__instance.items, __instance.players);
-            }
+                if (!NetworkServer.active)
+                    return true;
+                Collider[] colliderArray = Physics.OverlapBox(__instance.intake.position, __instance.inputSize / 2f);
+                __instance.players.Clear();
+                __instance.items.Clear();
+                foreach (Collider collider in colliderArray)
+                {
+                    CharacterClassManager component1 = collider.GetComponent<CharacterClassManager>();
+                    if (component1 != null)
+                    {
+                        __instance.players.Add(component1);
+                    }
+                    else
+                    {
+                        Pickup component2 = collider.GetComponent<Pickup>();
+                        if (component2 != null)
+                            __instance.items.Add(component2);
+                    }
+                }
 
-            return false;
+                List<Pickup> pickups = __instance.items;
+                bool allowUpgrade = true;
+
+                Events.InvokeScp914Upgrade(__instance, __instance.players, ref pickups, __instance.knobState,
+                    ref allowUpgrade);
+                
+                __instance.MoveObjects(pickups, __instance.players);
+                if (allowUpgrade)
+                    __instance.UpgradeObjects(pickups, __instance.players);
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Plugin.Error(e.ToString());
+                return true;
+            }
         }
     }
 }
