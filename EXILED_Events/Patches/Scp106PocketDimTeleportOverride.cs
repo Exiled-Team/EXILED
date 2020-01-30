dceee8816bedf27dc0f643ddab4100d066b32ade
galaxy119 <amathor929@gmail.com>
2020-01-29 16:51:56 -0800

    Idk what was broken, but I fixed it

--- EXILED_Events/Patches/Scp106PocketDimTeleportOverride.cs
+++ EXILED_Events/Patches/Scp106PocketDimTeleportOverride.cs
@@ -3,63 +3,79 @@ using Harmony;
 using Mirror;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 namespace EXILED.Patches
 {
-    [HarmonyPatch(typeof(PocketDimensionTeleport), "OnTriggerEnter")]
+    [HarmonyPatch(typeof(PocketDimensionTeleport), nameof(PocketDimensionTeleport.OnTriggerEnter))]
     public class Scp106PocketDimTeleportOverride
     {
-        public static bool Prefix(PocketDimensionTeleport __instance, Collider other)
+      public static bool Prefix(PocketDimensionTeleport __instance, Collider other)
+      {
+        if (!NetworkServer.active)
+          return false;
+        NetworkIdentity component1 = other.GetComponent<NetworkIdentity>();
+        if (!(component1 != null))
+          return false;
+        if (__instance.type == PocketDimensionTeleport.PDTeleportType.Killer ||
+            Object.FindObjectOfType<BlastDoor>().isClosed)
+          component1.GetComponent<PlayerStats>()
+            .HurtPlayer(new PlayerStats.HitInfo(999990f, "WORLD", DamageTypes.Pocket, 0), other.gameObject);
+        else if (__instance.type == PocketDimensionTeleport.PDTeleportType.Exit)
         {
-			NetworkIdentity component = other.GetComponent<NetworkIdentity>();
-			if (component != null)
-			{
-				if (__instance.type == PocketDimensionTeleport.PDTeleportType.Killer || UnityEngine.Object.FindObjectOfType<BlastDoor>().isClosed)
-				{
-					component.GetComponent<PlayerStats>().HurtPlayer(new PlayerStats.HitInfo(999990f, "WORLD", DamageTypes.Pocket, 0), other.gameObject);
-				}
-				
-				else if (__instance.type == PocketDimensionTeleport.PDTeleportType.Exit)
-				{
-					__instance.tpPositions.Clear();
-					List<string> stringList = ConfigFile.ServerConfig.GetStringList((GameObject.Find("Host").GetComponent<DecontaminationLCZ>().GetCurAnnouncement() > 5) ? "pd_random_exit_rids_after_decontamination" : "pd_random_exit_rids");
-					if (stringList.Count > 0)
-					{
-						foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("RoomID"))
-							if (gameObject.GetComponent<Rid>() != null && stringList.Contains(gameObject.GetComponent<Rid>().id))
-								__instance.tpPositions.Add(gameObject.transform.position);
-						if (stringList.Contains("PORTAL"))
-							foreach (Scp106PlayerScript scp106PlayerScript in UnityEngine.Object.FindObjectsOfType<Scp106PlayerScript>())
-								if (scp106PlayerScript.portalPosition != Vector3.zero)
-									__instance.tpPositions.Add(scp106PlayerScript.portalPosition);
-					}
-					if (__instance.tpPositions == null || __instance.tpPositions.Count == 0)
-						foreach (GameObject gameObject2 in GameObject.FindGameObjectsWithTag("PD_EXIT"))
-							__instance.tpPositions.Add(gameObject2.transform.position);
+          __instance.tpPositions.Clear();
+          List<string> stringList = ConfigFile.ServerConfig.GetStringList(
+            GameObject.Find("Host").GetComponent<DecontaminationLCZ>().GetCurAnnouncement() > 5
+              ? "pd_random_exit_rids_after_decontamination"
+              : "pd_random_exit_rids");
+          if (stringList.Count > 0)
+          {
+            foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("RoomID"))
+            {
+              if (gameObject.GetComponent<Rid>() != null && stringList.Contains(gameObject.GetComponent<Rid>().id))
+                __instance.tpPositions.Add(gameObject.transform.position);
+            }
 
-					bool allowEscape = true;
+            if (stringList.Contains("PORTAL"))
+            {
+              foreach (Scp106PlayerScript scp106PlayerScript in Object.FindObjectsOfType<Scp106PlayerScript>())
+              {
+                if (scp106PlayerScript.portalPosition != Vector3.zero)
+                  __instance.tpPositions.Add(scp106PlayerScript.portalPosition);
+              }
+            }
+          }
 
-					Events.InvokePocketDimEscaped(__instance.gameObject, ref allowEscape);
+          if (__instance.tpPositions == null || __instance.tpPositions.Count == 0)
+          {
+            foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("PD_EXIT"))
+              __instance.tpPositions.Add(gameObject.transform.position);
+          }
 
-					if (!allowEscape)
-						return false;
+          Vector3 tpPosition = __instance.tpPositions[Random.Range(0, __instance.tpPositions.Count)];
+          tpPosition.y += 2f;
+          PlyMovementSync component2 = other.GetComponent<PlyMovementSync>();
+          component2.SetSafeTime(2f);
 
-					Vector3 pos = __instance.tpPositions[UnityEngine.Random.Range(0, __instance.tpPositions.Count)];
-					pos.y += 2f;
-					PlyMovementSync component2 = other.GetComponent<PlyMovementSync>();
-					component2.SetSafeTime(2f);
-					component2.OverridePosition(pos, 0f, false);
-					__instance.RemoveCorrosionEffect(other.gameObject);
-					PlayerManager.localPlayer.GetComponent<PlayerStats>().TargetAchieve(component.connectionToClient, "larryisyourfriend");
-				}
-				if (__instance.RefreshExit)
-					ImageGenerator.pocketDimensionGenerator.GenerateRandom();
-			}
-			return false;
-		}
+          bool allowEscape = true;
+          Events.InvokePocketDimEscaped(component2.gameObject, ref allowEscape);
+
+          if (allowEscape)
+          {
+            component2.OverridePosition(tpPosition, 0.0f, false);
+            __instance.RemoveCorrosionEffect(other.gameObject);
+            PlayerManager.localPlayer.GetComponent<PlayerStats>()
+              .TargetAchieve(component1.connectionToClient, "larryisyourfriend");
+          }
+        }
+
+        if (!__instance.RefreshExit)
+          return false;
+        ImageGenerator.pocketDimensionGenerator.GenerateRandom();
+        return false;
+      }
     }
 }
--- EXILED_Events/Patches/WeaponManagerOverride.cs
+++ EXILED_Events/Patches/WeaponManagerOverride.cs
@@ -12,149 +12,160 @@ namespace EXILED.Patches
     [HarmonyPatch(typeof(WeaponManager), "CallCmdShoot")]
     public class WeaponManagerOverride
     {
-        public static bool Prefix(WeaponManager __instance, GameObject target, string hitboxType, Vector3 dir, Vector3 sourcePos, Vector3 targetPos)
+      public static bool Prefix(WeaponManager __instance, GameObject target, string hitboxType, Vector3 dir,
+        Vector3 sourcePos, Vector3 targetPos)
+      {
+        try
         {
-            // Nw spahgetti code
-            if (__instance._iawRateLimit.CanExecute(true))
-                return false;
-            ReferenceHub hub = __instance.hub;
-            int itemIndex = __instance.hub.inventory.GetItemIndex();
-            if (itemIndex < 0 || itemIndex >= __instance.hub.inventory.items.Count)
-	            return false;;
-            if (__instance.curWeapon < 0 || ((__instance.reloadCooldown > 0f || __instance.fireCooldown > 0f) && !__instance.isLocalPlayer || hub.inventory.curItem != __instance.weapons[__instance.curWeapon].inventoryID || hub.inventory.items[itemIndex].durability <= 0f))
-	            return false;;
-            if (Vector3.Distance(__instance.camera.transform.position, sourcePos) > 6.5f)
-            {
-                __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-                return false;;
-            }
-            hub.inventory.items.ModifyDuration(itemIndex, hub.inventory.items[itemIndex].durability - 1f);
+          if (!__instance._iawRateLimit.CanExecute(true))
+            return false;
+          int itemIndex = __instance.hub.inventory.GetItemIndex();
+          if (itemIndex < 0 || itemIndex >= __instance.hub.inventory.items.Count || __instance.curWeapon < 0 ||
+              (__instance.reloadCooldown > 0.0 || __instance.fireCooldown > 0.0) && !__instance.isLocalPlayer ||
+              (__instance.hub.inventory.curItem != __instance.weapons[__instance.curWeapon].inventoryID ||
+               __instance.hub.inventory.items[itemIndex].durability <= 0.0))
+            return false;
+          if (Vector3.Distance(__instance.camera.transform.position, sourcePos) > 6.5)
+          {
+            __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient,
+              "Shot rejected - Code 2.2 (difference between real source position and provided source position is too big)",
+              "gray");
+          }
+          else
+          {
+            __instance.hub.inventory.items.ModifyDuration(itemIndex,
+              __instance.hub.inventory.items[itemIndex].durability - 1f);
             __instance.scp268.ServerDisable();
-            __instance.fireCooldown = 1f / (__instance.weapons[__instance.curWeapon].shotsPerSecond * __instance.weapons[__instance.curWeapon].allEffects.firerateMultiplier) * 0.8f;
-            float num = __instance.weapons[__instance.curWeapon].allEffects.audioSourceRangeScale;
-            num = num * num * 70f;
-            __instance.GetComponent<Scp939_VisionController>().MakeNoise(Mathf.Clamp(num, 5f, 100f));
-            bool _f = target != null;
-            RaycastHit raycastHit2;
+            __instance.fireCooldown =
+              (float) (1.0 / (__instance.weapons[__instance.curWeapon].shotsPerSecond *
+                              (double) __instance.weapons[__instance.curWeapon].allEffects.firerateMultiplier) *
+                       0.800000011920929);
+            float sourceRangeScale = __instance.weapons[__instance.curWeapon].allEffects.audioSourceRangeScale;
+            __instance.GetComponent<Scp939_VisionController>()
+              .MakeNoise(Mathf.Clamp((float) (sourceRangeScale * (double) sourceRangeScale * 70.0), 5f, 100f));
+            bool flag = target != null;
             if (targetPos == Vector3.zero)
             {
-                RaycastHit raycastHit;
-                if (Physics.Raycast(sourcePos, dir, out raycastHit, 500f, __instance.raycastMask))
+              RaycastHit raycastHit;
+              if (Physics.Raycast(sourcePos, dir, out raycastHit, 500f, (int) __instance.raycastMask))
+              {
+                HitboxIdentity component = raycastHit.collider.GetComponent<HitboxIdentity>();
+                if (component != null)
                 {
-                    HitboxIdentity component = raycastHit.collider.GetComponent<HitboxIdentity>();
-                    if (component != null)
-                    {
-                        WeaponManager componentInParent = component.GetComponentInParent<WeaponManager>();
-                        if (componentInParent != null)
-                        {
-                            _f = false;
-                            target = componentInParent.gameObject;
-                            hitboxType = component.id;
-                            targetPos = componentInParent.transform.position;
-                        }
-                    }
+                  WeaponManager componentInParent = component.GetComponentInParent<WeaponManager>();
+                  if (componentInParent != null)
+                  {
+                    flag = false;
+                    target = componentInParent.gameObject;
+                    hitboxType = component.id;
+                    targetPos = componentInParent.transform.position;
+                  }
                 }
+              }
             }
-            else if (Physics.Linecast(sourcePos, targetPos, out raycastHit2, __instance.raycastMask))
+            else
             {
-                HitboxIdentity component2 = raycastHit2.collider.GetComponent<HitboxIdentity>();
-                if (component2 != null)
+              RaycastHit raycastHit;
+              if (Physics.Linecast(sourcePos, targetPos, out raycastHit, (int) __instance.raycastMask))
+              {
+                HitboxIdentity component = raycastHit.collider.GetComponent<HitboxIdentity>();
+                if (component != null)
                 {
-                    WeaponManager componentInParent2 = component2.GetComponentInParent<WeaponManager>();
-                    if (componentInParent2 != null)
+                  WeaponManager componentInParent = component.GetComponentInParent<WeaponManager>();
+                  if (componentInParent != null)
+                  {
+                    if (componentInParent.gameObject == target)
+                      flag = false;
+                    else if (componentInParent.scp268.Enabled)
                     {
-                        if (componentInParent2.gameObject == target)
-                            _f = false;
-                        else if (componentInParent2.scp268.Enabled)
-                        {
-                            _f = false;
-                            target = componentInParent2.gameObject;
-                            hitboxType = component2.id;
-                            targetPos = componentInParent2.transform.position;
-                        }
+                      flag = false;
+                      target = componentInParent.gameObject;
+                      hitboxType = component.id;
+                      targetPos = componentInParent.transform.position;
                     }
+                  }
                 }
+              }
             }
-			CharacterClassManager characterClassManager = null;
-			if (target != null)
-				characterClassManager = target.GetComponent<CharacterClassManager>();
-			if (characterClassManager != null && __instance.GetShootPermission(characterClassManager, false))
-			{
-				if (Math.Abs(__instance.camera.transform.position.y - characterClassManager.transform.position.y) > 40f)
-				{
-					__instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-					return false;;
-				}
-				if (Vector3.Distance(characterClassManager.transform.position, targetPos) > 6.5f)
-				{
-					__instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-					return false;;
-				}
-				if (Physics.Linecast(__instance.camera.transform.position, sourcePos, __instance.raycastServerMask))
-				{
-					__instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-					return false;;
-				}
-				if (_f && Physics.Linecast(sourcePos, targetPos, __instance.raycastServerMask))
-				{
-					__instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-					return false;;
-				}
-				if (characterClassManager.gameObject == __instance.gameObject)
-				{
-					__instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient, "Shot rejected", "red");
-					return false;;
-				}
-				float num2 = Vector3.Distance(__instance.camera.transform.position, target.transform.position);
-				float num3 = __instance.weapons[__instance.curWeapon].damageOverDistance.Evaluate(num2);
-				string a = hitboxType.ToUpper();
-				switch(a)
-				{
-					case "SCP106":
-						num3 /= 10f;
-						break;
-					case "LEG":
-						num3 /= 2f;
-						break;
-					default:
-						num3 *= 4f;
-						break;
-				}
-				num3 *= __instance.weapons[__instance.curWeapon].allEffects.damageMultiplier;
-				num3 *= __instance.overallDamagerFactor;
-				bool allow = true;
 
-				Events.InvokeOnShoot(hub, target, num3, num2, ref allow);
+            CharacterClassManager c = null;
+            if (target != null)
+              c = target.GetComponent<CharacterClassManager>();
+            if (c != null && __instance.GetShootPermission(c, false))
+            {
+              if (Math.Abs(__instance.camera.transform.position.y - c.transform.position.y) > 40.0)
+                __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient,
+                  "Shot rejected - Code 2.1 (too big Y-axis difference between source and target)", "gray");
+              else if (Vector3.Distance(c.transform.position, targetPos) > 6.5)
+                __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient,
+                  "Shot rejected - Code 2.3 (difference between real target position and provided target position is too big)",
+                  "gray");
+              else if (Physics.Linecast(__instance.camera.transform.position, sourcePos, __instance.raycastServerMask))
+                __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient,
+                  "Shot rejected - Code 2.4 (collision between source positions detected)", "gray");
+              else if (flag && Physics.Linecast(sourcePos, targetPos, __instance.raycastServerMask))
+              {
+                __instance.GetComponent<CharacterClassManager>().TargetConsolePrint(__instance.connectionToClient,
+                  "Shot rejected - Code 2.5 (collision on shot line detected)", "gray");
+              }
+              else
+              {
+                float num1 = Vector3.Distance(__instance.camera.transform.position, target.transform.position);
+                float num2 = __instance.weapons[__instance.curWeapon].damageOverDistance.Evaluate(num1);
+                string upper = hitboxType.ToUpper();
+                if (upper != "HEAD")
+                {
+                  if (upper != "LEG")
+                  {
+                    if (upper == "SCP106")
+                      num2 /= 10f;
+                  }
+                  else
+                    num2 /= 2f;
+                }
+                else
+                  num2 *= 4f;
 
-				if (allow)
-				{
-					__instance.hub.playerStats.HurtPlayer(new PlayerStats.HitInfo(num3, __instance.hub.nicknameSync.MyNick + " (" + __instance.hub.characterClassManager.UserId + ")", DamageTypes.FromWeaponId(__instance.curWeapon), __instance.hub.queryProcessor.PlayerId), characterClassManager.gameObject);
-					__instance.RpcConfirmShot(true, __instance.curWeapon);
-					__instance.PlaceDecal(true, new Ray(__instance.camera.position, dir), (int)characterClassManager.CurClass, num2);
-				}
-			}
-			else
-			{
-				// Game object stuff
-				bool Allow = true;
+                bool allow = true;
+                Events.InvokeOnShoot(__instance.hub, target, num2, num1, ref allow);
+                if (!allow)
+                  return false;
 
-				Events.InvokeOnShoot(hub, target, 0, Vector3.Distance(__instance.camera.transform.position, target.transform.position), ref Allow);
+                __instance.hub.playerStats.HurtPlayer(
+                  new PlayerStats.HitInfo(
+                    num2 * __instance.weapons[__instance.curWeapon].allEffects.damageMultiplier *
+                    __instance.overallDamagerFactor,
+                    __instance.hub.nicknameSync.MyNick + " (" + __instance.hub.characterClassManager.UserId + ")",
+                    DamageTypes.FromWeaponId(__instance.curWeapon), __instance.hub.queryProcessor.PlayerId),
+                  c.gameObject);
+                __instance.RpcConfirmShot(true, __instance.curWeapon);
+                __instance.PlaceDecal(true, new Ray(__instance.camera.position, dir), (int) c.CurClass, num1);
+              }
+            }
+            else if (target != null && hitboxType == "window" && target.GetComponent<BreakableWindow>() != null)
+            {
+              float damage = __instance.weapons[__instance.curWeapon].damageOverDistance
+                .Evaluate(Vector3.Distance(__instance.camera.transform.position, target.transform.position));
+              target.GetComponent<BreakableWindow>().ServerDamageWindow(damage);
+              __instance.RpcConfirmShot(true, __instance.curWeapon);
+            }
+            else
+            {
+              __instance.PlaceDecal(false, new Ray(__instance.camera.position, dir), __instance.curWeapon, 0.0f);
+              __instance.RpcConfirmShot(false, __instance.curWeapon);
+            }
+          }
 
-				if (Allow)
-				{
-					if (target != null && hitboxType == "window" && target.GetComponent<BreakableWindow>() != null)
-					{
-						BreakWindow(__instance, Vector3.Distance(__instance.camera.transform.position, target.transform.position), target.GetComponent<BreakableWindow>());
-						return false;;
-					}
-					__instance.PlaceDecal(false, new Ray(__instance.camera.position, dir), __instance.curWeapon, 0f);
-					__instance.RpcConfirmShot(false, __instance.curWeapon);
-				}
-			}
-			return false;
-		}
+          return false;
+        }
+        catch (Exception e)
+        {
+          Plugin.Error($"OnShoot: {e}");
+          return true;
+        }
+      }
 
-		public static void BreakWindow(WeaponManager __instance, float distance, BreakableWindow target)
+      public static void BreakWindow(WeaponManager __instance, float distance, BreakableWindow target)
 		{
 			if (distance > 40f) // Distance protection
 			{
