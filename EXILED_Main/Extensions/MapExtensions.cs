5c8593981b8b2e50a2cd96c88e399bd3d3c5e2e5
Jorge G <jorgg69@gmail.com>
2020-02-04 21:03:53 +0100

    Added new Extensions, tweaked and "fixed" a few old ones

---
+++ EXILED_Main/Extensions/MapExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EXILED.Extensions
+{
+	public static class Map
+	{
+		private static Inventory _hostInventory;
+		private static AlphaWarheadController _alphaWarheadController;
+		public static Inventory HostInventory
+		{
+			get
+			{
+				if (_hostInventory == null)
+				{
+					// Benchmarked by Petris (SL Programmer), GetHub is faster than a single GetComponent
+					_hostInventory = ReferenceHub.GetHub(PlayerManager.localPlayer).inventory;
+				}
+				return _hostInventory;
+			}
+		}
+		public static AlphaWarheadController AlphaWarheadController
+		{
+			get
+			{
+				if (_alphaWarheadController == null)
+				{
+					_alphaWarheadController = PlayerManager.localPlayer.GetComponent<AlphaWarheadController>();
+				}
+				return _alphaWarheadController;
+			}
+		}
+		
+		/// <summary>
+		/// Spawns an item of type <paramref name="itemType"/> in a desired <paramref name="position"/>.
+		/// </summary>
+		/// <param name="itemType">The type of the item to be spawned</param>
+		/// <param name="durability">The durability (or ammo, depends on the weapon) of the item</param>
+		/// <param name="position">Where the item will be spawned</param>
+		/// <param name="rotation">The rotation. We recommend you to use <see cref="Quaternion.Euler(float, float, float)"/></param>
+		/// <param name="sight">The sight the weapon will have (0 is nothing, 1 is the first sight available in the weapon manager, and so on)</param>
+		/// <param name="barrel">The barrel of the weapon (0 is no custom barrel, 1 is the first barrel available, and so on)</param>
+		/// <param name="other">Other attachments like flashlight, laser or ammo counter</param>
+		/// <returns>The <see cref="Pickup"/></returns>
+		public static Pickup SpawnItem(ItemType itemType, float durability, Vector3 position, Quaternion rotation = default, int sight = 0, int barrel = 0, int other = 0)
+			=> HostInventory.SetPickup(itemType, durability, position, rotation, sight, barrel, other);
+		
+			/// <summary>
+		/// Starts the warhead.
+		/// </summary>
+		public static void StartWarhead() => AlphaWarheadController.StartDetonation();
+		
+		/// <summary>
+		/// Stops the warhead.
+		/// </summary>
+		public static void StopWarhead() => AlphaWarheadController.CancelDetonation();
+		
+		/// <summary>
+		/// Detonates the warhead.
+		/// </summary>
+		public static void DetonateWarhead() => AlphaWarheadController.Detonate();
+
+		/// <summary>
+		/// Starts the nuke.
+		/// </summary>
+		public static void StartNuke() => AlphaWarheadController.StartDetonation();
+
+		/// <summary>
+		/// Stops the nuke.
+		/// </summary>
+		public static void StopNuke() => AlphaWarheadController.CancelDetonation();
+		
+		/// <summary>
+		/// Detonates the nuke.
+		/// </summary>
+		public static void DetonateNuke() => AlphaWarheadController.Detonate();
+	}
+}
--- EXILED_Main/Extensions/PlayerExtensions.cs
+++ EXILED_Main/Extensions/PlayerExtensions.cs
@@ -53,12 +53,41 @@ namespace EXILED.Extensions
         /// Sets the rank of a <see cref="ReferenceHub"/> to a <see cref="UserGroup"/>.
         /// </summary>
         public static void SetRole(this ReferenceHub rh, UserGroup userGroup) => rh.serverRoles.SetGroup(userGroup, false, false, false);
-        public static void SetNickname(this ReferenceHub rh, string name) => rh.nicknameSync.Network_myNickSync = name;
+        /// <summary>
+        /// Sets the nickname of a <see cref="ReferenceHub"/> to <paramref name="name"/>
+        /// </summary>
+        public static void SetNickname(this ReferenceHub rh, string name)
+        {
+            rh.nicknameSync.Network_myNickSync = name;
+            MEC.Timing.RunCoroutine(BlinkTag(rh));
+        }
+        private static IEnumerator<float> BlinkTag(ReferenceHub rh)
+        {
+            yield return MEC.Timing.WaitForOneFrame;
+            rh.characterClassManager.CallCmdRequestHideTag();
+            yield return MEC.Timing.WaitForOneFrame;
+            rh.characterClassManager.CallCmdRequestShowTag(false);
+        }
+        /// <summary>
+        /// Gives an item to the specified player.
+        /// </summary>
+        /// <param name="itemType">Your <see cref="ItemType"/></param>
+        /// <param name="dur">The durability (most of the times ammo) of the item</param>
+        /// <param name="sight">0 is no sight, 1 is the first sight in the Weapon Manager</param>
+        /// <param name="barrel">0 is no custom barrel, 1 is the first barrel in the Weapon Manager</param>
+        /// <param name="other">0 is no extra attachment, other numbers are the ammo counter, flashlight, etc.</param>
+        public static void GiveItem(this ReferenceHub rh, ItemType itemType, float dur = float.NegativeInfinity, int sight = 0, int barrel = 0, int other = 0) => rh.inventory.AddNewItem(itemType, dur, sight, barrel, other);
 
         // Adapted from https://github.com/galaxy119/SamplePlugin/blob/master/SamplePlugin/Extensions.cs
+        /// <summary>
+        /// Sends a message to the RA console of a <paramref name="sender"/>. <paramref name="pluginName"/> will be the name of your <see cref="Assembly"/>
+        /// </summary>
         public static void RAMessage(this CommandSender sender, string message, bool success = true, string pluginName = null) =>
             sender.RaReply((pluginName ?? Assembly.GetCallingAssembly().FullName) + "#" + message, success, true, string.Empty);
 
+        /// <summary>
+        /// A simple broadcast to a player. Doesn't get logged to the console.
+        /// </summary>
         public static void Broadcast(this ReferenceHub rh, uint time, string message) => rh.GetComponent<Broadcast>().TargetAddElement(rh.scp079PlayerScript.connectionToClient, message, time, false);
     }
 }
