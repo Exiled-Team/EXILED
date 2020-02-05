e3edfb99c45b9b69e59b3123cc8153e68418c274
galaxy119 <amathor929@gmail.com>
2020-02-04 21:43:51 -0800

    Add Idler boi

---
+++ EXILED_Idler/Plugin.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Threading;
+using EXILED;
+using UnityEngine;
+using Exception = Mono.WebBrowser.Exception;
+
+namespace EXILED_Idler
+{
+	public class IdlePlus : Plugin
+	{
+		private static Thread _thread;
+
+		public static DateTime LastActive;
+
+		public static bool WasLastCheckIdle;
+		
+		public override void OnEnable()
+		{
+			_thread = new Thread(DoIdleCheck);
+			_thread.Start();
+			Events.PreAuthEvent += EventsOnPreAuthEvent;
+		}
+
+		private void EventsOnPreAuthEvent(ref PreauthEvent ev)
+		{
+			Info("Server process resuming..");
+			WasLastCheckIdle = false;
+			LastActive = DateTime.UtcNow;
+		}
+
+		public override void OnDisable()
+		{
+			_thread = null;
+		}
+
+		public override void OnReload()
+		{
+			
+		}
+
+		public override string getName => "IdlePlus";
+
+
+
+		public static void DoIdleCheck()
+		{
+			while(_thread != null)
+			{
+				try
+				{
+					bool idle = IsServerIdle();
+
+					if (idle && !WasLastCheckIdle)
+					{
+						LastActive = DateTime.UtcNow;
+						Debug("cool, the server is idle for the first time");
+					}
+
+					if (idle && WasLastCheckIdle && LastActive != null && DateTime.UtcNow.Subtract(LastActive).TotalMinutes > 3)
+					{
+						Info("The server is now idle..");
+						Time.timeScale = 0.01f;
+						Application.targetFrameRate = 1;
+					}
+
+					if (!idle || (LastActive != null && DateTime.UtcNow.Subtract(LastActive).TotalMinutes < 1))
+					{
+						Time.timeScale = 1f;
+						Application.targetFrameRate = 60;
+						Debug("oh no, the server isnt idle");
+					}
+
+					WasLastCheckIdle = idle;
+				}
+				catch (Exception e)
+				{
+					Error(e.ToString());
+				}
+				Thread.Sleep(5000);
+			}
+		}
+
+
+		public static bool IsServerIdle()
+		{
+			lock(PlayerManager.players)
+				if (PlayerManager.players.Count == 0)
+					return true;
+
+			return false;
+		}
+	}
+}
