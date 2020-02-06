d4da2bf5486af4e9239365f5a34686903df5b979
galaxy119 <amathor929@gmail.com>
2020-02-06 06:26:34 -0800

    smol changes

--- EXILED_Events/Patches/UseMedicalEvent.cs
+++ EXILED_Events/Patches/UseMedicalEvent.cs
@@ -1,5 +1,6 @@
 using System;
 using Harmony;
+using MEC;
 
 namespace EXILED.Patches
 {
@@ -13,9 +14,23 @@ namespace EXILED.Patches
 
 			try
 			{
-				bool allow = true;
-				Events.InvokeUseMedicalItem(__instance.gameObject, __instance.GetComponent<Inventory>().curItem, ref allow);
-				return allow;
+				if (!__instance._interactRateLimit.CanExecute())
+					return false;
+				__instance.cancel = false;
+				if (__instance.cooldown > 0.0)
+					return false;
+				for (int mid = 0; mid < __instance.usableItems.Length; ++mid)
+				{
+					if (__instance.usableItems[mid].inventoryID == __instance.inv.curItem && __instance.usableCooldowns[mid] <= 0.0)
+					{
+						bool allow = true;
+						Events.InvokeUseMedicalItem(__instance.gameObject, __instance.GetComponent<Inventory>().curItem, ref allow);
+						if (allow)
+							Timing.RunCoroutine(__instance.UseMedicalItem(mid), Segment.FixedUpdate);
+					}
+				}
+
+				return false;
 			}
 			catch (Exception e)
 			{
