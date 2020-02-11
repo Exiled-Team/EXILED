3394d8929c93e442dcb30f1a68bcbeaf9f135de4
galaxy119 <amathor929@gmail.com>
2020-02-11 02:45:31 -0800

    version bump

--- EXILED_Events/Plugin.cs
+++ EXILED_Events/Plugin.cs
@@ -24,7 +24,7 @@ namespace EXILED
 		{
 			Major = 1,
 			Minor = 7,
-			Patch = 11
+			Patch = 12
 		};
 		
 		//The below variables are used to disable the patch for any particular event, allowing devs to implement events themselves.
