06ea96925fafffa0a2e49366a44b20e67390996c
Jorge G <jorgg69@gmail.com>
2020-02-18 18:01:18 +0100

    Added BanUserOverride method signature

---
+++ EXILED_Events/Patches/BanUserOverride.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EXILED.Patches
+{
+    [HarmonyPatch(typeof(BanPlayer), nameof(BanPlayer.BanUser))]
+    public class BanUserOverride
+    {
+        public static bool Prefix(GameObject user, int duration, string reason, string issuer, bool isGlobalBan)
+        {
+
+            return false;
+        }
+    }
+}
