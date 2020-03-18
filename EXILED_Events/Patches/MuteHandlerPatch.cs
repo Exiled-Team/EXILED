b3b603a7edecbdc8e3b7c4f6417ee330173e249f
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-03-19 02:33:28 +0900

    tab to space

--- EXILED_Events/Patches/MuteHandlerPatch.cs
+++ EXILED_Events/Patches/MuteHandlerPatch.cs
@@ -11,28 +11,28 @@ namespace EXILED.Patches
     {
         public static bool Prefix()
         {
-			MuteHandler._path = ConfigSharing.Paths[1] + "mutes.txt";
-			ServerConsole.AddLog("Loading saved mutes...");
-			try
-			{
-				MuteHandler.Mutes.Clear();
-				using(StreamReader streamReader = new StreamReader(MuteHandler._path))
-				{
-					string text;
-					while((text = streamReader.ReadLine()) != null)
-					{
-						if(!string.IsNullOrWhiteSpace(text))
-						{
-							MuteHandler.Mutes.Add(text.Trim());
-						}
-					}
-				}
-			}
-			catch
-			{
-				ServerConsole.AddLog("Can't load the mute file!");
-			}
-			return false;
+            MuteHandler._path = ConfigSharing.Paths[1] + "mutes.txt";
+            ServerConsole.AddLog("Loading saved mutes...");
+            try
+            {
+                MuteHandler.Mutes.Clear();
+                using(StreamReader streamReader = new StreamReader(MuteHandler._path))
+                {
+                    string text;
+                    while((text = streamReader.ReadLine()) != null)
+                    {
+                        if(!string.IsNullOrWhiteSpace(text))
+                        {
+                            MuteHandler.Mutes.Add(text.Trim());
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                ServerConsole.AddLog("Can't load the mute file!");
+            }
+            return false;
         }
     }
 }
