2d88dd3e012fb8a5b820cad5bb16849568dbc401
Lufou <lucas.damase@laposte.net>
2020-03-30 20:54:02 +0200

    Added GetGlobalBadge to Player extension
    Added GetGlobalBadge() method to Player Extension to return global badge infos or null

--- EXILED_Main/Extensions/Player.cs
+++ EXILED_Main/Extensions/Player.cs
@@ -436,6 +436,35 @@ namespace EXILED.Extensions
 		}
 
 		/// <summary>
+		/// Get the global badge of the player.
+		/// </summary>
+		/// <param name="player">Player's ReferenceHub</param>
+		/// <returns>GlobalBadge or null</returns>
+		public static GlobalBadge GetGlobalBadge(this ReferenceHub player)
+		{
+      			string token = player.serverRoles.NetworkGlobalBadge;
+       			if (string.IsNullOrEmpty(token)) { return null; }
+            		Dictionary<string, string> dictionary = (from rwr in token.Split(new string[]
+           		{
+               			"<br>"
+          		}, StringSplitOptions.None)
+           		select rwr.Split(new string[]
+           		{
+               			": "
+           		}, StringSplitOptions.None)).ToDictionary((string[] split) => split[0], (string[] split) => split[1]);
+
+       			int BadgeType = 0;
+            		if(int.TryParse(dictionary["Badge type"], out int type)) { BadgeType = type; }
+
+      			return new GlobalBadge
+       			{
+           			BadgeText = dictionary["Badge text"],
+                		BadgeColor = dictionary["Badge color"],
+           			Type = BadgeType
+       			};
+   		}
+
+		/// <summary>
 		/// Get the current room a player are in (from Smod2).
 		/// </summary>
 		/// <param name="player">Player's ReferenceHub</param>
