37ba8deee1e7b55fe09096bddf8d3c936f5db488
SebasCapo <sebascapoxo@gmail.com>
2020-04-27 23:21:50 -0300

    Ty Orange, I mean, Lemon

--- EXILED_Main/Extensions/Item.cs
+++ EXILED_Main/Extensions/Item.cs
@@ -6,7 +6,7 @@ namespace EXILED.Extensions {
         public static float GetWeaponAmmo( this Inventory.SyncItemInfo item ) => item.durability;
 
         public static bool IsAmmo( this ItemType item ) =>
-            (item == ItemType.Ammo556 || item == ItemType.Ammo9mm || item == ItemType.Ammo762);
+            item == ItemType.Ammo556 || item == ItemType.Ammo9mm || item == ItemType.Ammo762;
 
         public static bool IsWeapon( this ItemType type ) =>
             (type == ItemType.GunCOM15 || type == ItemType.GunE11SR || type == ItemType.GunLogicer
@@ -14,22 +14,22 @@ namespace EXILED.Extensions {
             || type == ItemType.MicroHID);
 
         public static bool IsSCP( this ItemType type ) =>
-            (type == ItemType.SCP018 || type == ItemType.SCP500 || type == ItemType.SCP268 || type == ItemType.SCP207);
+            type == ItemType.SCP018 || type == ItemType.SCP500 || type == ItemType.SCP268 || type == ItemType.SCP207;
 
         public static bool IsThrowable( this ItemType type ) =>
-            (type == ItemType.SCP018 || type == ItemType.GrenadeFrag || type == ItemType.GrenadeFlash);
+            type == ItemType.SCP018 || type == ItemType.GrenadeFrag || type == ItemType.GrenadeFlash;
 
         public static bool IsMedical( this ItemType type ) =>
-            (type == ItemType.Painkillers || type == ItemType.Medkit || type == ItemType.SCP500 || type == ItemType.Adrenaline);
+            type == ItemType.Painkillers || type == ItemType.Medkit || type == ItemType.SCP500 || type == ItemType.Adrenaline;
 
         public static bool IsUtility( this ItemType type ) =>
-            (type == ItemType.Disarmer || type == ItemType.Flashlight || type == ItemType.Radio || type == ItemType.WeaponManagerTablet);
+            type == ItemType.Disarmer || type == ItemType.Flashlight || type == ItemType.Radio || type == ItemType.WeaponManagerTablet;
 
         public static bool IsKeycard( this ItemType type ) =>
-            (ItemType.KeycardChaosInsurgency == type || ItemType.KeycardContainmentEngineer == type || ItemType.KeycardFacilityManager == type
+            ItemType.KeycardChaosInsurgency == type || ItemType.KeycardContainmentEngineer == type || ItemType.KeycardFacilityManager == type
             || ItemType.KeycardGuard == type || ItemType.KeycardJanitor == type || ItemType.KeycardNTFCommander == type
             || ItemType.KeycardNTFLieutenant == type || ItemType.KeycardO5 == type || ItemType.KeycardScientist == type
-            || ItemType.KeycardScientistMajor == type || ItemType.KeycardSeniorGuard == type || ItemType.KeycardZoneManager == type);
+            || ItemType.KeycardScientistMajor == type || ItemType.KeycardSeniorGuard == type || ItemType.KeycardZoneManager == type;
 
     }
 }
