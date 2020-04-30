38d3fbb3b64ba6bfafc71499ef64bc5be1b4bcd0
galaxy119 <amathor929@gmail.com>
2020-04-30 06:54:26 -0700

    oop

--- EXILED_Events/Plugin.cs
+++ EXILED_Events/Plugin.cs
@@ -19,7 +19,7 @@ namespace EXILED
 		internal static DateTime RoundTime;
 		public static Random Gen = new Random();
 		public static string VersionUpdateUrl = "none";
-		public static ExiledVersion Version = new ExiledVersion { Major = 1, Minor = 10, Patch = 3 };
+		public static ExiledVersion Version = new ExiledVersion { Major = 1, Minor = 10, Patch = 4 };
 
 		//The below variables are used to disable the patch for any particular event, allowing devs to implement events themselves.
 		#region Patch Disable
