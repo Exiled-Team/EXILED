d9cb2c991b805f98d4346ea7e3d5c71bdbbca631
kirun9 <ulubiony.creeper@gmail.com>
2020-05-15 17:24:43 +0200

    Cleanings

--- EXILED_Events/Patches/DamageTypesPatch.cs
+++ EXILED_Events/Patches/DamageTypesPatch.cs
@@ -1,9 +1,4 @@
-﻿using Harmony;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Harmony;
 
 namespace EXILED.Patches
 {
