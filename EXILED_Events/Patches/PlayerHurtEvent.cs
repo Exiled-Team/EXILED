5d67573b58393c2af896a8ced450901de2498c79
iRebbok <irebbok@outlook.com>
2020-05-20 19:15:58 +0300

    a more correct calculation of the damage & correction of call events of death when hit in the pocket dimension

--- EXILED_Events/Patches/PlayerHurtEvent.cs
+++ EXILED_Events/Patches/PlayerHurtEvent.cs
@@ -21,22 +21,6 @@ namespace EXILED.Patches
 
 				var goReferenceHub = ReferenceHub.Hubs[go];
 
-				if (info.GetDamageType() == DamageTypes.Pocket)
-				{
-					bool allow = true;
-
-					Events.InvokePocketDimDamage(__instance.gameObject, ref allow);
-
-					if (!allow)
-						info.Amount = 0f;
-
-					if (info.Amount >= goReferenceHub.playerStats.health)
-						Events.InvokePocketDimDeath(__instance.gameObject, ref allow);
-
-					if (!allow)
-						info.Amount = 0f;
-				}
-
 				if (info.GetDamageType() == DamageTypes.Grenade)
 					Events.InvokePlayerHurt(__instance, ref info, go, info.PlyId);
 				else
@@ -46,7 +30,26 @@ namespace EXILED.Patches
 				if (goReferenceHub.characterClassManager.GodMode)
 					return;
 
-				if (info.Amount >= goReferenceHub.playerStats.health || (goReferenceHub.playerStats.health - info.Amount) <= 1f)
+				bool isDied = Math.Floor(goReferenceHub.playerStats.health - info.Amount) <= 0f;
+
+				if (info.GetDamageType() == DamageTypes.Pocket)
+				{
+					bool allow = true;
+
+					Events.InvokePocketDimDamage(__instance.gameObject, ref allow);
+
+					if (!allow)
+						info.Amount = 0f;
+
+					if (isDied)
+						Events.InvokePocketDimDeath(__instance.gameObject, ref allow);
+
+					if (!allow)
+						info.Amount = 0f;
+				}
+
+				// don't need to check if it contains more damage
+				if (isDied)
 				{
 					// I don't think that's possible
 					// ReferenceHub can't be without CharterClassManager
