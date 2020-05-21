5e35826438cae1ca1e9e717e89c9e1349e6b6b6b
iRebbok <irebbok@outlook.com>
2020-05-21 09:50:03 +0300

    fix: remove unnecessary math & don't call playerdeathevent when unsyncedhp

--- EXILED_Events/Patches/PlayerHurtEvent.cs
+++ EXILED_Events/Patches/PlayerHurtEvent.cs
@@ -19,7 +19,7 @@ namespace EXILED.Patches
 				if (go == null) // As far as I remember, it's possible @iRebbok
 					return;
 
-				var goReferenceHub = ReferenceHub.Hubs[go];
+				var goReferenceHub = ReferenceHub.GetHub(go);
 
 				// observe the order of calling the event, first there is damage to the player
 				if (info.GetDamageType() == DamageTypes.Grenade)
@@ -31,7 +31,28 @@ namespace EXILED.Patches
 				if (goReferenceHub.characterClassManager.GodMode)
 					return;
 
-				bool isDied = Math.Floor(goReferenceHub.playerStats.health - info.Amount) <= 0f;
+				bool isDied = goReferenceHub.playerStats.health - info.Amount < 1f;
+
+				// If the last attack was from the 'ARTIFICIALDEGEN', then hp not change
+				if (goReferenceHub.playerStats.unsyncedArtificialHealth > 0f && !goReferenceHub.playerStats.lastHitInfo.Attacker.Equals("ARTIFICIALDEGEN")) 
+				{
+					// reduced attack damage for unsyncedHp
+					var reducedDamage = info.Amount * goReferenceHub.playerStats.artificialNormalRatio;
+					// the remainder of the damage
+					var remainderDamage = info.Amount - reducedDamage;
+
+					var finalUnsyncedHp = goReferenceHub.playerStats.unsyncedArtificialHealth - reducedDamage;
+					if (finalUnsyncedHp < 0f)
+					{
+						remainderDamage += Mathf.Abs(goReferenceHub.playerStats.unsyncedArtificialHealth);
+						goReferenceHub.playerStats.unsyncedArtificialHealth = 0f;
+					}
+					var finalHp = goReferenceHub.playerStats.health - remainderDamage;
+
+					// Don't use bitwise operations, 
+					// this is the hp that will remain after everything
+					isDied = finalHp < 1f;
+				}
 
 				if (info.GetDamageType() == DamageTypes.Pocket)
 				{
@@ -56,6 +77,9 @@ namespace EXILED.Patches
 					// ReferenceHub can't be without CharterClassManager
 					//if (goReferenceHub.characterClassManager != null)
 					//{
+
+					// Checking to re-call the event for this player??? Does it work?
+					// 
 					if (DeathStuff.Contains(goReferenceHub.characterClassManager.UserId))
 						return;
 
@@ -83,8 +107,10 @@ namespace EXILED.Patches
 			CharacterClassManager ccm = go.GetComponent<CharacterClassManager>();
 
 			if (ccm != null)
-				if (PlayerHurtEvent.DeathStuff.Contains(ccm.UserId))
-					PlayerHurtEvent.DeathStuff.Remove(ccm.UserId);
+				// No need to check for availability, 
+				// we will get true if deleted, 
+				// otherwise false, there will be no error
+				PlayerHurtEvent.DeathStuff.Remove(ccm.UserId);
 		}
 	}
 }
