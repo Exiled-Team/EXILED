6f3cdc8420637bf14f81c3ce7c348abe70712092
iRebbok <irebbok@outlook.com>
2020-05-31 21:10:29 +0300

    fix: doot interaction event isn't called

--- EXILED_Events/Patches/DoorInteractionEvent.cs
+++ EXILED_Events/Patches/DoorInteractionEvent.cs
@@ -20,27 +20,19 @@ namespace EXILED.Patches
 			{
 				if (!__instance._playerInteractRateLimit.CanExecute() || __instance._hc.CufferId > 0 && !__instance.CanDisarmedInteract || (doorId == null || __instance._ccm.CurClass == RoleType.None || (__instance._ccm.CurClass == RoleType.Spectator || !doorId.TryGetComponent<Door>(out door))) || (door.buttons.Count == 0 ? (__instance.ChckDis(doorId.transform.position) ? 1 : 0) : (door.buttons.Any(item => __instance.ChckDis(item.transform.position)) ? 1 : 0)) == 0)
 					return false;
+
 				__instance.OnInteract();
 				if (__instance._sr.BypassMode)
-					door.ChangeState(true);
+					allow = true;
 				else if (door.PermissionLevels.HasPermission(Door.AccessRequirements.Checkpoints) && __instance._ccm.CurRole.team == Team.SCP)
-				{
-					door.ChangeState();
-				}
+					allow = true;
 				else
 				{
 					try
 					{
 						if (door.PermissionLevels == 0)
 						{
-							if (door.locked)
-								return false;
-							Events.InvokeDoorInteract(__instance.gameObject, door, ref allow);
-							
-							if (allow)
-								door.ChangeState();
-							else
-								__instance.RpcDenied(doorId);
+							allow = !door.locked;
 						}
 						else if (!door.RequireAllPermissions)
 						{
@@ -48,29 +40,22 @@ namespace EXILED.Patches
 							{
 								Door.AccessRequirements flag;
 								if (door.backwardsCompatPermissions.TryGetValue(permission, out flag) && door.PermissionLevels.HasPermission(flag))
-								{
-									if (door.locked)
-										return false;
-									
-									Events.InvokeDoorInteract(__instance.gameObject, door, ref allow);
-							
-									if (allow)
-										door.ChangeState();
-									else
-										__instance.RpcDenied(doorId);
-									door.ChangeState();
-									return false;
-								}
+									allow = !door.locked;
 							}
-							__instance.RpcDenied(doorId);
 						}
-						else
-							__instance.RpcDenied(doorId);
 					}
 					catch
 					{
-						__instance.RpcDenied(doorId);
+						allow = false;
 					}
+
+					Events.InvokeDoorInteract(__instance.gameObject, door, ref allow);
+
+					if (allow)
+						// Simply pass the force status as the presence of bypassMode on the player
+						door.ChangeState(__instance._sr.BypassMode);
+					else
+						__instance.RpcDenied(doorId);
 				}
 
 				return false;
@@ -79,8 +64,10 @@ namespace EXILED.Patches
 			{
 				Log.Error($"DoorInteractionEvent error: {exception}");
 
-				if (allow) door.ChangeState();
-				else __instance.RpcDenied(doorId);
+				if (allow) 
+					door.ChangeState(__instance._sr.BypassMode);
+				else
+					__instance.RpcDenied(doorId);
 
 				return false;
 			}
