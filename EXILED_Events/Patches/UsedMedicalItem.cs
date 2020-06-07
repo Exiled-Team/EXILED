8bd503d36b780386f7a23603cc583b22931980e4
galaxy119 <amathor929@gmail.com>
2020-06-07 05:22:07 -0700

    Beta VIII

--- EXILED_Events/Patches/UsedMedicalItem.cs
+++ EXILED_Events/Patches/UsedMedicalItem.cs
@@ -6,11 +6,11 @@ namespace EXILED.Patches
 	[HarmonyPatch(typeof(ConsumableAndWearableItems), nameof(ConsumableAndWearableItems.SendRpc))]
 	public class UsedMedicalItem
 	{
-		public static void Prefix(ConsumableAndWearableItems __instance, ConsumableAndWearableItems.HealAnimation animation, int mid)
+		public static void Prefix(ConsumableAndWearableItems __instance, ConsumableAndWearableItems.HealAnimation healAnimation, int mid)
 		{
 			try
 			{
-				if (animation == ConsumableAndWearableItems.HealAnimation.DequipMedicalItem)
+				if (healAnimation == ConsumableAndWearableItems.HealAnimation.DequipMedicalItem)
 					Events.InvokeUsedMedicalItem(__instance.gameObject, __instance.usableItems[mid].inventoryID);
 			}
 			catch (Exception exception)
