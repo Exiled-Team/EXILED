63b690f76ea44e49617f5f4ff3c53c054077a6cf
Pietro <iopietro@hotmail.it>
2020-07-03 23:39:23 +0200

    Bug fixes +Removed a patch that prevented players to spawn at the start of the round.

--- Exiled.Example/Handlers/Player.cs
+++ Exiled.Example/Handlers/Player.cs
@@ -18,19 +18,19 @@ namespace Exiled.Example.Handlers
         /// <inheritdoc cref="Events.Handlers.Player.OnDied(DiedEventArgs)"/>
         public void OnDied(DiedEventArgs ev)
         {
-            Log.Info($"{ev.Target.Nickname} died from {ev.HitInformations.GetDamageName()}! {ev.Killer.Nickname} killed him!");
+            Log.Info($"{ev.Target?.Nickname} died from {ev.HitInformations.GetDamageName()}! {ev.Killer?.Nickname} killed him!");
         }
 
         /// <inheritdoc cref="Events.Handlers.Player.OnChangingRole(ChangingRoleEventArgs)"/>
         public void OnChangingRole(ChangingRoleEventArgs ev)
         {
-            Log.Info($"{ev.Player.Nickname} is changing his role! The new role will be {ev.NewRole}!");
+            Log.Info($"{ev.Player?.Nickname} ({ev.Player?.Role}) is changing his role! The new role will be {ev?.NewRole}!");
         }
 
         /// <inheritdoc cref="Events.Handlers.Player.OnChangingItem(ChangingItemEventArgs)"/>
         public void OnChangingItem(ChangingItemEventArgs ev)
         {
-            Log.Info($"{ev.Player.Nickname} is changing his {ev.OldItem.id} item to {ev.NewItem.id}!");
+            Log.Info($"{ev.Player?.Nickname} is changing his {ev.OldItem.id} item to {ev.NewItem.id}!");
         }
     }
 }
