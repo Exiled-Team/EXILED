eb2798f4616393db5b1ddbe69b2905c3d918174b
Pietro <iopietro@hotmail.it>
2020-08-10 04:25:26 +0200

    Bumped RequiredExiledVersion

--- Exiled.API/Features/Plugin.cs
+++ Exiled.API/Features/Plugin.cs
@@ -56,7 +56,7 @@ namespace Exiled.API.Features
         public virtual Version Version { get; }
 
         /// <inheritdoc/>
-        public virtual Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
+        public virtual Version RequiredExiledVersion { get; } = new Version(2, 0, 11);
 
         /// <inheritdoc/>
         public Dictionary<Type, Dictionary<Type, ICommand>> Commands { get; } = new Dictionary<Type, Dictionary<Type, ICommand>>()
