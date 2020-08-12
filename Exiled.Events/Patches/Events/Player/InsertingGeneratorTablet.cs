ca5f4be3c6ea33aa4a36671b79fc4ddb984af81d
BuildBoy12 <jwbradley2003@gmail.com>
2020-08-12 16:25:03 -0400

    Update Spacing
    Think I got it this time

--- Exiled.Events/Patches/Events/Player/InsertingGeneratorTablet.cs
+++ Exiled.Events/Patches/Events/Player/InsertingGeneratorTablet.cs
@@ -46,10 +46,10 @@ namespace Exiled.Events.Patches.Events.Player
                                         isAllowed = true;
                                 }
                             }
+                            
                             var unlockingEventArgs = new UnlockingGeneratorEventArgs(API.Features.Player.Get(person), __instance, isAllowed);
                             Player.OnUnlockingGenerator(unlockingEventArgs);
-                            isAllowed = unlockingEventArgs.IsAllowed;
-                            
+                            isAllowed = unlockingEventArgs.IsAllowed;                            
                             if (isAllowed)
                             {
                                 __instance.NetworkisDoorUnlocked = true;
@@ -61,7 +61,7 @@ namespace Exiled.Events.Patches.Events.Player
                             }
 
                             break;
-                        } 
+                        }
                         
                         switch (__instance.isDoorOpen)
                         {
