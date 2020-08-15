a1ecf0600f499a7a696ad4f2880f58228cb76731
kirun9 <ulubiony.creeper@gmail.com>
2020-08-15 23:05:11 +0200

    Update Hurting.cs

--- Exiled.Events/Patches/Events/Player/Hurting.cs
+++ Exiled.Events/Patches/Events/Player/Hurting.cs
@@ -39,7 +39,7 @@ namespace Exiled.Events.Patches.Events.Player
 
                 if (info.GetDamageType() == DamageTypes.Recontainment && target.Role == RoleType.Scp079)
                 {
-                    Scp079.OnRecontainmented(new RecontainmentedEventArgs(target));
+                    Scp079.OnRecontained(new RecontainedEventArgs(target));
                     var eventArgs = new DiedEventArgs(null, target, info);
                     Player.OnDied(eventArgs);
                 }
