edc4f9c5d2eb089eeedf88b06c2b7ea6703c0265
kirun9 <ulubiony.creeper@gmail.com>
2020-08-15 17:16:21 +0200

    Add files via upload

---
+++ Exiled.Events/EventArgs/RecontainmentedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RecontainmentedEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    /// <summary>
+    /// Contains informations after SCP-079 recontainming.
+    /// </summary>
+    public class RecontainmentedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecontainmentedEventArgs"/> class.
+        /// </summary>
+        /// <param name="target"><inheritdoc cref="Target"/></param>
+        public RecontainmentedEventArgs(Player target)
+        {
+            Target = target;
+        }
+
+        /// <summary>
+        /// Gets the player that was SCP-079.
+        /// </summary>
+        public Player Target { get; }
+    }
+}
