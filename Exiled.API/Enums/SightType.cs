0ca8cca8e89e343b75e7fd834908006d52e689b4
gamehunt <mt.demakov@gmail.com>
2020-09-03 16:46:37 +0300

    Enums for weapon attachments

---
+++ Exiled.API/Enums/SightType.cs
@@ -0,0 +1,50 @@
+// -----------------------------------------------------------------------
+// <copyright file="SightType.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Enums
+{
+    /// <summary>
+    /// Possible sight weapon modifications.
+    /// </summary>
+    public enum SightType
+    {
+        /// <summary>
+        /// No sight
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Collimator
+        /// </summary>
+        Collimator,
+
+        /// <summary>
+        /// Holo Sight
+        /// </summary>
+        Holo,
+
+        /// <summary>
+        /// Blue Dot Sight
+        /// </summary>
+        BlueDot,
+
+        /// <summary>
+        /// Red Dot Sight
+        /// </summary>
+        RedDot,
+
+        /// <summary>
+        /// Night Vision Sight
+        /// </summary>
+        NightVision,
+
+        /// <summary>
+        /// Sniper Scope
+        /// </summary>
+        Sniper,
+    }
+}
--- Exiled.API/Extensions/Item.cs
+++ Exiled.API/Extensions/Item.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="Item.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -7,6 +7,9 @@
 
 namespace Exiled.API.Extensions
 {
+    using System.Linq;
+
+    using Exiled.API.Enums;
     using Exiled.API.Features;
 
     using UnityEngine;
@@ -107,5 +110,230 @@ namespace Exiled.API.Extensions
             type == ItemType.KeycardGuard || type == ItemType.KeycardJanitor || type == ItemType.KeycardNTFCommander ||
             type == ItemType.KeycardNTFLieutenant || type == ItemType.KeycardO5 || type == ItemType.KeycardScientist ||
             type == ItemType.KeycardScientistMajor || type == ItemType.KeycardSeniorGuard || type == ItemType.KeycardZoneManager;
+
+        /// <summary>
+        /// Gets sight modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <returns>Returns <see cref="SightType"/>.</returns>
+        public static SightType GetSight(this Player player, Inventory.SyncItemInfo weapon)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    switch (wep.mod_sights[weapon.modSight].name)
+                    {
+                        case "Collimator":
+                            return SightType.Collimator;
+                        case "Holo Sight":
+                            return SightType.Holo;
+                        case "Red Dot":
+                            return SightType.RedDot;
+                        case "Blue Dot Sight":
+                            return SightType.BlueDot;
+                        case "Night Vision Sight":
+                            return SightType.NightVision;
+                        case "Sniper Scope":
+                            return SightType.Sniper;
+                    }
+                }
+            }
+
+            return SightType.None;
+        }
+
+        /// <summary>
+        /// Sets sight modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <param name="type">Type of the sight.</param>
+        public static void SetSight(this Player player, Inventory.SyncItemInfo weapon, SightType type)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    string name = "None";
+                    switch (type)
+                    {
+                        case SightType.Collimator:
+                            name = "Collimator";
+                            break;
+                        case SightType.Holo:
+                            name = "Holo Sight";
+                            break;
+                        case SightType.BlueDot:
+                            name = "Blue Dot Sight";
+                            break;
+                        case SightType.RedDot:
+                            name = "Red Dot";
+                            break;
+                        case SightType.NightVision:
+                            name = "Night Vision Sight";
+                            break;
+                        case SightType.Sniper:
+                            name = "Sniper Scope";
+                            break;
+                    }
+
+                    int weaponMod = wep.mod_sights.Select((s, i) => new { s, i }).Where(e => e.s.name == name).Select(e => e.i).FirstOrDefault();
+                    int weaponId = player.Inventory.items.FindIndex(s => s == weapon);
+                    weapon.modSight = weaponMod;
+                    player.Inventory.items[weaponId] = weapon;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets barrel modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <returns>Returns <see cref="BarrelType"/>.</returns>
+        public static BarrelType GetBarrel(this Player player, Inventory.SyncItemInfo weapon)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    switch (wep.mod_barrels[weapon.modBarrel].name)
+                    {
+                        case "Suppressor":
+                            return BarrelType.Suppressor;
+                        case "Silencer":
+                            return BarrelType.Silencer;
+                        case "Muzzle Brake":
+                            return BarrelType.MuzzleBrake;
+                        case "Heavy Barrel":
+                            return BarrelType.HeavyBarrel;
+                        case "Muzzle Booster":
+                            return BarrelType.MuzzleBooster;
+                    }
+                }
+            }
+
+            return BarrelType.None;
+        }
+
+        /// <summary>
+        /// Sets barrel modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <param name="type">Type of the barrel.</param>
+        public static void SetBarrel(this Player player, Inventory.SyncItemInfo weapon, BarrelType type)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    string name = "None";
+                    switch (type)
+                    {
+                        case BarrelType.HeavyBarrel:
+                            name = "Heavy Barrel";
+                            break;
+                        case BarrelType.MuzzleBooster:
+                            name = "Muzzle Booster";
+                            break;
+                        case BarrelType.MuzzleBrake:
+                            name = "Muzzle Brake";
+                            break;
+                        case BarrelType.Silencer:
+                            name = "Silencer";
+                            break;
+                        case BarrelType.Suppressor:
+                            name = "Suppressor";
+                            break;
+                    }
+
+                    int weaponMod = wep.mod_barrels.Select((s, i) => new { s, i }).Where(e => e.s.name == name).Select(e => e.i).FirstOrDefault();
+                    int weaponId = player.Inventory.items.FindIndex(s => s == weapon);
+                    weapon.modBarrel = weaponMod;
+                    player.Inventory.items[weaponId] = weapon;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets other modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <returns>Returns <see cref="OtherType"/>.</returns>
+        public static OtherType GetOther(this Player player, Inventory.SyncItemInfo weapon)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    switch (wep.mod_others[weapon.modOther].name)
+                    {
+                        case "Flashlight":
+                            return OtherType.Flashlight;
+                        case "Ammo Counter":
+                            return OtherType.AmmoCounter;
+                        case "Gyroscopic Stabilizer":
+                            return OtherType.GyroStabilizer;
+                        case "Laser":
+                            return OtherType.Laser;
+                    }
+                }
+            }
+
+            return OtherType.None;
+        }
+
+        /// <summary>
+        /// Sets other modification of the weapon.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <param name="weapon">The weapon with attachment.</param>
+        /// <param name="type">Type of the other.</param>
+        public static void SetOther(this Player player, Inventory.SyncItemInfo weapon, OtherType type)
+        {
+            WeaponManager wmanager = player.ReferenceHub.weaponManager;
+            if (weapon.id.IsWeapon())
+            {
+                WeaponManager.Weapon wep = wmanager.weapons.Where(wp => wp.inventoryID == weapon.id).FirstOrDefault();
+                if (wep != null)
+                {
+                    string name = "None";
+                    switch (type)
+                    {
+                        case OtherType.AmmoCounter:
+                            name = "Ammo Counter";
+                            break;
+                        case OtherType.Flashlight:
+                            name = "Flashlight";
+                            break;
+                        case OtherType.GyroStabilizer:
+                            name = "Gyroscopic Stabilizer";
+                            break;
+                        case OtherType.Laser:
+                            name = "Laser";
+                            break;
+                    }
+
+                    int weaponMod = wep.mod_others.Select((s, i) => new { s, i }).Where(e => e.s.name == name).Select(e => e.i).FirstOrDefault();
+                    int weaponId = player.Inventory.items.FindIndex(s => s == weapon);
+                    weapon.modOther = weaponMod;
+                    player.Inventory.items[weaponId] = weapon;
+                }
+            }
+        }
     }
 }
