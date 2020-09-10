0456617362035cca50e5a11b08b527c321b9261f
gamehunt <mt.demakov@gmail.com>
2020-09-10 13:40:35 +0300

    Backward compatibility

--- Exiled.Events/Events.cs
+++ Exiled.Events/Events.cs
@@ -9,6 +9,7 @@ namespace Exiled.Events
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
 
     using Exiled.API.Enums;
@@ -22,6 +23,7 @@ namespace Exiled.Events
     /// </summary>
     public sealed class Events : Plugin<Config>
     {
+#pragma warning disable 0618
         private static readonly Lazy<Events> LazyInstance = new Lazy<Events>(() => new Events());
         private readonly InternalHandlers.Round round = new InternalHandlers.Round();
 
@@ -53,9 +55,15 @@ namespace Exiled.Events
         public static Events Instance => LazyInstance.Value;
 
         /// <summary>
+        /// Gets a list of types and methods for which EXILED patches should not be run.
+        /// </summary>
+        [Obsolete("Use DisabledPatchesHashSet instead.")]
+        public static List<MethodBase> DisabledPatches { get; } = new List<MethodBase>();
+
+        /// <summary>
         /// Gets a set of types and methods for which EXILED patches should not be run.
         /// </summary>
-        public static HashSet<MethodBase> DisabledPatches { get; } = new HashSet<MethodBase>();
+        public static HashSet<MethodBase> DisabledPatchesHashSet { get; } = new HashSet<MethodBase>();
 
         /// <inheritdoc/>
         public override PluginPriority Priority { get; } = PluginPriority.First;
@@ -89,6 +97,7 @@ namespace Exiled.Events
 
             Unpatch();
 
+            DisabledPatchesHashSet.Clear();
             DisabledPatches.Clear();
 
             Handlers.Server.WaitingForPlayers -= round.OnWaitingForPlayers;
@@ -128,6 +137,11 @@ namespace Exiled.Events
         public void ReloadDisabledPatches()
         {
             foreach (MethodBase method in DisabledPatches)
+            {
+                DisabledPatchesHashSet.Add(method);
+            }
+
+            foreach (MethodBase method in DisabledPatchesHashSet)
             {
                 Harmony.Unpatch(method, HarmonyPatchType.All, Harmony.Id);
                 Log.Info($"Unpatched {method.Name}");
