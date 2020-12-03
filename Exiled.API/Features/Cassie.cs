6ec5ec8d7f3084add73462a511a67edbf3f64d0b
Thundermaker300 <thundermaker300@gmail.com>
2020-12-03 14:48:25 -0500

    Adding two new methods in Cassie class to announce with a % chance of glitching/jamming

--- Exiled.API/Features/Cassie.cs
+++ Exiled.API/Features/Cassie.cs
@@ -17,12 +17,12 @@ namespace Exiled.API.Features
     public static class Cassie
     {
         /// <summary>
-        /// Gets a value indicating whether or not a C.A.S.S.I.E announcement is currently playing. Does not include decontamination messages.
+        /// Gets a value indicating whether or not C.A.S.S.I.E is currently announcing. Does not include decontamination messages.
         /// </summary>
         public static bool IsSpeaking => !NineTailedFoxAnnouncer.singleton.Free;
 
         /// <summary>
-        /// Reproduce a C.A.S.S.I.E message.
+        /// Reproduce a non-glitched C.A.S.S.I.E message.
         /// </summary>
         /// <param name="message">The message to be reproduced.</param>
         /// <param name="isHeld">Indicates whether C.A.S.S.I.E has to hold the message.</param>
@@ -31,7 +31,16 @@ namespace Exiled.API.Features
             RespawnEffectsController.PlayCassieAnnouncement(message, isHeld, isNoisy);
 
         /// <summary>
-        /// Reproduce a C.A.S.S.I.E message after a certain amount of seconds.
+        /// Reproduce a glitchy C.A.S.S.I.E announcement.
+        /// </summary>
+        /// <param name="message">The message to be reproduced.</param>
+        /// <param name="glitchChance">The chance of placing a glitch between each word.</param>
+        /// <param name="jamChance">The chance of jamming each word.</param>
+        public static void GlitchyMessage(string message, float glitchChance, float jamChance) =>
+            NineTailedFoxAnnouncer.singleton.ServerOnlyAddGlitchyPhrase(message, glitchChance, jamChance);
+
+        /// <summary>
+        /// Reproduce a non-glitched C.A.S.S.I.E message after a certain amount of seconds.
         /// </summary>
         /// <param name="message">The message to be reproduced.</param>
         /// <param name="delay">The seconds that have to pass before reproducing the message.</param>
@@ -39,6 +48,16 @@ namespace Exiled.API.Features
         /// <param name="isNoisy">Indicates whether C.A.S.S.I.E has to make noises or not during the message.</param>
         public static void DelayedMessage(string message, float delay, bool isHeld = false, bool isNoisy = true) =>
             Timing.CallDelayed(delay, () => RespawnEffectsController.PlayCassieAnnouncement(message, isHeld, isNoisy));
+
+        /// <summary>
+        /// Reproduce a glitchy C.A.S.S.I.E announcement after a certain period of seconds.
+        /// </summary>
+        /// <param name="message">The message to be reproduced.</param>
+        /// <param name="delay">The seconds that have to pass before reproducing the message.</param>
+        /// <param name="glitchChance">The chance of placing a glitch between each word.</param>
+        /// <param name="jamChance">The chance of jamming each word.</param>
+        public static void DelayedGlitchyMessage(string message, float delay, float glitchChance, float jamChance) =>
+            Timing.CallDelayed(delay, () => NineTailedFoxAnnouncer.singleton.ServerOnlyAddGlitchyPhrase(message, glitchChance, jamChance);
 
         /// <summary>
         /// Calculates duration of a C.A.S.S.I.E message.
