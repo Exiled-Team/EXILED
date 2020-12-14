b6acb116b1a668cfacbae2756ad3406ff0dd60d3
Nick Rebbok <irebbok@outlook.com>
2020-12-14 09:47:44 +0300

    Update Exiled.Example/Commands/Test.cs

--- Exiled.Example/Commands/Test.cs
+++ Exiled.Example/Commands/Test.cs
@@ -38,7 +38,7 @@ namespace Exiled.Example.Commands
             else
                 response = "The command has been sent from the server console!";
 
-            // Return true if command executed successfully, otherwise return false.
+            // Return true if the command was executed successfully; otherwise, false.
             return true;
         }
     }
