cd6557d3411584c929b2178c977a17d808c469b6
PintTheDragon <64887885+PintTheDragon@users.noreply.github.com>
2020-12-13 22:45:21 -0800

    Add 106 to entering pocket dimension (#351)
    * Add 106 to entering pocket dimension 1/3
    
    * Add 106 to entering pocket dimension 2/2
    
    * Nice spelling
    
    * Oh my god stylecop
    
    * Update EnteringPocketDimensionEventArgs.cs

--- Exiled.Events/EventArgs/EnteringPocketDimensionEventArgs.cs
+++ Exiled.Events/EventArgs/EnteringPocketDimensionEventArgs.cs
@@ -23,11 +23,13 @@ namespace Exiled.Events.EventArgs
         /// </summary>
         /// <param name="player"><inheritdoc cref="Player"/></param>
         /// <param name="position"><inheritdoc cref="Position"/></param>
+        /// <param name="scp106"><inheritdoc cref="Scp106"/></param>
         /// <param name="isAllowed"><inheritdoc cref="IsAllowed"/></param>
-        public EnteringPocketDimensionEventArgs(Player player, Vector3 position, bool isAllowed = true)
+        public EnteringPocketDimensionEventArgs(Player player, Vector3 position, Player scp106, bool isAllowed = true)
         {
             Player = player;
             Position = position;
+            Scp106 = scp106;
             IsAllowed = isAllowed;
         }
 
@@ -35,6 +37,11 @@ namespace Exiled.Events.EventArgs
         /// Gets the player who's entering the pocket dimension.
         /// </summary>
         public Player Player { get; }
+
+        /// <summary>
+        /// Gets the SCP-106 who sent the player to the pocket dimension.
+        /// </summary>
+        public Player Scp106 { get; }
 
         /// <summary>
         /// Gets or sets the pocket dimension position.
