0d657c7ba2030ed33c306c180a77c8e57c0e9b2b
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-12-22 13:45:39 -0500

    Add files via upload

---
+++ Exiled.API/Enums/Recontain079Status.cs
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------
+// <copyright file="RoomType.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Enums
+{
+    /// <summary>
+    /// Different stages of 079's recontainment
+    /// </summary>
+    public enum Recontain079Status
+    {
+        Initialize,
+        Start,
+        Finished
+    }
+}
