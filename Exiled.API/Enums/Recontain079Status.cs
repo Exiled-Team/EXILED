7638eaa1acbf589a3609916e574e3a764dfe013a
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-12-22 13:53:35 -0500

    Add files via upload

---
+++ Exiled.API/Enums/Recontain079Status.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Recontain079Status.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Exiled.API.Enums
+{
+    /// <summary>
+    /// Stages for SCP079's recontainment.
+    /// </summary>
+    public enum Recontain079Status
+    {
+        Initiated,
+        Start,
+        End,
+    }
+}
