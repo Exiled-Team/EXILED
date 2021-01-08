13b624a5eb6cc0a247abcc866b2ba4ac3615fa49
Pietro <iopietro@hotmail.it>
2021-01-09 00:04:55 +0100

    Fixed a typo

--- Exiled.Events/EventArgs/DestroyingEventArgs.cs
+++ Exiled.Events/EventArgs/DestroyingEventArgs.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="DestoryingEventArgs.cs" company="Exiled Team">
+// <copyright file="DestroyingEventArgs.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -14,13 +14,13 @@ namespace Exiled.Events.EventArgs
     /// <summary>
     /// Contains all informations before a player's object is destroyed.
     /// </summary>
-    public class DestoryingEventArgs : EventArgs
+    public class DestroyingEventArgs : EventArgs
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="DestoryingEventArgs"/> class.
+        /// Initializes a new instance of the <see cref="DestroyingEventArgs"/> class.
         /// </summary>
         /// <param name="player"><inheritdoc cref="Player"/></param>
-        public DestoryingEventArgs(Player player) => Player = player;
+        public DestroyingEventArgs(Player player) => Player = player;
 
         /// <summary>
         /// Gets the destoying player.
