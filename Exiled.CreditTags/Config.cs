1a9237dc966147e4f0a085a3e6b924ed99bf58dc
iRebbok <irebbok@outlook.com>
2021-02-22 21:33:34 +0300

    set Badge as default mode value

--- Exiled.CreditTags/Config.cs
+++ Exiled.CreditTags/Config.cs
@@ -20,7 +20,7 @@ namespace Exiled.CreditTags
         public bool IsEnabled { get; set; } = true;
 
         [Description("Info side - Badge, CustomPlayerInfo, Both")]
-        public InfoSide Mode { get; private set; } = InfoSide.Both;
+        public InfoSide Mode { get; private set; } = InfoSide.Badge;
 
         [Description("Overrides badge if exists")]
         public bool BadgeOverride { get; private set; } = false;
