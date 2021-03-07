140050cedb620f59ee4ef8658cc085d72290dbbb
galaxy119 <amathor929@gmail.com>
2021-03-07 09:48:08 -0800

    New event: Map.ChangingIntoGrenade (my first transpiler <3) (#440)
    * New event: Map.ChangingIntoGrenade (my first transpiler <3)
    
    * Back to for loop brrr
    
    * And address the review stuff
    
    * consistency go brr
    
    * Added fuseTime to constructor. Removed unused default property values.

---
+++ Exiled.Events/EventArgs/ChangingIntoGrenadeEventArgs.cs
@@ -0,0 +1,52 @@
+// -----------------------------------------------------------------------
+// <copyright file="ChangingIntoGrenadeEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs
+{
+    using System;
+    using Exiled.API.Enums;
+
+    /// <summary>
+    /// Contains all information for when the server is turning a pickup into a live grenade.
+    /// </summary>
+    public class ChangingIntoGrenadeEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangingIntoGrenadeEventArgs"/> class.
+        /// </summary>
+        /// <param name="pickup">The <see cref="Pickup"/> being changed.</param>
+        /// <param name="fuseTime">The duration, in seconds, of the fuse time to be used when the grenade goes live.</param>
+        /// <param name="isAllowed">Whether or not the event is allowed to continue.</param>
+        public ChangingIntoGrenadeEventArgs(Pickup pickup, float fuseTime = 3f, bool isAllowed = true)
+        {
+            Pickup = pickup;
+            Type = pickup.itemId;
+            FuseTime = fuseTime;
+            IsAllowed = isAllowed;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the pickup being changed.
+        /// </summary>
+        public Pickup Pickup { get; }
+
+        /// <summary>
+        /// Gets or sets a value indicating what type of grenade will be spawned.
+        /// </summary>
+        public ItemType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the pickup will be changed.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating how long the fuse of the changed grenade will be.
+        /// </summary>
+        public float FuseTime { get; set; }
+    }
+}
