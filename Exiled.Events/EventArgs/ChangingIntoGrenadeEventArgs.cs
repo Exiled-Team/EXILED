f85252f70500ab758b16eaf2367f3184b9d42588
galaxy119 <amathor929@gmail.com>
2021-03-07 08:11:41 -0800

    New event: Map.ChangingIntoGrenade (my first transpiler <3)

---
+++ Exiled.Events/EventArgs/ChangingIntoGrenadeEventArgs.cs
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------
+// <copyright file="ChangingIntoGrenadeEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs
+{
+    using System;
+    using Exiled.API.Enums;
+
+    /// <summary>
+    /// Contains all information for when the server is turning a pickup into a live grenade.
+    /// </summary>
+    public class ChangingIntoGrenadeEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangingIntoGrenadeEventArgs"/> class.
+        /// </summary>
+        /// <param name="pickup">The <see cref="Pickup"/> being changed.</param>
+        public ChangingIntoGrenadeEventArgs(Pickup pickup)
+        {
+            Pickup = pickup;
+            Type = pickup.itemId;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the pickup being changed.
+        /// </summary>
+        public Pickup Pickup { get; }
+
+        /// <summary>
+        /// Gets or sets a value indicating what type of grenade will be spawned.
+        /// </summary>
+        public ItemType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the pickup will be changed.
+        /// </summary>
+        public bool IsAllowed { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating how long the fuse of the changed grenade will be.
+        /// </summary>
+        public float FuseTime { get; set; } = 3f;
+    }
+}
