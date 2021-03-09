79f6795cc3301ca8ea180e3450e3a911ba04ea49
galaxy119 <amathor929@gmail.com>
2021-03-09 01:53:52 -0800

    de-duplication is nice

--- Exiled.Events/Patches/Fixes/Scp096FailEnrageFix.cs
+++ Exiled.Events/Patches/Fixes/Scp096FailEnrageFix.cs
@@ -9,6 +9,7 @@ namespace Exiled.Events.Patches.Fixes
 {
 #pragma warning disable SA1118
 #pragma warning disable SA1402
+#pragma warning disable SA1649
     using System.Collections.Generic;
     using System.Reflection.Emit;
     using HarmonyLib;
@@ -18,14 +19,19 @@ namespace Exiled.Events.Patches.Fixes
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// patches <see cref="Scp096.PreWindup"/>.
+    /// Handles building new instructions.
     /// </summary>
-    [HarmonyPatch(typeof(Scp096), nameof(Scp096.PreWindup))]
-    internal static class Scp096FailEnrageFix
+    internal static class InstructionBuilder
     {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        /// <summary>
+        /// Builds new instructions for enrage fix transpilers.
+        /// </summary>
+        /// <param name="instructions"><see cref="CodeInstruction"/>.</param>
+        /// <param name="generator"><see cref="ILGenerator"/>.</param>
+        /// <returns>New <see cref="CodeInstruction"/>.</returns>
+        internal static IEnumerable<CodeInstruction> FixInstructions(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+            List<CodeInstruction> newInstructions = new List<CodeInstruction>(instructions);
 
             const int offset = 1;
 
@@ -43,6 +49,20 @@ namespace Exiled.Events.Patches.Fixes
             });
 
             newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+
+            return newInstructions;
+        }
+    }
+
+    /// <summary>
+    /// patches <see cref="Scp096.PreWindup"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp096), nameof(Scp096.PreWindup))]
+    internal static class Scp096FailEnrageFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(InstructionBuilder.FixInstructions(instructions, generator));
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
@@ -59,24 +79,7 @@ namespace Exiled.Events.Patches.Fixes
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
-
-            const int offset = 1;
-
-            // Find the only "throw" call
-            int index = newInstructions.FindLastIndex(instruction => instruction.opcode == OpCodes.Throw) + offset;
-
-            Label returnLabel = generator.DefineLabel();
-
-            newInstructions.InsertRange(index, new[]
-            {
-                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
-                new CodeInstruction(OpCodes.Ldfld, Field(typeof(Scp096), nameof(Scp096._targets))),
-                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Count))),
-                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
-            });
-
-            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(InstructionBuilder.FixInstructions(instructions, generator));
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
