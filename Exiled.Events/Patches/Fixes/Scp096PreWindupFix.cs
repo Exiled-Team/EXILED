6727dfaa8fde71e946d704762ed24d19ddba2410
galaxy119 <amathor929@gmail.com>
2021-03-09 01:33:35 -0800

    I'm not tinybrain I promise.

--- Exiled.Events/Patches/Fixes/Scp096PreWindupFix.cs
+++ Exiled.Events/Patches/Fixes/Scp096PreWindupFix.cs
@@ -39,8 +39,43 @@ namespace Exiled.Events.Patches.Fixes
                 new CodeInstruction(OpCodes.Ldfld, Field(typeof(Scp096), nameof(Scp096._targets))),
                 new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Count))),
                 new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
-                new CodeInstruction(OpCodes.Ret).WithLabels(returnLabel),
             });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+
+    /// <summary>
+    /// Patches <see cref="Scp096.Windup"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp096), nameof(Scp096.Windup))]
+    internal static class Scp096WindupFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent();
+
+            const int offset = 1;
+
+            // Find the only "throw" call
+            int index = newInstructions.FindLastIndex(instruction => instruction.opcode == OpCodes.Throw) + offset;
+
+            Label returnLabel = generator.DefineLabel();
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(Scp096), nameof(Scp096._targets))),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Count))),
+                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
+            });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
