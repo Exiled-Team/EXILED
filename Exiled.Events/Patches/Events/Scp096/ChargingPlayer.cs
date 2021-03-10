5b4e62876ad87c20dd11673c82043a179596fdd2
Steven Webster <steven@is-just-a.dev>
2021-03-10 17:58:18 -0500

    Change ChargingPlayer and TryingNotToCry into transpilers, Fix ChargingPlayer
    ChargingPlayer was calling the event once per frame, now it will call once per player.

--- Exiled.Events/Patches/Events/Scp096/ChargingPlayer.cs
+++ Exiled.Events/Patches/Events/Scp096/ChargingPlayer.cs
@@ -7,18 +7,25 @@
 
 namespace Exiled.Events.Patches.Events.Scp096
 {
-#pragma warning disable SA1129
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+#pragma warning disable SA1118
 #pragma warning disable SA1313
+#pragma warning disable SA1402
     using Exiled.Events.EventArgs;
 
     using HarmonyLib;
 
     using Mirror;
 
+    using NorthwoodLib.Pools;
+
     using PlayableScps;
     using PlayableScps.Messages;
 
     using UnityEngine;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="Scp096.ChargePlayer"/>.
@@ -27,38 +34,89 @@ namespace Exiled.Events.Patches.Events.Scp096
     [HarmonyPatch(typeof(Scp096), nameof(Scp096.ChargePlayer))]
     internal static class ChargingPlayer
     {
-        private static readonly int ChargingMask = LayerMask.GetMask("Default", "Door", "Glass");
+        /// <summary>
+        /// The hashset of already charged players.
+        /// Prevents double calling on the same player.
+        /// </summary>
+        public static readonly HashSet<ReferenceHub> ChargedPlayers = new HashSet<ReferenceHub>();
 
-        private static bool Prefix(Scp096 __instance, ReferenceHub player)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (player.characterClassManager.IsAnyScp())
-                return false;
+            var newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            if (Physics.Linecast(__instance.Hub.transform.position, player.transform.position, ChargingMask))
-                return false;
+            // The index offset.
+            const int offset = 1;
 
-            bool isTarget = __instance._targets.Contains(player);
+            // Search for last "lfsfld".
+            var index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stloc_0) + offset;
 
-            var ev = new ChargingPlayerEventArgs(__instance, API.Features.Player.Get(__instance.Hub), API.Features.Player.Get(player), isTarget, isTarget ? 9696f : 35f, isTarget);
+            // Get the count to find the previous index
+            var oldCount = newInstructions.Count;
 
-            Handlers.Scp096.OnChargingPlayer(ev);
+            // Get the return label from the last instruction.
+            var returnLabel = newInstructions[newInstructions.Count - 1].labels[0];
 
-            if (ev.IsAllowed)
+            // Create the damage labels.
+            var damageLabel1 = generator.DefineLabel();
+            var damageLabel2 = generator.DefineLabel();
+
+            newInstructions.InsertRange(index, new[]
             {
-                if (__instance.Hub.playerStats.HurtPlayer(new PlayerStats.HitInfo(ev.Damage, player.LoggedNameFromRefHub(), DamageTypes.Scp096, __instance.Hub.queryProcessor.PlayerId), player.gameObject))
-                {
-                    __instance._targets.Remove(player);
-                    NetworkServer.SendToClientOfPlayer(__instance.Hub.characterClassManager.netIdentity, new Scp096HitmarkerMessage(1.35f));
-                    NetworkServer.SendToAll(new Scp096OnKillMessage());
-                }
+                new CodeInstruction(OpCodes.Ldsfld, Field(typeof(ChargingPlayer), nameof(ChargedPlayers))),
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Callvirt, Method(typeof(HashSet<ReferenceHub>), nameof(ChargedPlayers.Add))),
+                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayableScp), nameof(PlayableScp.Hub))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Brtrue_S, damageLabel1),
+                new CodeInstruction(OpCodes.Ldc_R4, 35f),
+                new CodeInstruction(OpCodes.Br_S, damageLabel2),
+                new CodeInstruction(OpCodes.Ldc_R4, 9696f).WithLabels(damageLabel1),
+                new CodeInstruction(OpCodes.Ldloc_0).WithLabels(damageLabel2),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(ChargingPlayerEventArgs))[0]),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Handlers.Scp096), nameof(Handlers.Scp096.OnChargingPlayer))),
+                new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(ChargingPlayerEventArgs), nameof(ChargingPlayerEventArgs.IsAllowed))),
+                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
+            });
 
-                if (ev.EndCharge)
-                {
-                    __instance.EndChargeNextFrame();
-                }
-            }
+            newInstructions[index].MoveLabelsFrom(newInstructions[newInstructions.Count - oldCount + index]);
 
-            return false;
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+
+    /// <summary>
+    /// Patches <see cref="Scp096.EndCharge"/>.
+    /// Serves to clear the ChargedPlayers.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp096), nameof(Scp096.EndCharge))]
+    internal static class ChargeEnded
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            newInstructions.InsertRange(0, new[]
+            {
+                new CodeInstruction(OpCodes.Ldsfld, Field(typeof(ChargingPlayer), nameof(ChargingPlayer.ChargedPlayers))),
+                new CodeInstruction(OpCodes.Callvirt, Method(typeof(HashSet<ReferenceHub>), nameof(ChargingPlayer.ChargedPlayers.Clear))),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
--- Exiled.Events/Patches/Events/Scp096/TryingNotToCry.cs
+++ Exiled.Events/Patches/Events/Scp096/TryingNotToCry.cs
@@ -7,16 +7,22 @@
 
 namespace Exiled.Events.Patches.Events.Scp096
 {
-#pragma warning disable SA1313
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+#pragma warning disable SA1118
     using Exiled.Events.EventArgs;
 
     using HarmonyLib;
 
     using Interactables.Interobjects.DoorUtils;
 
+    using NorthwoodLib.Pools;
+
     using PlayableScps;
 
     using UnityEngine;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches the <see cref="Scp096.TryNotToCry"/> method.
@@ -25,28 +31,43 @@ namespace Exiled.Events.Patches.Events.Scp096
     [HarmonyPatch(typeof(Scp096), nameof(Scp096.TryNotToCry))]
     internal static class TryingNotToCry
     {
-        private static readonly int DoorMask = LayerMask.GetMask("Door");
-
-        private static bool Prefix(Scp096 __instance)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (!Physics.Raycast(__instance.Hub.PlayerCameraReference.position, __instance.Hub.PlayerCameraReference.forward, out RaycastHit hitInfo, 1f, DoorMask))
-                return false;
+            var newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            DoorVariant componentInParent = hitInfo.collider.gameObject.GetComponentInParent<DoorVariant>();
-            if (componentInParent == null || componentInParent.GetExactState() > 0.0 || Scp096._takenDoors.ContainsKey(__instance.Hub.gameObject))
-                return false;
+            // The index offset.
+            const int offset = 0;
 
-            var ev = new TryingNotToCryEventArgs(__instance, API.Features.Player.Get(__instance.Hub), componentInParent);
+            // Search for last "lfsfld".
+            var index = newInstructions.FindLastIndex(instruction => instruction.opcode == OpCodes.Ldsfld) + offset;
 
-            Handlers.Scp096.OnTryingNotToCry(ev);
+            // Get the count to find the previous index
+            var oldCount = newInstructions.Count;
 
-            if (ev.IsAllowed)
+            // Get the return label from the instruction.
+            var returnLabel = newInstructions[index - 1].labels[0];
+
+            newInstructions.InsertRange(index, new[]
             {
-                Scp096._takenDoors.Add(__instance.Hub.gameObject, componentInParent);
-                __instance.PlayerState = Scp096PlayerState.TryNotToCry;
-            }
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayableScp), nameof(PlayableScp.Hub))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Ldloc_1),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(TryingNotToCryEventArgs))[0]),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Handlers.Scp096), nameof(Handlers.Scp096.OnTryingNotToCry))),
+                new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(TryingNotToCryEventArgs), nameof(TryingNotToCryEventArgs.IsAllowed))),
+                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
+            });
 
-            return false;
+            newInstructions[index].MoveLabelsFrom(newInstructions[newInstructions.Count - oldCount + index]);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
