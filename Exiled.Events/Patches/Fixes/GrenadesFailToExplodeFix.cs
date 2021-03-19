2834cf504e166df730ca2019b78a21c90a167604
iRebbok <irebbok@outlook.com>
2021-03-19 21:11:21 +0300

    add some explanatory comments

--- Exiled.Events/Patches/Fixes/GrenadesFailToExplodeFix.cs
+++ Exiled.Events/Patches/Fixes/GrenadesFailToExplodeFix.cs
@@ -29,8 +29,11 @@ namespace Exiled.Events.Patches.Fixes
             {
                 yield return inst;
 
+                // Checks if it's a call to the Grenade._Fuse(float) method
+                // Then insterts the wrapper MECExtensionMethods2.CancelWith(IEnumerator<float>, GameObject)
                 if (inst.opcode == OpCodes.Call && (MethodInfo)inst.operand == AccessTools.Method(typeof(Grenade), nameof(Grenade._Fuse)))
                 {
+                    // MECExtensionMethods2.CancelWith(Grenade._Fuse(float), this.gameObject);
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
                     yield return new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(Component), nameof(Component.gameObject)));
                     yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MECExtensionMethods2), nameof(MECExtensionMethods2.CancelWith), new[] { typeof(IEnumerator<float>), typeof(GameObject) }));
