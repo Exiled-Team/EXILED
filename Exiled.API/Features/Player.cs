f489d20a1365b515b143f0c803e99bbc1462f9e3
green1052 <cydyellowgreen@gmail.com>
2021-05-24 12:09:26 +0900

    Add Player:Ping

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -25,6 +25,7 @@ namespace Exiled.API.Features
     using MEC;
 
     using Mirror;
+    using Mirror.LiteNetLib4Mirror;
 
     using NorthwoodLib;
     using NorthwoodLib.Pools;
@@ -1325,6 +1326,12 @@ namespace Exiled.API.Features
 
             HintDisplay.Show(new TextHint(message, parameters, null, duration));
         }
+
+        /// <summary>
+        /// Get player ping.
+        /// </summary>
+        /// <returns>Return player ping.</returns>
+        public int Ping() => LiteNetLib4MirrorServer.GetPing(Connection.connectionId);
 
         /// <summary>
         /// Gets a <see cref="bool"/> describing whether or not the given <see cref="PlayerEffect">status effect</see> is currently enabled.
