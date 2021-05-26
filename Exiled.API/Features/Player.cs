d3b69b256a41e1341a0444768d17b895eea5d812
Jesús <69375249+Jesus-QC@users.noreply.github.com>
2021-05-27 00:16:59 +0200

    Player.CountItem(ItemType) (#499)
    * Player.CountItem(ItemType)
    
    This is useful to count the items of a certain ItemType a player has in his inventory.
    
    Co-authored-by: Pietro <iopietro@hotmail.it>

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -1170,6 +1170,13 @@ namespace Exiled.API.Features
         }
 
         /// <summary>
+        /// Counts how many items of a certain <see cref="ItemType"/> a player has.
+        /// </summary>
+        /// <param name="item">The item to search for.</param>
+        /// <returns>How many items of that <see cref="ItemType"/> the player has.</returns>
+        public int CountItem(ItemType item) => Inventory.items.Count(inventoryItem => inventoryItem.id == item);
+
+        /// <summary>
         /// Removes an item from the player's inventory.
         /// </summary>
         /// <param name="item">The item to be removed.</param>
