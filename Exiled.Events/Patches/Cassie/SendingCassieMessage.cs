5ff5edf9a68b5350715e0a1cbaedec2af0b525d5
Jesús <69375249+Jesus-QC@users.noreply.github.com>
2021-06-24 03:47:07 +0200

    Exiled.Events - Cassie (#505)
    * Added C.A.S.S.I.E. related events.

---
+++ Exiled.Events/Patches/Cassie/SendingCassieMessage.cs
@@ -0,0 +1,34 @@
+// -----------------------------------------------------------------------
+// <copyright file="SendingCassieMessage.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Cassie
+{
+    using System;
+    using Exiled.Events.EventArgs;
+    using Exiled.Events.Handlers;
+    using HarmonyLib;
+
+    /// <summary>
+    /// Patches <see cref="Respawning.RespawnEffectsController.PlayCassieAnnouncement(string, bool, bool)"/>.
+    /// Adds the <see cref="Cassie.SendingCassieMessage"/> event.
+    /// </summary>
+    [HarmonyPatch(typeof(Respawning.RespawnEffectsController), nameof(Respawning.RespawnEffectsController.PlayCassieAnnouncement))]
+    internal static class SendingCassieMessage
+    {
+        private static bool Prefix(ref string words, ref bool makeHold, ref bool makeNoise)
+        {
+            var ev = new SendingCassieMessageEventArgs(words, makeHold, makeNoise);
+            Handlers.Cassie.OnSendingCassieMessage(ev);
+
+            words = ev.Words;
+            makeHold = ev.MakeHold;
+            makeNoise = ev.MakeNoise;
+
+            return ev.IsAllowed;
+        }
+    }
+}
