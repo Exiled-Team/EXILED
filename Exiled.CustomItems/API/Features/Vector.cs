3a5d66214cd6c9a849937358417c3c09279d5306
PintTheDragon <64887885+PintTheDragon@users.noreply.github.com>
2021-06-27 19:11:21 -0700

    Mark CustomItems Vector as obsolete

--- Exiled.CustomItems/API/Features/Vector.cs
+++ Exiled.CustomItems/API/Features/Vector.cs
@@ -16,6 +16,7 @@ namespace Exiled.CustomItems.API.Features
     /// <summary>
     /// A yaml-serializable vector object.
     /// </summary>
+    [Obsolete("Use EXILED.API.Features.ConfigVector3.", true)]
     public struct Vector
     {
         /// <summary>
