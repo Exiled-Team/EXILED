4464b3626d2bb7df30b9a60849f8effd6d1248d5
Nick Rebbok <irebbok@outlook.com>
2021-07-03 00:33:28 +0300

    apple suggestion - avoid printing exception twice
    Co-authored-by: Pietro <iopietro@hotmail.it>

--- Exiled.Events/Patches/Events/Player/ChangingItem.cs
+++ Exiled.Events/Patches/Events/Player/ChangingItem.cs
@@ -57,7 +57,7 @@ namespace Exiled.Events.Patches.Events.Player
             }
             catch (Exception e)
             {
-                Exiled.API.Features.Log.Error($"Exiled.Events.Patches.Events.Player.ChangingItem: {e}\n{e.ToStringDemystified()}");
+                Exiled.API.Features.Log.Error($"Exiled.Events.Patches.Events.Player.ChangingItem:\n{e.ToStringDemystified()}");
             }
         }
     }
