53ed1daa231687ba295994a7755fd5250bfa883c
xRoier <rogerazomar@gmail.com>
2021-07-04 04:21:23 +0200

    Converted DamagingWindow into a transpiler

--- Exiled.Events/Patches/Events/Map/DamagingWindow.cs
+++ Exiled.Events/Patches/Events/Map/DamagingWindow.cs
@@ -7,9 +7,16 @@
 
 namespace Exiled.Events.Patches.Events.Map
 {
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using Mirror;
+    using NorthwoodLib.Pools;
+    using static HarmonyLib.AccessTools;
+
+#pragma warning disable SA1118 // The parameter spans multiple lines
     using Exiled.Events.EventArgs;
     using Exiled.Events.Handlers;
-#pragma warning disable SA1313
     using HarmonyLib;
 
     /// <summary>
@@ -19,13 +26,37 @@ namespace Exiled.Events.Patches.Events.Map
     [HarmonyPatch(typeof(BreakableWindow), nameof(BreakableWindow.ServerDamageWindow))]
     internal static class DamagingWindow
     {
-        private static void Prefix(BreakableWindow __instance, ref float damage)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            var ev = new DamagingWindowEventArgs(__instance, damage);
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            Map.OnDamagingWindow(ev);
+            var offset = 1;
+            var index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret) + offset;
 
-            damage = ev.Damage;
+            newInstructions.InsertRange(index, new[]
+            {
+                // this
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+
+                // damage
+                new CodeInstruction(OpCodes.Ldarg_1),
+
+                // var ev = new DamagingWindowEventArgs(this, damage);
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(DamagingWindowEventArgs))[0]),
+                new CodeInstruction(OpCodes.Dup),
+
+                // Map.OnDamagingWindow(ev);
+                new CodeInstruction(OpCodes.Call, Method(typeof(Map), nameof(Map.OnDamagingWindow))),
+
+                // damage = ev.Damage;
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(DamagingWindowEventArgs), nameof(DamagingWindowEventArgs.Damage))),
+                new CodeInstruction(OpCodes.Starg_S, 1),
+            });
+
+            for (int i = 0; i < newInstructions.Count; i++)
+                yield return newInstructions[i];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
