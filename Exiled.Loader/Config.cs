35569bb4f11e54f4df553b1eab748f8333b4bcf2
Joker <amathor929@gmail.com>
2021-08-21 12:10:30 -0700

    smol hotfix

--- Exiled.Loader/Config.cs
+++ Exiled.Loader/Config.cs
@@ -31,6 +31,6 @@ namespace Exiled.Loader
         /// Gets or sets the environment type.
         /// </summary>
         [Description("The working environment type (Development, Testing, Production, Ptb)")]
-        public EnvironmentType Environment { get; set; } = EnvironmentType.Production;
+        public EnvironmentType Environment { get; set; } = EnvironmentType.Ptb;
     }
 }
