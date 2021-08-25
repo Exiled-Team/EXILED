48c54fc3c0cc9b0043d95783900f13a5f98c4f97
Joker <amathor929@gmail.com>
2021-08-25 10:22:52 -0700

    Another possible lockup fix

---
+++ Exiled.Events/Patches/Fixes/PlayableScpControllerPatch.cs
@@ -0,0 +1,25 @@
+// -----------------------------------------------------------------------
+// <copyright file="PlayableScpControllerPatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Fixes
+{
+    using HarmonyLib;
+
+    /// <summary>
+    /// Patches another round lock bug.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayableScpsController), nameof(PlayableScpsController.Update))]
+    internal class PlayableScpControllerPatch
+    {
+        private static bool Prefox()
+        {
+            if (ReferenceHub.LocalHub == null)
+                return false;
+            return true;
+        }
+    }
+}
