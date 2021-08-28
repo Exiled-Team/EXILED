3305c488fb5c223b9eeceee2c28a2537307d4cbd
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-08-28 15:00:32 -0500

    i didn't want to accident pr this ah fuck

--- Exiled.Events/Patches/Events/Player/ChangingMicroHIDState.cs
+++ Exiled.Events/Patches/Events/Player/ChangingMicroHIDState.cs
@@ -5,45 +5,18 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-using System;
-
 namespace Exiled.Events.Patches.Events.Player
 {
 #pragma warning disable SA1313
 
-    using Exiled.Events.EventArgs;
-    using Exiled.Events.Handlers;
-
-    using HarmonyLib;
     using InventorySystem.Items.MicroHID;
 
     /// <summary>
     /// Patches <see cref="MicroHIDItem.State"/>.
     /// Adds the <see cref="Handlers.Player.OnChangingMicroHIDState"/> event.
     /// </summary>
-    [HarmonyPatch(typeof(MicroHIDItem), nameof(MicroHIDItem.ExecuteServerside))]
     internal static class ChangingMicroHIDState
     {
-        private static bool Prefix(MicroHIDItem __instance, ref byte value)
-        {
-            try
-            {
-                ChangingMicroHidStateEventArgs ev = new ChangingMicroHidStateEventArgs(API.Features.Player.Get(__instance.Owner), __instance, HidState.Idle, HidState.PoweringUp, true);
-
-                Player.OnChangingMicroHIDState(ev);
-
-                if (!ev.IsAllowed)
-                {
-                    return false;
-                }
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                API.Features.Log.Error($"{typeof(ChangingMicroHidStateEventArgs).FullName}.{nameof(Prefix)}:\n{e}");
-                return true;
-            }
-        }
+        // TODO: this
     }
 }
