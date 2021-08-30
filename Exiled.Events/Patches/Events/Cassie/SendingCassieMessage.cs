1daa2fd4784f6c59aa7f1e71f9d2c505f79fb912
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-08-29 22:39:21 -0500

    Converted SendingCassieMessage to Transpiler

--- Exiled.Events/Patches/Events/Cassie/SendingCassieMessage.cs
+++ Exiled.Events/Patches/Events/Cassie/SendingCassieMessage.cs
@@ -7,9 +7,15 @@
 
 namespace Exiled.Events.Patches.Events.Cassie
 {
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
     using Exiled.Events.EventArgs;
     using Exiled.Events.Handlers;
+
     using HarmonyLib;
+
+    using NorthwoodLib.Pools;
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="Respawning.RespawnEffectsController.PlayCassieAnnouncement(string, bool, bool)"/>.
@@ -18,16 +24,44 @@ namespace Exiled.Events.Patches.Events.Cassie
     [HarmonyPatch(typeof(Respawning.RespawnEffectsController), nameof(Respawning.RespawnEffectsController.PlayCassieAnnouncement))]
     internal static class SendingCassieMessage
     {
-        private static bool Prefix(ref string words, ref bool makeHold, ref bool makeNoise)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            SendingCassieMessageEventArgs ev = new SendingCassieMessageEventArgs(words, makeHold, makeNoise);
-            Cassie.OnSendingCassieMessage(ev);
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            words = ev.Words;
-            makeHold = ev.MakeHold;
-            makeNoise = ev.MakeNoise;
+            var index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ret) - 1;
 
-            return ev.IsAllowed;
+            var returnLabel = generator.DefineLabel();
+
+            newInstructions.InsertRange(index, new[]
+            {
+                // string words
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+
+                // bool makeHold
+                new CodeInstruction(OpCodes.Ldarg_1),
+
+                // bool makeNoise
+                new CodeInstruction(OpCodes.Ldarg_2),
+
+                // var ev = SendingCassieMessageEventArgs(...)
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(SendingCassieMessageEventArgs))[0]),
+                new CodeInstruction(OpCodes.Dup),
+
+                // Cassie.SendingCassieMessageEventArgs(ev);
+                new CodeInstruction(OpCodes.Call, Method(typeof(Cassie), nameof(Cassie.OnSendingCassieMessage))),
+
+                // if (!ev.IsAllowed) return;
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(SendingCassieMessageEventArgs), nameof(SendingCassieMessageEventArgs.IsAllowed))),
+                new CodeInstruction(OpCodes.Brfalse_S, returnLabel),
+            });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+
+            for (var z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
