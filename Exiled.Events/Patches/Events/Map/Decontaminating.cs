8c71d97598dda9160721d34333db53f65c4bfce0
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-08-30 07:05:48 +0200

    tranzpiler

--- Exiled.Events/Patches/Events/Map/Decontaminating.cs
+++ Exiled.Events/Patches/Events/Map/Decontaminating.cs
@@ -46,8 +46,8 @@ namespace Exiled.Events.Patches.Events.Map
                 new CodeInstruction(OpCodes.Brfalse, returnLabel),
             });
 
-            for (int i = 0; i < newInstructions.Count; i++)
-                yield return newInstructions[i];
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
 
             ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
