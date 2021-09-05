05096d228506f1abb6c92eb3e039e77a6f6b0cbf
Steven Webster <steven@is-just-a.dev>
2021-09-04 20:45:56 -0400

    Remove unnecessary using

--- Exiled.Events/Patches/Events/Player/InteractingShootingTarget.cs
+++ Exiled.Events/Patches/Events/Player/InteractingShootingTarget.cs
@@ -11,7 +11,6 @@ namespace Exiled.Events.Patches.Events.Player
 #pragma warning disable SA1313
 #pragma warning disable SA1402
     using System.Collections.Generic;
-    using System.Reflection;
     using System.Reflection.Emit;
 
     using Exiled.API.Features;
