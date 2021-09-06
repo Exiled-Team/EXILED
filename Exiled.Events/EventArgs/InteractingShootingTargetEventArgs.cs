ffe27af48506bc4191dcbd4468a2dd26e012e91c
Steven Webster <steven@is-just-a.dev>
2021-09-06 14:28:50 -0400

    Fixed InteractingShootingTarget event.
    It will now actually work and sync new values correctly.

--- Exiled.Events/EventArgs/InteractingShootingTargetEventArgs.cs
+++ Exiled.Events/EventArgs/InteractingShootingTargetEventArgs.cs
@@ -12,25 +12,33 @@ namespace Exiled.Events.EventArgs
     using Exiled.API.Enums;
     using Exiled.API.Features;
 
+    using UnityEngine;
+
     /// <summary>
     /// Contains all information before a player interacts with a shooting target.
     /// </summary>
     public class InteractingShootingTargetEventArgs : EventArgs
     {
         private bool isAllowed;
+        private int maxHp;
+        private int autoResetTime;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="InteractingShootingTargetEventArgs"/> class.
         /// </summary>
         /// <param name="player"><inheritdoc cref="Player"/></param>
-        /// /// <param name="shootingTarget"><inheritdoc cref="ShootingTarget"/></param>
-        /// /// <param name="targetButton"><inheritdoc cref="TargetButton"/></param>
+        /// <param name="shootingTarget"><inheritdoc cref="ShootingTarget"/></param>
+        /// <param name="targetButton"><inheritdoc cref="TargetButton"/></param>
+        /// <param name="maxHp"><inheritdoc cref="MaxHp"/></param>
+        /// <param name="autoResetTime"><inheritdoc cref="AutoResetTime"/></param>
         /// <param name="isAllowed"><inheritdoc cref="IsAllowed"/></param>
-        public InteractingShootingTargetEventArgs(Player player, InventorySystem.Items.Firearms.Utilities.ShootingTarget shootingTarget, ShootingTargetButton targetButton, bool isAllowed = true)
+        public InteractingShootingTargetEventArgs(Player player, InventorySystem.Items.Firearms.Utilities.ShootingTarget shootingTarget, ShootingTargetButton targetButton, int maxHp, int autoResetTime, bool isAllowed = true)
         {
             Player = player;
             ShootingTarget = ShootingTarget.Get(shootingTarget);
             TargetButton = targetButton;
+            this.maxHp = maxHp;
+            this.autoResetTime = autoResetTime;
             this.isAllowed = isAllowed;
         }
 
@@ -48,6 +56,34 @@ namespace Exiled.Events.EventArgs
         /// Gets the button the player interacted with.
         /// </summary>
         public ShootingTargetButton TargetButton { get; }
+
+        /// <summary>
+        /// Gets or sets the max hp of the target.
+        /// </summary>
+        public int MaxHp
+        {
+            get => maxHp;
+            set
+            {
+                if (!ShootingTarget.IsSynced)
+                    throw new InvalidOperationException("Attempted to set MaxHp while target is in local mode. Set target's IsSynced to true before setting IsAllowed.");
+                maxHp = Mathf.Clamp(value, 1, 256);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the auto reset time of the target.
+        /// </summary>
+        public int AutoResetTime
+        {
+            get => autoResetTime;
+            set
+            {
+                if (!ShootingTarget.IsSynced)
+                    throw new InvalidOperationException("Attempted to set AutoResetTime while target is in local mode. Set target's IsSynced to true before setting IsAllowed.");
+                autoResetTime = Mathf.Clamp(value, 0, 10);
+            }
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether or not the interaction is allowed.
--- Exiled.Events/Patches/Events/Player/InteractingShootingTarget.cs
+++ Exiled.Events/Patches/Events/Player/InteractingShootingTarget.cs
@@ -9,6 +9,8 @@ namespace Exiled.Events.Patches.Events.Player
 {
 #pragma warning disable SA1118
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using System.Reflection.Emit;
 
     using Exiled.API.Features;
@@ -16,15 +18,21 @@ namespace Exiled.Events.Patches.Events.Player
 
     using HarmonyLib;
 
+    using Mirror;
+
     using NorthwoodLib.Pools;
+
+    using UnityEngine;
 
     using static HarmonyLib.AccessTools;
 
+    using BaseTarget = InventorySystem.Items.Firearms.Utilities.ShootingTarget;
+
     /// <summary>
-    /// Patches <see cref="InventorySystem.Items.Firearms.Utilities.ShootingTarget.ServerInteract(ReferenceHub, byte)"/>.
+    /// Patches <see cref="BaseTarget.ServerInteract(ReferenceHub, byte)"/>.
     /// Adds the <see cref="Handlers.Player.InteractingShootingTarget"/> event.
     /// </summary>
-    [HarmonyPatch(typeof(InventorySystem.Items.Firearms.Utilities.ShootingTarget), nameof(InventorySystem.Items.Firearms.Utilities.ShootingTarget.ServerInteract))]
+    [HarmonyPatch(typeof(BaseTarget), nameof(BaseTarget.ServerInteract))]
     internal static class InteractingShootingTarget
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
@@ -35,8 +43,13 @@ namespace Exiled.Events.Patches.Events.Player
             int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ldarg_2) + offset;
 
             Label returnLabel = generator.DefineLabel();
+            Label checkMaxHpLabel = generator.DefineLabel();
+            Label checkAutoResetTimeLabel = generator.DefineLabel();
+            Label rpcLabel = generator.DefineLabel();
 
             newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+
+            LocalBuilder ev = generator.DeclareLocal(typeof(InteractingShootingTargetEventArgs));
 
             newInstructions.InsertRange(index, new[]
             {
@@ -47,21 +60,93 @@ namespace Exiled.Events.Patches.Events.Player
 
                 new CodeInstruction(OpCodes.Ldarg_2),
 
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(BaseTarget), nameof(BaseTarget._maxHp))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(InteractingShootingTarget), nameof(InteractingShootingTarget.GetNextValue))),
+
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Ldc_I4_3),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(BaseTarget), nameof(BaseTarget._autoDestroyTime))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(InteractingShootingTarget), nameof(InteractingShootingTarget.GetNextValue))),
+
                 new CodeInstruction(OpCodes.Ldc_I4_1),
 
                 new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(InteractingShootingTargetEventArgs))[0]),
                 new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Stloc_S, ev.LocalIndex),
 
                 new CodeInstruction(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnInteractingShootingTarget))),
 
                 new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(InteractingShootingTargetEventArgs), nameof(InteractingShootingTargetEventArgs.IsAllowed))),
-                new CodeInstruction(OpCodes.Brfalse, returnLabel),
+                new CodeInstruction(OpCodes.Brfalse, checkMaxHpLabel),
+            });
+
+            offset = 1;
+            index = newInstructions.FindIndex(i => i.opcode == OpCodes.Call && (MethodInfo)i.operand == Method(typeof(NetworkServer), nameof(NetworkServer.Destroy))) + offset;
+            newInstructions[index] = new CodeInstruction(OpCodes.Br, checkMaxHpLabel);
+
+            offset = 1;
+            index = newInstructions.FindIndex(i => i.opcode == OpCodes.Call && (MethodInfo)i.operand == Method(typeof(BaseTarget), "set_Network_syncMode")) + offset;
+            newInstructions[index] = new CodeInstruction(OpCodes.Br, checkMaxHpLabel);
+
+            offset = -5;
+            index = newInstructions.FindIndex(i => i.opcode == OpCodes.Call && (MethodInfo)i.operand == Method(typeof(BaseTarget), nameof(BaseTarget.RpcSendInfo))) + offset;
+
+            newInstructions[index].labels.Add(rpcLabel);
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_S, ev.LocalIndex).WithLabels(checkMaxHpLabel),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(InteractingShootingTargetEventArgs), nameof(InteractingShootingTargetEventArgs.MaxHp))),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(BaseTarget), nameof(BaseTarget._maxHp))),
+                new CodeInstruction(OpCodes.Ceq),
+                new CodeInstruction(OpCodes.Brtrue, checkAutoResetTimeLabel),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldloc_S, ev.LocalIndex),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(InteractingShootingTargetEventArgs), nameof(InteractingShootingTargetEventArgs.MaxHp))),
+                new CodeInstruction(OpCodes.Stfld, Field(typeof(BaseTarget), nameof(BaseTarget._maxHp))),
+
+                new CodeInstruction(OpCodes.Ldloc_S, ev.LocalIndex).WithLabels(checkAutoResetTimeLabel),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(InteractingShootingTargetEventArgs), nameof(InteractingShootingTargetEventArgs.AutoResetTime))),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(BaseTarget), nameof(BaseTarget._autoDestroyTime))),
+                new CodeInstruction(OpCodes.Ceq),
+                new CodeInstruction(OpCodes.Brtrue_S, rpcLabel),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldloc_S, ev.LocalIndex),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(InteractingShootingTargetEventArgs), nameof(InteractingShootingTargetEventArgs.AutoResetTime))),
+                new CodeInstruction(OpCodes.Stfld, Field(typeof(BaseTarget), nameof(BaseTarget._autoDestroyTime))),
             });
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
 
             ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+
+        private static int GetNextValue(byte buttonPressed, int targetButton, int curValue)
+        {
+            if (targetButton - buttonPressed < 0)
+                return curValue;
+
+            switch ((BaseTarget.TargetButton)buttonPressed)
+            {
+                case BaseTarget.TargetButton.IncreaseHP:
+                    return Mathf.Clamp(curValue * 2, 1, 256);
+                case BaseTarget.TargetButton.DecreaseHP:
+                    return curValue / 2;
+                case BaseTarget.TargetButton.IncreaseResetTime:
+                    return Mathf.Clamp(curValue + 1, 0, 10);
+                case BaseTarget.TargetButton.DecreaseResetTime:
+                    return Mathf.Clamp(curValue - 1, 0, 10);
+                default:
+                    return curValue;
+            }
         }
     }
 }
