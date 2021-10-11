31f584ec263942e502ee7cac949e1c6be544c511
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-10-11 12:27:41 +0200

    Add event handler

--- Exiled.Events/Handlers/Player.cs
+++ Exiled.Events/Handlers/Player.cs
@@ -343,6 +343,11 @@ namespace Exiled.Events.Handlers
         public static event CustomEventHandler<FlippingCoinEventArgs> FlippingCoin;
 
         /// <summary>
+        /// Invoked after a player presses the transmission key.
+        /// </summary>
+        public static event CustomEventHandler<TransmittingEventArgs> Transmitting;
+
+        /// <summary>
         /// Called before pre-authenticating a player.
         /// </summary>
         /// <param name="ev">The <see cref="PreAuthenticatingEventArgs"/> instance.</param>
@@ -726,5 +731,11 @@ namespace Exiled.Events.Handlers
         /// </summary>
         /// <param name="ev">The <see cref="FlippingCoinEventArgs"/> instance.</param>
         public static void OnFlippingCoin(FlippingCoinEventArgs ev) => FlippingCoin.InvokeSafely(ev);
+
+        /// <summary>
+        /// Invoked after a player presses the transmission key.
+        /// </summary>
+        /// <param name="ev">The <see cref="TransmittingEventArgs"/> instance.</param>
+        public static void OnTransmitting(TransmittingEventArgs ev) => Transmitting.InvokeSafely(ev);
     }
 }
