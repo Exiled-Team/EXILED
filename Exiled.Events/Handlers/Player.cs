e1398415aac04ef5c69ce73151c34921ea64c265
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-10-11 11:36:32 +0200

    Add event handler

--- Exiled.Events/Handlers/Player.cs
+++ Exiled.Events/Handlers/Player.cs
@@ -343,6 +343,11 @@ namespace Exiled.Events.Handlers
         public static event CustomEventHandler<FlippingCoinEventArgs> FlippingCoin;
 
         /// <summary>
+        /// Invoked after a player presses the voicechat key.
+        /// </summary>
+        public static event CustomEventHandler<VoiceChattingEventArgs> VoiceChatting;
+
+        /// <summary>
         /// Called before pre-authenticating a player.
         /// </summary>
         /// <param name="ev">The <see cref="PreAuthenticatingEventArgs"/> instance.</param>
@@ -726,5 +731,11 @@ namespace Exiled.Events.Handlers
         /// </summary>
         /// <param name="ev">The <see cref="FlippingCoinEventArgs"/> instance.</param>
         public static void OnFlippingCoin(FlippingCoinEventArgs ev) => FlippingCoin.InvokeSafely(ev);
+
+        /// <summary>
+        /// Invoked after a player presses the voicechat key.
+        /// </summary>
+        /// <param name="ev">The <see cref="VoiceChattingEventArgs"/> instance.</param>
+        public static void OnVoiceChatting(VoiceChattingEventArgs ev) => VoiceChatting.InvokeSafely(ev);
     }
 }
