4af32e5576aa3809d5b5a1ecce7c129b74477441
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-10-11 10:11:40 +0200

    Add event handler

--- Exiled.Events/Handlers/Player.cs
+++ Exiled.Events/Handlers/Player.cs
@@ -343,6 +343,11 @@ namespace Exiled.Events.Handlers
         public static event CustomEventHandler<FlippingCoinEventArgs> FlippingCoin;
 
         /// <summary>
+        /// Called before a player walks on a tantrum.
+        /// </summary>
+        public static event CustomEventHandler<WalkingOnTantrumEventArgs> WalkingOnTantrum;
+
+        /// <summary>
         /// Called before pre-authenticating a player.
         /// </summary>
         /// <param name="ev">The <see cref="PreAuthenticatingEventArgs"/> instance.</param>
@@ -726,5 +731,11 @@ namespace Exiled.Events.Handlers
         /// </summary>
         /// <param name="ev">The <see cref="FlippingCoinEventArgs"/> instance.</param>
         public static void OnFlippingCoin(FlippingCoinEventArgs ev) => FlippingCoin.InvokeSafely(ev);
+
+        /// <summary>
+        /// Called before a player walks on a tantrum.
+        /// </summary>
+        /// /// <param name="ev">The <see cref="WalkingOnTantrumEventArgs"/> instance.</param>
+        public static void OnWalkingOnTantrum(WalkingOnTantrumEventArgs ev) => WalkingOnTantrum.InvokeSafely(ev);
     }
 }
