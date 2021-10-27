6fdd3d16c61e9634a17864efb4a3168ceb8ebc08
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-10-28 01:35:28 +0200

    Update ChangingDurabilityEventArgs.cs

--- Exiled.Events/EventArgs/ChangingDurabilityEventArgs.cs
+++ Exiled.Events/EventArgs/ChangingDurabilityEventArgs.cs
@@ -7,32 +7,39 @@
 
 namespace Exiled.Events.EventArgs
 {
-    using InventorySystem.Items.Firearms;
+    using Exiled.API.Features.Items;
 
     /// <summary>
     /// Contains all informations before changing item durability.
     /// </summary>
-    public class ChangingDurabilityEventArgs : ChangingAttributesEventArgs
+    public class ChangingDurabilityEventArgs : System.EventArgs
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ChangingDurabilityEventArgs"/> class.
         /// </summary>
-        /// <param name="item"><inheritdoc cref="ChangingAttributesEventArgs.OldItem"/></param>
+        /// <param name="firearm"><inheritdoc cref="Firearm"/></param>
         /// <param name="newDurability"><inheritdoc cref="NewDurability"/></param>
-        /// <param name="isAllowed"><inheritdoc cref="ChangingAttributesEventArgs.IsAllowed"/></param>
-        public ChangingDurabilityEventArgs(Firearm item, float newDurability, bool isAllowed = true)
-            : base(item, item, isAllowed)
+        /// <param name="isAllowed"><inheritdoc cref="IsAllowed"/></param>
+        public ChangingDurabilityEventArgs(Firearm firearm, float newDurability, bool isAllowed = true)
         {
-            Item = (API.Features.Items.Firearm)API.Features.Items.Item.Get(item);
+            Firearm = firearm;
             NewDurability = newDurability;
+            IsAllowed = isAllowed;
         }
 
-        /// <inheritdoc cref="Item"/>
-        public API.Features.Items.Firearm Item { get; set; }
+        /// <summary>
+        /// Gets the <see cref="API.Features.Items.Firearm"/> the durability is being changed to.
+        /// </summary>
+        public Firearm Firearm { get; }
 
         /// <summary>
-        /// Gets or sets the new durability to be used by the weapon.
+        /// Gets or sets the new durability to be used by the firearm.
         /// </summary>
         public float NewDurability { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the durability can be changed.
+        /// </summary>
+        public bool IsAllowed { get; set; }
     }
 }
