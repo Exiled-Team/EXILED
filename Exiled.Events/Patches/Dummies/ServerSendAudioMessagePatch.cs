4433876e05400829d1d9d6fa1be5bffd9246b299
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-11-06 21:35:53 +0100

    who is ayylmao? he's a korean hecker!

---
+++ Exiled.Events/Patches/Dummies/ServerSendAudioMessagePatch.cs
@@ -0,0 +1,53 @@
+// -----------------------------------------------------------------------
+// <copyright file="ServerSendAudioMessagePatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Cassie
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using HarmonyLib;
+
+    using InventorySystem.Items.Firearms;
+
+    using NorthwoodLib.Pools;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="FirearmExtensions.ServerSendAudioMessage(Firearm, byte)"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(FirearmExtensions), nameof(FirearmExtensions.ServerSendAudioMessage))]
+    internal static class ServerSendAudioMessagePatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            const int offset = 1;
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stloc_0) + offset;
+
+            Label ret = generator.DefineLabel();
+
+            newInstructions.InsertRange(0, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.Player), nameof(API.Features.Player.IsDummy))),
+                new CodeInstruction(OpCodes.Brtrue_S, ret),
+            });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(ret);
+
+            for (int i = 0; i < newInstructions.Count; i++)
+                yield return newInstructions[i];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
