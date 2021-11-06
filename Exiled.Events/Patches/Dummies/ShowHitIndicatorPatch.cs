1ecc87f3bf09fe9977b096594946302e8f7ce829
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-11-06 21:30:14 +0100

    who is lmfao? he's a chinese hecker!

---
+++ Exiled.Events/Patches/Dummies/ShowHitIndicatorPatch.cs
@@ -0,0 +1,51 @@
+// -----------------------------------------------------------------------
+// <copyright file="ShowHitIndicatorPatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Cassie
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using HarmonyLib;
+
+    using NorthwoodLib.Pools;
+
+    using UnityEngine;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="InventorySystem.Items.Firearms.Modules.StandardHitregBase.ShowHitIndicator(uint, float, Vector3)"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(InventorySystem.Items.Firearms.Modules.StandardHitregBase), nameof(InventorySystem.Items.Firearms.Modules.StandardHitregBase.ShowHitIndicator))]
+    internal static class ShowHitIndicatorPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            const int offset = 1;
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret) + offset;
+
+            Label ret = generator.DefineLabel();
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_0).MoveLabelsFrom(newInstructions[index]),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.Player), nameof(API.Features.Player.IsDummy))),
+                new CodeInstruction(OpCodes.Brtrue_S, ret),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
