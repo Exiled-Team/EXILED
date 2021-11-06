a53c71cdfffd5f86df73a70b1d8f2072c465148f
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-11-06 21:11:14 +0100

    Learn IL pt 2

---
+++ Exiled.Events/Patches/Dummies/RemovePlayerPatch.cs
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------
+// <copyright file="RemovePlayerPatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Cassie
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection.Emit;
+
+    using Exiled.API.Features;
+
+    using HarmonyLib;
+
+    using NorthwoodLib.Pools;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="PlayerManager.RemovePlayer(UnityEngine.GameObject, int)"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.RemovePlayer))]
+    internal static class RemovePlayerPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stsfld);
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(Dummy), nameof(Dummy.List))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Enumerable), nameof(Enumerable.Count)).MakeGenericMethod(typeof(Dummy))),
+                new CodeInstruction(OpCodes.Sub),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
