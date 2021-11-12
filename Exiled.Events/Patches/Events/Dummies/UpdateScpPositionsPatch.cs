5b9454e1c2e7819c0a135e252facb1b675dee7ab
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-11-12 01:46:27 +0100

    I don't know more asian countries

---
+++ Exiled.Events/Patches/Events/Dummies/UpdateScpPositionsPatch.cs
@@ -0,0 +1,58 @@
+// -----------------------------------------------------------------------
+// <copyright file="UpdateScpPositionsPatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Dummies
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using HarmonyLib;
+
+    using Mirror;
+
+    using NorthwoodLib.Pools;
+
+    using RemoteAdmin;
+
+    using UnityEngine;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="Scp079PlayerScript.UpdateScpPositions"/> to prevent tracking of dummies.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp079PlayerScript), nameof(Scp079PlayerScript.UpdateScpPositions))]
+    internal static class UpdateScpPositionsPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Br_S);
+
+            Label rep = (Label)newInstructions[index].operand;
+
+            const int offset = 1;
+            index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stloc_S) + offset;
+
+            newInstructions.InsertRange(0, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloca_S, 4),
+                new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(KeyValuePair<GameObject, ReferenceHub>), nameof(KeyValuePair<GameObject, ReferenceHub>.Key))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.Player), nameof(API.Features.Player.IsDummy))),
+                new CodeInstruction(OpCodes.Brtrue_S, rep),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
