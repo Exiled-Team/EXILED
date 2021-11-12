7a1411a0bd01b03fa916f7bb8c71e8b9e11121e2
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-11-12 02:32:05 +0100

    `PlayerEffectsController::Start` Patch

---
+++ Exiled.Events/Patches/Events/Dummies/PlayerEffectsControllerStartPatch.cs
@@ -0,0 +1,51 @@
+// -----------------------------------------------------------------------
+// <copyright file="PlayerEffectsControllerStartPatch.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Dummies
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using HarmonyLib;
+
+    using NorthwoodLib.Pools;
+
+    using RemoteAdmin;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="PlayerEffectsController.Start"/> to prevent the sending of messages to dummies.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerEffectsController), nameof(PlayerEffectsController.Start))]
+    internal static class PlayerEffectsControllerStartPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            Label ret = generator.DefineLabel();
+
+            newInstructions.InsertRange(0, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayerEffectsController), nameof(PlayerEffectsController.hub))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.Player), nameof(API.Features.Player.IsDummy))),
+                new CodeInstruction(OpCodes.Brtrue_S, ret),
+            });
+
+            newInstructions[newInstructions.Count - 1].labels.Add(ret);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
