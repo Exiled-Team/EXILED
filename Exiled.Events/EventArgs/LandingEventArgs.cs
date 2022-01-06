4416acb1266036d1574007843a1f011170a559a0
Jesus-QC <jesusquirocampos@gmail.com>
2022-01-06 16:23:15 +0100

    added landing transpiler

---
+++ Exiled.Events/EventArgs/LandingEventArgs.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LandingEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs
+{
+    using System;
+    using Exiled.API.Features;
+
+    /// <summary>
+    /// Contains all the information when a player lands.
+    /// </summary>
+    public class LandingEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LandingEventArgs"/> class.
+        /// </summary>
+        /// <param name="player">The player landing.</param>
+        public LandingEventArgs(Player player) => Player = player;
+
+        /// <summary>
+        /// Gets the player landing.
+        /// </summary>
+        public Player Player { get; }
+    }
+}
