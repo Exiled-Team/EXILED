b5f124597dab1fbad1709fa93cb5b5d298f5ad13
louis1706 <louismonneyron5@yahoo.com>
2022-01-09 00:35:40 +0100

    Player new Method AddAhp

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -2037,6 +2037,20 @@ namespace Exiled.API.Features
         /// <returns>The tantrum's <see cref="GameObject"/>.</returns>
         public GameObject PlaceTantrum() => Map.PlaceTantrum(Position);
 
+        /// <summary>
+        /// Give an new AhpProccess.
+        /// </summary>
+        /// <param name="amount">The ammount of AHP he get.</param>
+        /// <param name="limit">The maximum AHP he can get.</param>
+        /// <param name="decay">How many it's lost per tick.</param>
+        /// <param name="efficacy">percentage of damage it's take on Hurt.</param>
+        /// <param name="sustain">Time before the AHP is decay.</param>
+        /// <param name="persistant">The Proccess it's not deleted when ahp it's at 0.</param>
+        public void AddAhp(float amount, float limit = 75f, float decay = 1.2f, float efficacy = 0.7f, float sustain = 0f, bool persistant = false)
+        {
+            ReferenceHub.playerStats.GetModule<AhpStat>().ServerAddProcess(amount, limit, decay, efficacy, sustain, persistant);
+        }
+
         /// <inheritdoc/>
         public override string ToString() => $"{Id} {Nickname} {UserId} {Role} {Team}";
     }
