33406550fc7967d82ff44d4cbeb578eb54f04865
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2022-01-17 06:02:32 +0100

    TriggeringTesla fix

--- Exiled.Events/Patches/Events/Player/TriggeringTesla.cs
+++ Exiled.Events/Patches/Events/Player/TriggeringTesla.cs
@@ -82,7 +82,7 @@ namespace Exiled.Events.Patches.Events.Player
                 new CodeInstruction(OpCodes.Dup),
                 new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.TeslaGate), nameof(API.Features.TeslaGate.Base))),
                 new CodeInstruction(OpCodes.Ldloc_S, referenceHub.LocalIndex),
-                new CodeInstruction(OpCodes.Callvirt, Property(typeof(ReferenceHub), nameof(ReferenceHub.gameObject))),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(ReferenceHub), nameof(ReferenceHub.gameObject))),
                 new CodeInstruction(OpCodes.Callvirt, Method(typeof(TeslaGate), nameof(TeslaGate.PlayerInHurtRange))),
 
                 // referenceHub::characterClassManager::CurClass != RoleType::Spectator && teslaGate::PlayerInRange(referenceHub) && !teslaGate::InProgress
