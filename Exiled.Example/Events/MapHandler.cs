b354028716eb628ea42ce373f5b2f94ea73be0ff
SplatTab <skidm44@gmail.com>
2022-02-07 00:22:26 -0600

    Smol changes <:

--- Exiled.Example/Events/MapHandler.cs
+++ Exiled.Example/Events/MapHandler.cs
@@ -28,5 +28,41 @@ namespace Exiled.Example.Events
         {
             Log.Info($"A generator has been activated in {ev.Generator.gameObject.GetComponent<Room>().Name}!");
         }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnAnnouncingDecontamination(AnnouncingDecontaminationEventArgs)"/>
+        public void OnAnnouncingDecontamination(AnnouncingDecontaminationEventArgs ev)
+        {
+            Log.Info("Cassie is announcing decontamination");
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnAnnouncingNtfEntrance(AnnouncingNtfEntranceEventArgs)"/>
+        public void OnAnnouncingNtfEntrance(AnnouncingNtfEntranceEventArgs ev)
+        {
+            Log.Info("Cassie is announcing a Ntf entrance");
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnAnnouncingScpTermination(AnnouncingScpTerminationEventArgs)"/>
+        public void OnAnnouncingScpTermination(AnnouncingScpTerminationEventArgs ev)
+        {
+            Log.Info($"Cassie is announcing a Scp Termination with - Killer: {ev.Killer} Victim: {ev.Player} Scp Role: {ev.Role} Termination Cause: {ev.TerminationCause}");
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnDamagingWindow(DamagingWindowEventArgs)"/>
+        public void OnDamagingWindow(DamagingWindowEventArgs ev)
+        {
+            Log.Info($"A window was damaged - Window Damage: {ev.Damage}");
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnPlacingBlood(PlacingBloodEventArgs)"/>
+        public void OnPlacingBlood(PlacingBloodEventArgs ev)
+        {
+            Log.Info($"A blood decal was placed - Bleeding Player: {ev.Player} Position:{ev.Position} Multiplier: {ev.Multiplier}");
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Map.OnPlacingBulletHole(PlacingBulletHole)"/>
+        public void OnPlacingBulletHole(PlacingBulletHole ev)
+        {
+            Log.Info($"A bullet hole decal was placed - Shooter Player: {ev.Owner} Position:{ev.Position} Rotation: {ev.Rotation}");
+        }
     }
 }
