574feade52d389e7820a7a27242d11edb88273d6
rayzer <rayzerbrain@gmail.com>
2022-02-16 22:33:21 -0600

    why was that there

--- Exiled.Events/Patches/Events/Map/SpawningItem.cs
+++ Exiled.Events/Patches/Events/Map/SpawningItem.cs
@@ -11,7 +11,6 @@ namespace Exiled.Events.Patches.Events.Map
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
-    using Exiled.API.Features;
     using Exiled.Events.EventArgs;
 
     using HarmonyLib;
