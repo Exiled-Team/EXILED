2a971e248847f2b6489f501567e9e19c382bea0a
joker-119 <amathor929@gmail.com>
2022-04-12 04:24:50 -0700

    Transpiler go brr

--- Exiled.Events/Patches/Fixes/Scp1853Fix.cs
+++ Exiled.Events/Patches/Fixes/Scp1853Fix.cs
@@ -5,14 +5,22 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Exiled.Events.Patches.Generic
+namespace Exiled.Events.Patches.Fixes
 {
-    using System;
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
 
     using CustomPlayerEffects;
 
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+
     using HarmonyLib;
-    #pragma warning disable SA1313
+
+    using NorthwoodLib.Pools;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patch the <see cref="Scp1853.OnUpdate"/>.
@@ -21,23 +29,30 @@ namespace Exiled.Events.Patches.Generic
     [HarmonyPatch(typeof(Scp1853), nameof(Scp1853.OnUpdate))]
     internal static class Scp1853Fix
     {
-        private static bool Prefix(Scp1853 __instance)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            try
-            {
-                if (__instance.IsEnabled && __instance._scp207Reference.IsEnabled && !__instance.Hub.playerEffectsController.GetEffect<Poisoned>().IsEnabled)
-                {
-                    __instance.Hub.playerEffectsController.EnableEffect<Poisoned>(0f, false);
-                }
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+            Label retLabel = generator.DefineLabel();
+            int offset = 1;
+            int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Brfalse_S) + offset;
 
-                return false;
-            }
-            catch (Exception e)
+            newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                API.Features.Log.Error($"Exiled.Events.Patches.Generic.Scp1853Fix: {e}\n{e.StackTrace}");
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldfld, Field(typeof(Scp1853), nameof(Scp1853.Hub))),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Ldc_I4, (int)EffectType.Poisoned),
+                new(OpCodes.Callvirt, Method(typeof(Player), nameof(Player.GetEffect))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(PlayerEffect), nameof(PlayerEffect.IsEnabled))),
+                new(OpCodes.Brtrue_S, retLabel),
+            });
 
-                return true;
-            }
+            newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
