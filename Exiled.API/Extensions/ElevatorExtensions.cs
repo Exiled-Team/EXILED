4da4bc955c1af37528846fe7a143f714733c1d2e
lifyxus <lifyxus@gmail.com>
2022-05-04 21:29:59 +0300

    Added RoomExtensions and ElevatorExtensions

---
+++ Exiled.API/Extensions/ElevatorExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Exiled.API.Extensions
+{
+    using Exiled.API.Enums;
+
+    /// <summary>
+    /// A set of extensions for <see cref="ElevatorType"/>.
+    /// </summary>
+    public static class ElevatorExtensions
+    {
+        /// <summary>
+        /// Checks if a <see cref="ElevatorType">elevator type</see> is a gate.
+        /// </summary>
+        /// <param name="elevator">The elevator to be checked.</param>
+        /// <returns>Returns whether the <see cref="ElevatorType"/> is a gate or not.</returns>
+        public static bool IsGate(this ElevatorType elevator)
+        {
+            return elevator is ElevatorType.GateA or ElevatorType.GateB;
+        }
+
+        /// <summary>
+        /// Checks if a <see cref="ElevatorType">elevator type</see> is between Hcz and Lcz.
+        /// </summary>
+        /// <param name="elevator">The elevator to be checked.</param>
+        /// <returns>Returns whether the <see cref="ElevatorType"/> is between Hcz and Lcz.</returns>
+        public static bool IsBetweenHczAndLcz(this ElevatorType elevator)
+        {
+            return elevator is ElevatorType.LczA or ElevatorType.LczB;
+        }
+    }
+}
---
+++ Exiled.API/Extensions/RoomExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Exiled.API.Extensions
+{
+    using System.Collections.Generic;
+    using Exiled.API.Enums;
+
+    /// <summary>
+    /// A set of extensions for <see cref="RoomType"/>.
+    /// </summary>
+    public static class RoomExtensions
+    {
+        /// <summary>
+        /// Checks if a <see cref="RoomType">room type</see> is a gate.
+        /// </summary>
+        /// <param name="room">The room to be checked.</param>
+        /// <returns>Returns whether the <see cref="RoomType"/> is a gate or not.</returns>
+        public static bool IsGate(this RoomType room)
+        {
+            return room is RoomType.EzGateA or RoomType.EzGateB;
+        }
+
+        /// <summary>
+        /// Checks if a <see cref="RoomType">room type</see> is a checkpoint.
+        /// </summary>
+        /// <param name="room">The room to be checked.</param>
+        /// <returns>Returns whether the <see cref="RoomType"/> is an checkpoint or not.</returns>
+        public static bool IsCheckpoint(this RoomType room)
+        {
+            return room is RoomType.LczChkpA
+                or RoomType.LczChkpB or RoomType.HczEzCheckpoint
+                or RoomType.HczChkpA or RoomType.HczChkpB;
+        }
+
+        /// <summary>
+        ///  Checks if a <see cref="RoomType">room type</see> is a SCP room.
+        /// </summary>
+        /// <param name="room">The room to be checked.</param>
+        /// <returns>Returns whether the <see cref="RoomType"/> is a SCP room or not.</returns>
+        public static bool IsScpRoom(this RoomType room)
+        {
+            List<RoomType> scpRooms = new()
+            {
+                RoomType.Lcz173,
+                RoomType.Lcz330,
+                RoomType.Lcz914,
+                RoomType.Hcz049,
+                RoomType.Hcz079,
+                RoomType.Hcz096,
+                RoomType.Hcz106,
+                RoomType.Hcz939,
+            };
+
+            return scpRooms.Contains(room);
+        }
+    }
+}
