4798a928a76eb6e00c0b8549d82f53b0050e402f
IRacle1 <zornik2006@yandex.ru>
2022-05-21 21:20:35 +0300

    Patch

--- Exiled.Events/Patches/Events/Player/ItemDrop.cs
+++ Exiled.Events/Patches/Events/Player/ItemDrop.cs
@@ -8,15 +8,15 @@
 namespace Exiled.Events.Patches.Events.Player
 {
 #pragma warning disable SA1118
+    using System;
     using System.Collections.Generic;
+    using System.Reflection;
     using System.Reflection.Emit;
 
     using Exiled.Events.EventArgs;
     using Exiled.Events.Handlers;
 
     using HarmonyLib;
-
-    using InventorySystem.Items;
 
     using NorthwoodLib.Pools;
 
@@ -33,27 +33,29 @@ namespace Exiled.Events.Patches.Events.Player
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            LocalBuilder item = generator.DeclareLocal(typeof(Item));
+            LocalBuilder item = generator.DeclareLocal(typeof(API.Features.Items.Item));
+            LocalBuilder ev = generator.DeclareLocal(typeof(DroppingItemEventArgs));
             Label returnLabel = generator.DefineLabel();
             Label notNullLabel = generator.DefineLabel();
 
             newInstructions.InsertRange(0, new[]
             {
-                // Item item = GetItem(player, itemSerial)
-                // if (item is null)
+                // if (!player.TryGetItem(itemSerial, out Item item)) {
                 //    Handlers.Player.OnDroppingNull(new DroppingNullEventArgs(Player));
                 //    return;
+                // }
                 //
-                // Handlers.Player.OnDroppingNull(new DroppingItemEventArgs(Player, Item, true));
+                // var ev = new DroppingItemEventArgs(player, item, isThrow, true);
+                // Handlers.Player.OnDroppingItem(ev);
                 // if (!ev.IsAllowed)
                 //     return;
+                // isThrow = ev.IsThrown;
                 new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(InventorySystem.Inventory), nameof(InventorySystem.Inventory._hub))),
                 new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
                 new(OpCodes.Ldarg_1),
-                new(OpCodes.Call, Method(typeof(ItemDrop), nameof(ItemDrop.GetItem))),
-                new(OpCodes.Stloc_S, item.LocalIndex),
-                new(OpCodes.Ldloc_S, item.LocalIndex),
+                new(OpCodes.Ldloca_S, item.LocalIndex),
+                new(OpCodes.Callvirt, FirstMethod(typeof(API.Features.Player), info => info.Name == "TryGetItem" && info.GetCustomAttribute<ObsoleteAttribute>() == null)),
                 new(OpCodes.Brtrue_S, notNullLabel),
                 new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(InventorySystem.Inventory), nameof(InventorySystem.Inventory._hub))),
@@ -65,13 +67,19 @@ namespace Exiled.Events.Patches.Events.Player
                 new(OpCodes.Ldfld, Field(typeof(InventorySystem.Inventory), nameof(InventorySystem.Inventory._hub))),
                 new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
                 new(OpCodes.Ldloc_S, item.LocalIndex),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(API.Features.Items.Item), nameof(API.Features.Items.Item.Base))),
                 new(OpCodes.Ldarg_2),
                 new(OpCodes.Ldc_I4_1),
                 new(OpCodes.Newobj, GetDeclaredConstructors(typeof(DroppingItemEventArgs))[0]),
                 new(OpCodes.Dup),
+                new(OpCodes.Dup),
+                new(OpCodes.Stloc_S, ev.LocalIndex),
                 new(OpCodes.Call, Method(typeof(Player), nameof(Player.OnDroppingItem))),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(DroppingItemEventArgs), nameof(DroppingItemEventArgs.IsAllowed))),
                 new(OpCodes.Brfalse, returnLabel),
+                new(OpCodes.Ldloc_S, ev.LocalIndex),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(DroppingItemEventArgs), nameof(DroppingItemEventArgs.IsThrown))),
+                new(OpCodes.Starg_S, 2),
             });
 
             newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
@@ -81,7 +89,5 @@ namespace Exiled.Events.Patches.Events.Player
 
             ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
-
-        private static ItemBase GetItem(API.Features.Player player, ushort serial) => player.TryGetItem(serial, out ItemBase item) ? item : null;
     }
 }
