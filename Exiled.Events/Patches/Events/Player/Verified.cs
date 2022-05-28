2318736bc76c28259150f68e486c045091e3b957
IRacle1 <zornik2006@yandex.ru>
2022-05-28 16:53:35 +0300

    comment

--- Exiled.Events/Patches/Events/Player/Verified.cs
+++ Exiled.Events/Patches/Events/Player/Verified.cs
@@ -44,6 +44,22 @@ namespace Exiled.Events.Patches.Events.Player
             const int offset = -1;
             int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Call && (MethodInfo)i.operand == Method(typeof(ServerRoles), nameof(ServerRoles.RefreshPermissions))) + offset;
 
+            // Player player;
+            // if(!Player.UnverifiedPlayers.TryGetValue(_hub, out player)) {
+            //     Means the player connected before WaitingForPlayers event is fired
+            //     Let's call Joined event, since it wasn't called, to avoid breaking the logic of the order of event calls
+            //     Blame NorthWood
+            //
+            //     Joined.CallEvent(_hub, out player);
+            // }
+            // #if DEBUG
+            // Log.Debug("{player.Nickname} has verified!");
+            // #endif
+            // Player.Dictionary.Add(_hub.gameObject, player);
+            // player.IsVerified = true;
+            // player.RawUserId = player.UserId.GetRawUserId();
+            // Log.SendRaw("Player {player.Nickname} ({player.UserId}) ({player.Id}) connected with the IP: {player.IPAddress}", ConsoleColor.Green);
+            // Player.OnVerified(new VerifiedEventArgs(player));
             newInstructions.InsertRange(index, new[]
             {
                 new(OpCodes.Call, PropertyGetter(typeof(Player), nameof(Player.UnverifiedPlayers))),
@@ -52,10 +68,12 @@ namespace Exiled.Events.Patches.Events.Player
                 new(OpCodes.Ldloca_S, player.LocalIndex),
                 new(OpCodes.Callvirt,  Method(typeof(ConditionalWeakTable<ReferenceHub, Player>), nameof(ConditionalWeakTable<ReferenceHub, Player>.TryGetValue))),
                 new(OpCodes.Brtrue_S, callJoined),
+
                 new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(ServerRoles), nameof(ServerRoles._hub))),
                 new(OpCodes.Ldloca_S, player.LocalIndex),
                 new(OpCodes.Call,  Method(typeof(Joined), nameof(Joined.CallEvent))),
+
                 new CodeInstruction(OpCodes.Nop).WithLabels(callJoined),
 #if DEBUG
                 new(OpCodes.Ldstr, "{0} has verified!"),
