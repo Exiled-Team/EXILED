1428c63b2c5dc18cdda1733096ad56cdd637a517
Claudio <72409092+ClaudioPanConQueso@users.noreply.github.com>
2022-06-13 23:47:59 -0300

    Renamed...
    renamed.

--- Exiled.Events/EventArgs/StartingWarheadEventArgs.cs
+++ Exiled.Events/EventArgs/StartingWarheadEventArgs.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="StartingEventArgs.cs" company="Exiled Team">
+// <copyright file="StartingWarheadEventArgs.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -12,14 +12,14 @@ namespace Exiled.Events.EventArgs
     /// <summary>
     /// Contains all informations before starting the warhead.
     /// </summary>
-    public class StartingEventArgs : StoppingEventArgs
+    public class StartingWarheadEventArgs : StoppingEventArgs
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="StartingEventArgs"/> class.
+        /// Initializes a new instance of the <see cref="StartingWarheadEventArgs"/> class.
         /// </summary>
         /// <param name="player">The player who's going to start the warhead.</param>
         /// <param name="isAllowed">Indicating whether the event can be executed or not.</param>
-        public StartingEventArgs(Player player, bool isAllowed = true)
+        public StartingWarheadEventArgs(Player player, bool isAllowed = true)
             : base(player, isAllowed)
         {
         }
