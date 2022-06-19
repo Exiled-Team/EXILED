5ba3611cb2f3a31858ee96d430dcb024a8f4727b
undid-iridium <francis.x.irizarry@gmail.com>
2022-06-18 21:28:26 -0400

    Ok well, this is simplistic enough, but cassie does not work.

--- Exiled.API/Features/GenericDamageHandler.cs
+++ Exiled.API/Features/GenericDamageHandler.cs
@@ -50,95 +50,7 @@ namespace Exiled.API.Features
             this.Damage = amount;
             this.ServerLogsText = $"You were damaged by {damageType}";
 
-            switch(damageType)
-            {
-                case DamageType.Falldown:
-                case DamageType.Hypothermia:
-                case DamageType.Asphyxiation:
-                case DamageType.Poison:
-                case DamageType.Bleeding:
-                case DamageType.Crushed:
-                case DamageType.FemurBreaker:
-                case DamageType.PocketDimension:
-                case DamageType.FriendlyFireDetector:
-                case DamageType.SeveredHands:
-                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", amount, cassieAnnouncement.Announcement);
-                    break;
-                case DamageType.Warhead:
-                case DamageType.Decontamination:
-                case DamageType.Tesla:
-                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", amount, cassieAnnouncement.Announcement);
-                    break;
-
-                case DamageType.Recontainment:
-                    Base = new RecontainmentDamageHandler(Attacker);
-                    break;
-
-                case DamageType.Firearm:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunAK);
-                    break;
-                case DamageType.MicroHid:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.MicroHID);
-                    break;
-                case DamageType.Explosion:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GrenadeHE);
-                    break;
-                case DamageType.Crossvec:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunCrossvec);
-                    break;
-                case DamageType.Logicer:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunLogicer);
-                    break;
-                case DamageType.Revolver:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunRevolver);
-                    break;
-                case DamageType.Shotgun:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunShotgun);
-                    break;
-                case DamageType.AK:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunAK);
-                    break;
-                case DamageType.Com15:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunCOM15);
-                    break;
-                case DamageType.Com18:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunCOM18);
-                    break;
-                case DamageType.Fsp9:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunFSP9);
-                    break;
-                case DamageType.E11Sr:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.GunE11SR);
-                    break;
-                case DamageType.ParticleDisruptor:
-                    GenericFirearm(player, attacker, amount, damageType, ItemType.ParticleDisruptor);
-                    break;
-
-                case DamageType.Scp096:
-                    PlayableScps.Scp096 curr096 = attacker.CurrentScp as PlayableScps.Scp096 ?? new PlayableScps.Scp096();
-                    if(curr096 != null)
-                    {
-                        curr096.Hub = attacker.ReferenceHub;
-                    }
-
-                    Base = new Scp096DamageHandler(curr096, amount, Scp096DamageHandler.AttackType.Slap);
-                    break;
-                case DamageType.Scp:
-                case DamageType.Scp018:
-                case DamageType.Scp207:
-                case DamageType.Scp049:
-                case DamageType.Scp173:
-                case DamageType.Scp939:
-                case DamageType.Scp0492:
-                case DamageType.Scp106:
-                    Base = new ScpDamageHandler(attacker.ReferenceHub, amount, DeathTranslations.Unknown);
-                    break;
-                case DamageType.Custom:
-                case DamageType.Unknown:
-                default:
-                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", amount, $"{player.Nickname} killed by {attacker.Nickname} utilizing {damageType}");
-                    break;
-            }
+            Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", amount, string.IsNullOrEmpty(cassieAnnouncement?.Announcement) ? $"{player.Nickname} killed by {attacker.Nickname} utilizing {damageType}" : cassieAnnouncement.Announcement);
         }
 
         /// <summary>
@@ -178,28 +90,7 @@ namespace Exiled.API.Features
         /// <returns> Handlers for processing. </returns>
         public override HandlerOutput ApplyDamage(ReferenceHub ply)
         {
-            return base.ApplyDamage(ply);          // return base.ApplyDamage(ply);
-        }
-
-        /// <summary>
-        /// Generic firearm path for handle type.
-        /// </summary>
-        /// <param name="player"> Current player. </param>
-        /// <param name="attacker"> Current attacker. </param>
-        /// <param name="amount"> Damage amount. </param>
-        /// <param name="damageType"> Damage type. </param>
-        /// <param name="itemType"> ItemType. </param>
-        private void GenericFirearm(Player player, Player attacker, float amount, DamageType damageType, ItemType itemType)
-        {
-            Log.Info($"What was amount {amount}");
-            Firearm firearm = new(itemType)
-            {
-                Base =
-                            {
-                                Owner = attacker.ReferenceHub,
-                            },
-            };
-            Base = new FirearmDamageHandler(firearm.Base, amount);
+            return base.ApplyDamage(ply);
         }
     }
 }
