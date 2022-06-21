aa7d3c3d709144a3cb2fd3f86dc227082cd1ae05
undid-iridium <francis.x.irizarry@gmail.com>
2022-06-20 20:47:24 -0400

    Should allow specific type damage

--- Exiled.API/Features/GenericDamageHandler.cs
+++ Exiled.API/Features/GenericDamageHandler.cs
@@ -33,10 +33,10 @@ namespace Exiled.API.Features
         /// </summary>
         /// <param name="player"> Current player (Target). </param>
         /// <param name="attacker"> Attacker. </param>
-        /// <param name="amount"> Damage quantity. </param>
+        /// <param name="damage"> Damage quantity. </param>
         /// <param name="damageType"> Damage type. </param>
         /// <param name="cassieAnnouncement"> Custom cassie announcment. </param>
-        public GenericDamageHandler(Player player, Player attacker, float amount, DamageType damageType, DamageHandlers.DamageHandlerBase.CassieAnnouncement cassieAnnouncement)
+        public GenericDamageHandler(Player player, Player attacker, float damage, DamageType damageType, DamageHandlers.DamageHandlerBase.CassieAnnouncement cassieAnnouncement)
             : base($"You were damaged by {damageType}")
         {
             this.player = player;
@@ -45,10 +45,98 @@ namespace Exiled.API.Features
 
             this.Attacker = attacker.Footprint;
             this.AllowSelfDamage = true;
-            this.Damage = amount;
+            this.Damage = damage;
             this.ServerLogsText = $"You were damaged by {damageType}";
 
-            Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", amount, string.IsNullOrEmpty(cassieAnnouncement?.Announcement) ? $"{player.Nickname} killed by {attacker.Nickname} utilizing {damageType}" : cassieAnnouncement.Announcement);
+            switch (damageType)
+            {
+                case DamageType.Falldown:
+                case DamageType.Hypothermia:
+                case DamageType.Asphyxiation:
+                case DamageType.Poison:
+                case DamageType.Bleeding:
+                case DamageType.Crushed:
+                case DamageType.FemurBreaker:
+                case DamageType.PocketDimension:
+                case DamageType.FriendlyFireDetector:
+                case DamageType.SeveredHands:
+                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", damage, cassieAnnouncement.Announcement);
+                    break;
+                case DamageType.Warhead:
+                case DamageType.Decontamination:
+                case DamageType.Tesla:
+                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", damage, cassieAnnouncement.Announcement);
+                    break;
+
+                case DamageType.Recontainment:
+                    Base = new RecontainmentDamageHandler(Attacker);
+                    break;
+
+                case DamageType.Firearm:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunAK);
+                    break;
+                case DamageType.MicroHid:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.MicroHID);
+                    break;
+                case DamageType.Explosion:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GrenadeHE);
+                    break;
+                case DamageType.Crossvec:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunCrossvec);
+                    break;
+                case DamageType.Logicer:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunLogicer);
+                    break;
+                case DamageType.Revolver:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunRevolver);
+                    break;
+                case DamageType.Shotgun:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunShotgun);
+                    break;
+                case DamageType.AK:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunAK);
+                    break;
+                case DamageType.Com15:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunCOM15);
+                    break;
+                case DamageType.Com18:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunCOM18);
+                    break;
+                case DamageType.Fsp9:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunFSP9);
+                    break;
+                case DamageType.E11Sr:
+                    GenericFirearm(player, attacker, damage, damageType, ItemType.GunE11SR);
+                    break;
+                case DamageType.ParticleDisruptor:
+                    Base = new DisruptorDamageHandler(Attacker, damage);
+                    break;
+
+                case DamageType.Scp096:
+                    PlayableScps.Scp096 curr096 = attacker.CurrentScp as PlayableScps.Scp096 ?? new PlayableScps.Scp096();
+                    if (curr096 != null)
+                    {
+                        curr096.Hub = attacker.ReferenceHub;
+                    }
+
+                    Base = new Scp096DamageHandler(curr096, damage, Scp096DamageHandler.AttackType.Slap);
+                    break;
+                case DamageType.Scp:
+                case DamageType.Scp018:
+                case DamageType.Scp207:
+                case DamageType.Scp049:
+                case DamageType.Scp173:
+                case DamageType.Scp939:
+                case DamageType.Scp0492:
+                case DamageType.Scp106:
+                    Base = new ScpDamageHandler(attacker.ReferenceHub, damage, DeathTranslations.Unknown);
+                    break;
+                case DamageType.Custom:
+                case DamageType.Unknown:
+                default:
+                    Base = new CustomReasonDamageHandler($"You were damaged by {damageType}", damage, string.IsNullOrEmpty(cassieAnnouncement?.Announcement) ? $"{player.Nickname} killed by {attacker.Nickname} utilizing {damageType}" : cassieAnnouncement.Announcement);
+                    break;
+            }
         }
 
         /// <summary>
@@ -100,6 +188,27 @@ namespace Exiled.API.Features
             }
 
             return output;
+        }
+
+        /// <summary>
+        /// Generic firearm path for handle type.
+        /// </summary>
+        /// <param name="player"> Current player. </param>
+        /// <param name="attacker"> Current attacker. </param>
+        /// <param name="amount"> Damage amount. </param>
+        /// <param name="damageType"> Damage type. </param>
+        /// <param name="itemType"> ItemType. </param>
+        private void GenericFirearm(Player player, Player attacker, float amount, DamageType damageType, ItemType itemType)
+        {
+            Log.Info($"What was amount {amount}");
+            Firearm firearm = new(itemType)
+            {
+                Base =
+                            {
+                                Owner = attacker.ReferenceHub,
+                            },
+            };
+            Base = new FirearmDamageHandler(firearm.Base, amount);
         }
     }
 }
