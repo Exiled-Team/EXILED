61b4df110f5efbbf04c1039c3189ad042c3b6956
IRacle <zornik2006@yandex.ru>
2022-06-22 15:13:35 +0300

    colliderId 0

--- Exiled.Events/Patches/Events/Player/InteractingGeneratorEvents.cs
+++ Exiled.Events/Patches/Events/Player/InteractingGeneratorEvents.cs
@@ -9,6 +9,9 @@ namespace Exiled.Events.Patches.Events.Player
 {
 #pragma warning disable SA1313
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Reflection.Emit;
 
     using Exiled.Events.EventArgs;
     using Exiled.Events.Handlers;
@@ -21,6 +24,10 @@ namespace Exiled.Events.Patches.Events.Player
 
     using MapGeneration.Distributors;
 
+    using NorthwoodLib.Pools;
+
+    using static HarmonyLib.AccessTools;
+
     /// <summary>
     /// Patches <see cref="Scp079Generator.ServerInteract(ReferenceHub, byte)"/>.
     /// Adds the <see cref="Player.ActivatingGenerator"/> event.
@@ -28,7 +35,127 @@ namespace Exiled.Events.Patches.Events.Player
     [HarmonyPatch(typeof(Scp079Generator), nameof(Scp079Generator.ServerInteract))]
     internal static class InteractingGeneratorEvents
     {
-        private static bool Prefix(Scp079Generator __instance, ReferenceHub ply, byte colliderId)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            LocalBuilder player = generator.DeclareLocal(typeof(API.Features.Player));
+
+            Label isClosed = generator.DefineLabel();
+            Label isActivating = generator.DefineLabel();
+            Label check = generator.DefineLabel();
+            Label notAllowed = generator.DefineLabel();
+            Label skip = generator.DefineLabel();
+            Label @break = newInstructions.FindLast(i => i.IsLdarg(0)).labels[0];
+
+            int offset = 1;
+            int index = newInstructions.FindIndex(i => i.Calls(Method(typeof(Stopwatch), nameof(Stopwatch.Stop)))) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Stloc_S, player.LocalIndex),
+            });
+
+            offset = -9;
+            index = newInstructions.FindIndex(i => i.Calls(Method(typeof(Scp079Generator), nameof(Scp079Generator.ServerSetFlag)))) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new(OpCodes.Ldloc_S, player.LocalIndex),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldc_I4_1),
+
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldfld, Field(typeof(Scp079Generator), nameof(Scp079Generator._flags))),
+                new(OpCodes.Ldc_I4_2), // GeneratorFlags.Open
+                new(OpCodes.Callvirt, Method(typeof(Scp079Generator), nameof(Scp079Generator.HasFlag))),
+                new(OpCodes.Brfalse_S, isClosed),
+
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ClosingGeneratorEventArgs))[0]),
+                new(OpCodes.Dup),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.OnClosingGenerator))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(ClosingGeneratorEventArgs), nameof(ClosingGeneratorEventArgs.IsAllowed))),
+                new(OpCodes.Br_S, check),
+
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(OpeningGeneratorEventArgs))[0]).WithLabels(isClosed),
+                new(OpCodes.Dup),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.OnOpeningGenerator))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(OpeningGeneratorEventArgs), nameof(OpeningGeneratorEventArgs.IsAllowed))),
+
+                new CodeInstruction(OpCodes.Brtrue_S, skip).WithLabels(check),
+
+                new CodeInstruction(OpCodes.Ldarg_0).WithLabels(notAllowed),
+                new(OpCodes.Callvirt, Method(typeof(Scp079Generator), nameof(Scp079Generator.RpcDenied))),
+                new(OpCodes.Br_S, @break),
+
+                new CodeInstruction(OpCodes.Nop).WithLabels(skip),
+            });
+
+            offset = 7;
+            index = newInstructions.FindIndex(i => i.LoadsField(Field(typeof(Scp079Generator), nameof(Scp079Generator._requiredPermission)))) + offset;
+
+            // remove base game unlocking
+            newInstructions.RemoveRange(index, 4);
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_S, player.LocalIndex),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldc_I4_1),
+            });
+
+            offset = -1;
+            index = newInstructions.FindLastIndex(i => i.Calls(Method(typeof(Scp079Generator), nameof(Scp079Generator.RpcDenied)))) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_S, player.LocalIndex).MoveLabelsFrom(newInstructions[index]),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldc_I4_0),
+            });
+
+            // remove base game RpcDenied
+            newInstructions.RemoveRange(index + 3, 2);
+
+            index += 3;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(UnlockingGeneratorEventArgs))[0]).MoveLabelsFrom(newInstructions[index]),
+                new(OpCodes.Dup),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.OnUnlockingGenerator))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(UnlockingGeneratorEventArgs), nameof(UnlockingGeneratorEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, notAllowed),
+
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldc_I4_4),
+                new(OpCodes.Ldc_I4_1),
+                new(OpCodes.Callvirt, Method(typeof(Scp079Generator), nameof(Scp079Generator.ServerSetFlag))),
+            });
+
+            offset = -5;
+            index = newInstructions.FindIndex(i => i.Calls(PropertySetter(typeof(Scp079Generator), nameof(Scp079Generator.Activating)))) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Scp079Generator), nameof(Scp079Generator.Activating))),
+                new(OpCodes.Brtrue_S, isActivating),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+            {
+                API.Features.Log.Debug(newInstructions[z]);
+                yield return newInstructions[z];
+            }
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+
+        /*private static bool Prefix(Scp079Generator __instance, ReferenceHub ply, byte colliderId)
         {
             try
             {
@@ -149,6 +276,6 @@ namespace Exiled.Events.Patches.Events.Player
             }
 
             return false;
-        }
+        }*/
     }
 }
