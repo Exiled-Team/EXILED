bf5c588f7a69c7b713c685869eafc4337c58b2c5
IRacle <zornik2006@yandex.ru>
2022-07-01 19:23:50 +0300

    forgot main thing

--- Exiled.Events/Patches/Generic/InventoryControlPatch.cs
+++ Exiled.Events/Patches/Generic/InventoryControlPatch.cs
@@ -40,7 +40,7 @@ namespace Exiled.Events.Patches.Generic
             ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
-            const int offset = -2;
+            const int offset = 1;
             int index = newInstructions.FindIndex(i =>
                 i.opcode == OpCodes.Callvirt &&
                 (MethodInfo)i.operand == Method(typeof(ItemBase), nameof(ItemBase.OnAdded))) + offset;
