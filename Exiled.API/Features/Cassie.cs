02c1ce18078a8db7b636ade5ceece295ad804551
louis1706 <66829532+louis1706@users.noreply.github.com>
2022-07-10 20:43:20 +0200

    little change on MessageTranslated (#1308)
    * little change on MessageTranslated
    
    - i thinks it's would be better like this but not sure
    
    * not needed now
    
    * Oups

--- Exiled.API/Features/Cassie.cs
+++ Exiled.API/Features/Cassie.cs
@@ -65,7 +65,7 @@ namespace Exiled.API.Features
             string[] cassies = message.Split('\n');
             string[] translations = translation.Split('\n');
             for (int i = 0; i < cassies.Length; i++)
-                annoucement.Append($"{translations[i].Replace(' ', ' ')}<alpha=#00> {cassies[i]} </alpha><split>");
+                annoucement.Append($"{translations[i].Replace(' ', ' ')}<size=0> {cassies[i]} </size><split>");
 
             RespawnEffectsController.PlayCassieAnnouncement(annoucement.ToString(), isHeld, isNoisy, isSubtitles);
         }
