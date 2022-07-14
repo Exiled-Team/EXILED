ac55c20933546829b184b47fc7daa9bdfbbb20aa
louis1706 <louismonneyron5@yahoo.com>
2022-07-14 21:38:43 +0200

    Oups

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -333,9 +333,7 @@ namespace Exiled.API.Features
             set
             {
                 runningSpeed = value;
-#pragma warning disable CS0618
                 this.ChangeRunningSpeed(value, false);
-#pragma warning restore CS0618
             }
         }
 
@@ -348,9 +346,7 @@ namespace Exiled.API.Features
             set
             {
                 walkingSpeed = value;
-#pragma warning disable CS0618
                 this.ChangeWalkingSpeed(value, false);
-#pragma warning restore CS0618
             }
         }
 
