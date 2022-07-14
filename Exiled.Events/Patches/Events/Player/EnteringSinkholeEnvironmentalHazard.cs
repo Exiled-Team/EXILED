5a3a26f83923886fc0bec67f92231873cefea3e6
IRacle <zornik2006@yandex.ru>
2022-07-14 20:26:38 +0300

    OnEnter event fixes

--- Exiled.Events/Patches/Events/Player/EnteringSinkholeEnvironmentalHazard.cs
+++ Exiled.Events/Patches/Events/Player/EnteringSinkholeEnvironmentalHazard.cs
@@ -1,5 +1,5 @@
-// -----------------------------------------------------------------------
-// <copyright file="EnteringEnvironmentalHazard.cs" company="Exiled Team">
+﻿// -----------------------------------------------------------------------
+// <copyright file="EnteringSinkholeEnvironmentalHazard.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -19,24 +19,41 @@ namespace Exiled.Events.Patches.Events.Player
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patches <see cref="EnvironmentalHazard.OnEnter(ReferenceHub)"/>.
+    /// Patches <see cref="SinkholeEnvironmentalHazard.OnEnter(ReferenceHub)"/>.
     /// Adds the <see cref="Handlers.Player.EnteringEnvironmentalHazard"/> event.
     /// </summary>
-    [HarmonyPatch(typeof(EnvironmentalHazard), nameof(EnvironmentalHazard.OnEnter))]
-    internal static class EnteringEnvironmentalHazard
+    [HarmonyPatch(typeof(SinkholeEnvironmentalHazard), nameof(SinkholeEnvironmentalHazard.OnEnter))]
+    internal static class EnteringSinkholeEnvironmentalHazard
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
+            LocalBuilder isAllowed = generator.DeclareLocal(typeof(bool));
+
+            Label cnt = generator.DefineLabel();
             Label ret = generator.DefineLabel();
 
-            newInstructions.InsertRange(0, new CodeInstruction[]
+            int offset = -1;
+            int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ret) + offset;
+
+            newInstructions.RemoveAt(index);
+
+            newInstructions.InsertRange(index, new[]
             {
-                new(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Stloc_S, isAllowed.LocalIndex),
+                new(OpCodes.Br_S, cnt),
+            });
+
+            offset = -2;
+            index = newInstructions.FindIndex(i => i.Calls(Method(typeof(EnvironmentalHazard), nameof(EnvironmentalHazard.OnEnter)))) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_1).WithLabels(cnt),
                 new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
                 new(OpCodes.Ldarg_0),
-                new(OpCodes.Ldc_I4_1),
+                new(OpCodes.Ldloc_S, isAllowed.LocalIndex),
                 new(OpCodes.Newobj, GetDeclaredConstructors(typeof(EnteringEnvironmentalHazardEventArgs))[0]),
                 new(OpCodes.Dup),
                 new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnEnteringEnvironmentalHazard))),
