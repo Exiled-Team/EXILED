a7605b666341476adaa02e9cbb80dc73d904433c
RunicLib <mystrunic32@gmail.com>
2022-07-26 02:49:25 +0500

    Revert "Fixed WTF stashes"
    This reverts commit a0ad0c9ae7faf31417a055ff246c0476538e1090.

--- Exiled.API/Features/Door.cs
+++ Exiled.API/Features/Door.cs
@@ -277,7 +277,7 @@ namespace Exiled.API.Features
             {
                 door.IsOpen = false;
                 door.Lock(lockType);
-                MEC.Timing.CallDelayed(duration, () => door.Unlock());
+                MEC.Timing.CallDelayed(duration, () => door.Lock(DoorLockType.None));
             }
         }
 
@@ -299,7 +299,7 @@ namespace Exiled.API.Features
         public static void UnlockAll()
         {
             foreach (Door door in List)
-                door.Unlock();
+                door.Lock(DoorLockType.None);
         }
 
         /// <summary>
@@ -321,7 +321,7 @@ namespace Exiled.API.Features
         public static void UnlockAll(Func<Door, bool> predicate)
         {
             foreach (Door door in Get(predicate))
-                door.Unlock();
+                door.Lock(DoorLockType.None);
         }
 
         /// <summary>
