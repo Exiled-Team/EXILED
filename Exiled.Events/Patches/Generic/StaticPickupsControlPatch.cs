eb48149ccf5ec57c2b3a5fa038ed21ca882b467a
IRacle <zornik2006@yandex.ru>
2022-07-28 12:43:04 +0300

    Better PickupListAdd

--- Exiled.Events/Patches/Generic/StaticPickupsControlPatch.cs
+++ Exiled.Events/Patches/Generic/StaticPickupsControlPatch.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="PickupListAdd.cs" company="Exiled Team">
+// <copyright file="StaticPickupsControlPatch.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -7,36 +7,40 @@
 
 namespace Exiled.Events.Patches.Generic
 {
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
-    using Exiled.API.Features.Items;
     using Exiled.API.Features.Pickups;
 
     using HarmonyLib;
 
     using InventorySystem.Items.Pickups;
 
+    using MapGeneration.Distributors;
+
     using NorthwoodLib.Pools;
+
+    using UnityEngine;
 
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patches <see cref="ItemPickupBase.Awake"/>.
+    /// Patches <see cref="ItemDistributor.CreatePickup"/>.
     /// </summary>
-    [HarmonyPatch(typeof(ItemPickupBase), nameof(ItemPickupBase.Awake))]
-    internal static class PickupListAdd
+    [HarmonyPatch(typeof(ItemDistributor), nameof(ItemDistributor.CreatePickup))]
+    internal class StaticPickupsControlPatch
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            newInstructions.InsertRange(newInstructions.Count - 1, new[]
+            int offset = 1;
+            int index = newInstructions.FindLastIndex(i => i.Calls(Method(typeof(Transform), nameof(Transform.SetParent), new[] { typeof(Transform) }))) + offset;
+
+            newInstructions.InsertRange(index, new[]
             {
-                new CodeInstruction(OpCodes.Ldarg_0),
-                new(OpCodes.Call, Method(typeof(Pickup), nameof(Pickup.Get), new[] { typeof(ItemPickupBase) })),
+                new CodeInstruction(OpCodes.Ldloc_1),
+                new(OpCodes.Callvirt, Method(typeof(Pickup), nameof(Pickup.Get), new[] { typeof(ItemPickupBase) })),
                 new(OpCodes.Pop),
             });
 
