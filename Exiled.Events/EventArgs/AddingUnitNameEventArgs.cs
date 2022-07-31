85da947768e5400e9eb8e30f63c1459a619d66fb
lifyxus <lifyxus@gmail.com>
2022-07-31 19:27:26 +0300

    Add new event

---
+++ Exiled.Events/EventArgs/AddingUnitNameEventArgs.cs
@@ -0,0 +1,38 @@
+﻿namespace Exiled.Events.EventArgs
+{
+    using Respawning;
+
+    /// <summary>
+    /// Contains all information before adding a new unit name
+    /// </summary>
+    public class AddingUnitNameEventArgs : System.EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddingUnitNameEventArgs"/> class.
+        /// </summary>
+        /// <param name="unitName">The generated unit name.</param>
+        /// <param name="team">The <see cref="SpawnableTeamType"/> team.</param>
+        /// <param name="isAllowed">The value indicating whether or not the unit name will be added.</param>
+        public AddingUnitNameEventArgs(string unitName, SpawnableTeamType team, bool isAllowed = true)
+        {
+            UnitName = unitName;
+            Team = team;
+            IsAllowed = isAllowed;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating what the spawnable team is.
+        /// </summary>
+        public SpawnableTeamType Team { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next unit name.
+        /// </summary>
+        public string UnitName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the unit name will be added.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+    }
+}
---
+++ Exiled.Events/Patches/Events/Server/AddingUnitName.cs
@@ -0,0 +1,22 @@
+﻿namespace Exiled.Events.Patches.Events.Server
+{
+    using Exiled.Events.EventArgs;
+    using HarmonyLib;
+    using Respawning;
+    using Respawning.NamingRules;
+
+    /// <summary>
+    /// Patches <see cref="UnitNamingRule.AddCombination"/>.
+    /// Adds the <see cref="Handlers.Server.AddingUnitName"/> event.
+    /// </summary>
+    [HarmonyPatch(typeof(UnitNamingRule), nameof(UnitNamingRule.AddCombination))]
+    internal static class AddingUnitName
+    {
+        private static bool Prefix(string regular, SpawnableTeamType type)
+        {
+            AddingUnitNameEventArgs ev = new AddingUnitNameEventArgs(regular, type);
+            Handlers.Server.OnAddingUnitName(ev);
+            return ev.IsAllowed;
+        }
+    }
+}
