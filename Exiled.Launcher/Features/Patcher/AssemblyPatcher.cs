58d4bbd669eef03b12e8fc0e0be2de64515f9da6
Jesus-QC <jesusquirocampos@gmail.com>
2022-08-02 00:33:09 +0200

    patcher 1st part

---
+++ Exiled.Launcher/Features/Patcher/AssemblyPatcher.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyPatcher.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Exiled.Launcher.Features.Patcher;
+
+public static class AssemblyPatcher
+{
+    public static void Patch(string path)
+    {
+        ModuleDefMD assembly = ModuleDefMD.Load(path);
+
+        if (assembly is null)
+        {
+            Console.WriteLine($"[Patcher] Assembly in {path} not found. Could not patch.");
+            return;
+        }
+
+        TypeDef? serverConsoleDef = HelpMethods.FindServerConsoleDefinition(assembly);
+
+        if (serverConsoleDef is null)
+        {
+            Console.WriteLine("[Patcher] Could not find ServerConsole.");
+            return;
+        }
+
+        MethodDef? startMethodDef = HelpMethods.FindStartMethodDefinition(serverConsoleDef);
+
+        if (startMethodDef is null)
+        {
+            Console.WriteLine("[Patcher] Could not find ServerConsole::Start()");
+            return;
+        }
+
+
+
+    }
+}
---
+++ Exiled.Launcher/Features/Patcher/HelpMethods.cs
@@ -0,0 +1,28 @@
+﻿using dnlib.DotNet;
+
+namespace Exiled.Launcher.Features.Patcher;
+
+public class HelpMethods
+{
+    public static TypeDef? FindServerConsoleDefinition(ModuleDefMD assembly)
+    {
+        foreach (var type in assembly.Types)
+        {
+            if (type.FullName == "ServerConsole")
+                return type;
+        }
+
+        return null;
+    }
+
+    public static MethodDef? FindStartMethodDefinition(TypeDef serverConsole)
+    {
+        foreach (var method in serverConsole.Methods)
+        {
+            if (method.Name == "Start")
+                return method;
+        }
+
+        return null;
+    }
+}
