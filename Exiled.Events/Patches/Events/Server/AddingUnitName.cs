1397e79be59b05f3ec05b2327015b1489971614f
lifyxus <lifyxus@gmail.com>
2022-08-08 17:44:13 +0300

    Transpiler

--- Exiled.Events/Patches/Events/Server/AddingUnitName.cs
+++ Exiled.Events/Patches/Events/Server/AddingUnitName.cs
@@ -7,10 +7,16 @@
 
 namespace Exiled.Events.Patches.Events.Server
 {
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
     using Exiled.Events.EventArgs;
     using HarmonyLib;
+    using NorthwoodLib.Pools;
     using Respawning;
     using Respawning.NamingRules;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="UnitNamingRule.AddCombination"/>.
@@ -19,16 +25,42 @@ namespace Exiled.Events.Patches.Events.Server
     [HarmonyPatch(typeof(UnitNamingRule), nameof(UnitNamingRule.AddCombination))]
     internal static class AddingUnitName
     {
-        private static bool Prefix(ref string regular, SpawnableTeamType type)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            AddingUnitNameEventArgs ev = new AddingUnitNameEventArgs(regular);
-            Handlers.Server.OnAddingUnitName(ev);
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            if (!ev.IsAllowed)
-                return false;
+            Label returnLabel = generator.DefineLabel();
+            Label skipLabel = generator.DefineLabel();
 
-            regular = ev.UnitName;
-            return true;
+            newInstructions.InsertRange(0, new CodeInstruction[]
+            {
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Ldc_I4_1),
+
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(AddingUnitNameEventArgs))[0]),
+
+                new(OpCodes.Dup),
+                new(OpCodes.Dup),
+
+                new(OpCodes.Call, Method(typeof(Handlers.Server), nameof(Handlers.Server.OnAddingUnitName))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(AddingUnitNameEventArgs), nameof(AddingUnitNameEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, skipLabel),
+
+                new(OpCodes.Callvirt, PropertyGetter(typeof(AddingUnitNameEventArgs), nameof(AddingUnitNameEventArgs.UnitName))),
+                new(OpCodes.Starg_S, 1),
+            });
+
+            newInstructions.InsertRange(newInstructions.Count - 1, new[]
+            {
+                new(OpCodes.Br_S, returnLabel),
+                new CodeInstruction(OpCodes.Pop).WithLabels(skipLabel),
+                new CodeInstruction(OpCodes.Ret).WithLabels(returnLabel),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
