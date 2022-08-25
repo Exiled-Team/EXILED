6c1a8a2638cf54cbded5b9419f46eeea3659177c
IRacle <zornik2006@yandex.ru>
2022-08-25 18:36:49 +0300

    -_-

--- Exiled.Events/Patches/Fixes/GrenadePropertiesFix.cs
+++ Exiled.Events/Patches/Fixes/GrenadePropertiesFix.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="GrenadeFuseTimeFix.cs" company="Exiled Team">
+// <copyright file="GrenadePropertiesFix.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
