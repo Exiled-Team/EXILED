3b0817e2aa6df74eb2532d7685e6c70be11e6e79
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2022-08-26 17:55:30 +0200

    Delete assets directory

--- assets/js/c2cd1de3.b564df57.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkexiled_docs=self.webpackChunkexiled_docs||[]).push([[912],{5745:function(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
