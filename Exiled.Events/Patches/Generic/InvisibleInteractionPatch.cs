380882e5fbdfa8aef759ca8ce775b1104218dfb1
microsievert <76050161+microsievert@users.noreply.github.com>
2022-09-03 09:23:32 -0700

    New feature

--- Exiled.Events/Patches/Generic/InvisibleInteractionPatch.cs
+++ Exiled.Events/Patches/Generic/InvisibleInteractionPatch.cs
@@ -8,7 +8,12 @@
 namespace Exiled.Events.Patches.Generic
 {
 #pragma warning disable SA1313
-    using Exiled.API.Enums;
+#pragma warning disable SA1402
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using CustomPlayerEffects;
+
     using Exiled.API.Features;
 
     using HarmonyLib;
@@ -16,35 +21,89 @@ namespace Exiled.Events.Patches.Generic
     using Interactables;
     using Interactables.Verification;
 
+    using NorthwoodLib.Pools;
+
     using UnityEngine;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="StandardDistanceVerification.ServerCanInteract(ReferenceHub, InteractableCollider)"/>.
-    /// Implements <see cref="Player.InteractionInvisibilityRemove"/> property logic.
+    /// Implements <see cref="Player.InvisibilityInteractionProtect"/> property logic.
     /// </summary>
     [HarmonyPatch(typeof(StandardDistanceVerification), nameof(StandardDistanceVerification.ServerCanInteract))]
     internal static class InvisibleInteractionPatch
     {
-        private static bool Prefix(StandardDistanceVerification __instance, ReferenceHub hub, InteractableCollider collider, ref bool __result)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (!__instance._allowHandcuffed && !PlayerInteract.CanDisarmedInteract && hub.interCoordinator.Handcuffed)
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int offset = -1;
+            int index = newInstructions.FindIndex(x => x.LoadsField(Field(typeof(StandardDistanceVerification), nameof(StandardDistanceVerification._cancel268)))) + offset;
+
+            Label skipEffectChangeLabel = generator.DefineLabel();
+            LocalBuilder player = generator.DeclareLocal(typeof(Player));
+
+            newInstructions.RemoveRange(index, 2);
+
+            newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                __result = false;
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Stloc_S, player.LocalIndex),
+                new CodeInstruction(OpCodes.Brfalse_S, skipEffectChangeLabel),
+                new CodeInstruction(OpCodes.Ldloc_S, player.LocalIndex),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.InvisibilityInteractionProtect))),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Ceq),
+            });
 
-                return false;
-            }
+            newInstructions[newInstructions.FindLastIndex(x => x.opcode == OpCodes.Ldc_I4_1)].labels.Add(skipEffectChangeLabel);
 
-            if (Vector3.Distance(hub.playerMovementSync.RealModelPosition, collider.transform.position + collider.transform.TransformDirection(collider.VerificationOffset)) < __instance._maxDistance * 1.4f)
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+
+    /// <summary>
+    /// Patches <see cref="PlayerInteract.OnInteract"/>.
+    /// Implements <see cref="Player.InvisibilityInteractionProtect"/> property logic.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerInteract), nameof(PlayerInteract.OnInteract))]
+    internal static class InvisibleInteractionPlayerInteractPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int offset = -3;
+            int index = newInstructions.FindIndex(x => x.Calls(PropertySetter(typeof(PlayerEffect), nameof(PlayerEffect.Intensity)))) + offset;
+
+            Label label = generator.DefineLabel();
+            LocalBuilder player = generator.DeclareLocal(typeof(Player));
+
+            newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                __result = true;
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayerInteract), nameof(PlayerInteract._hub))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Stloc_S, player.LocalIndex),
+                new CodeInstruction(OpCodes.Brfalse_S, label),
+                new CodeInstruction(OpCodes.Ldloc_S, player.LocalIndex),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.InvisibilityInteractionProtect))),
+                new CodeInstruction(OpCodes.Brtrue_S, label),
+            });
 
-                if (!Player.TryGet(hub, out Player player) || player.InteractionInvisibilityRemove)
-                    return false;
+            newInstructions[newInstructions.Count - 1].labels.Add(label);
 
-                player.DisableEffect(EffectType.Invisible);
-            }
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
 
-            return false;
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
