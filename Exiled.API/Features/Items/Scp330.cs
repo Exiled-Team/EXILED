20ddb8360fbead90e3dff8d9ed74ddd83cceb784
louis1706 <louismonneyron5@yahoo.com>
2022-09-05 10:51:51 +0200

    Revert "=>"
    This reverts commit d985500a4e02471e594c34a59dc6de4edd73433a.

--- Exiled.API/Features/Items/Scp330.cs
+++ Exiled.API/Features/Items/Scp330.cs
@@ -69,7 +69,10 @@ namespace Exiled.API.Features.Items
         /// <summary>
         /// Gets the <see cref="CandyKindID"/>s held in this bag.
         /// </summary>
-        public IReadOnlyCollection<CandyKindID> Candies => Base.Candies.AsReadOnly();
+        public IReadOnlyCollection<CandyKindID> Candies
+        {
+            get => Base.Candies.AsReadOnly();
+        }
 
         /// <summary>
         /// Adds a specific candy to the bag.
