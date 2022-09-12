81ad6f167afb01a862fe9d10c2ded7bc4ece5374
Claudio <72409092+ClaudioPanConQueso@users.noreply.github.com>
2022-09-12 19:12:32 +0200

    Rename warhead

---
+++ Exiled.Events/EventArgs/Warhead/StoppingWarheadEventArgs.cs
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------------------
+// <copyright file="StoppingWarheadEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Warhead
+{
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs.Interfaces;
+
+    /// <summary>
+    ///     Contains all information before stopping the warhead.
+    /// </summary>
+    public class StoppingWarheadEventArgs : IPlayerEvent, IDeniableEvent
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="StoppingWarheadEventArgs" /> class.
+        /// </summary>
+        /// <param name="player">
+        ///     <inheritdoc cref="Player" />
+        /// </param>
+        /// <param name="isAllowed">
+        ///     <inheritdoc cref="IsAllowed" />
+        /// </param>
+        public StoppingWarheadEventArgs(Player player, bool isAllowed = true)
+        {
+            Player = player ?? Server.Host;
+            IsAllowed = isAllowed;
+        }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether or not the warhead can be stopped.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+
+        /// <summary>
+        ///     Gets the player who's going to stop the warhead.
+        /// </summary>
+        public Player Player { get; }
+    }
+}
