70d1459f8f26aca731dcf3e3a1fdff7817fe2c55
IRacle <zornik2006@yandex.ru>
2022-09-20 18:42:32 +0300

    IRoomEvent

---
+++ Exiled.Events/EventArgs/Interfaces/IRoomEvent.cs
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------
+// <copyright file="IRoomEvent.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Interfaces
+{
+    using Exiled.API.Features;
+
+    /// <summary>
+    ///     Event args used for all <see cref="API.Features.Room" /> related events.
+    /// </summary>
+    public interface IRoomEvent : IExiledEvent
+    {
+        /// <summary>
+        ///     Gets the <see cref="API.Features.Room" /> triggering the event.
+        /// </summary>
+        public Room Room { get; }
+    }
+}
