28ced5f31fe269afa6f3f53d96aff8a872fb21e3
louis1706 <louismonneyron5@yahoo.com>
2022-09-20 16:55:00 +0200

    remove uint for BaseCode

--- Exiled.API/Enums/BaseCode.cs
+++ Exiled.API/Enums/BaseCode.cs
@@ -10,56 +10,56 @@ namespace Exiled.API.Enums
     /// <summary>
     /// Firearms base codes.
     /// </summary>
-    public enum BaseCode : uint
+    public enum BaseCode
     {
         /// <summary>
         /// The COM15's base code.
         /// </summary>
-        GunCOM15 = 43u,
+        GunCOM15 = 43,
 
         /// <summary>
         /// The COM18's base code.
         /// </summary>
-        GunCOM18 = 37u,
+        GunCOM18 = 37,
 
         /// <summary>
         /// The Revolver's base code.
         /// </summary>
-        GunRevolver = 297u,
+        GunRevolver = 297,
 
         /// <summary>
         /// The E11SR's base code.
         /// </summary>
-        GunE11SR = 4737569u,
+        GunE11SR = 4737569,
 
         /// <summary>
         /// The Crossvec's base code.
         /// </summary>
-        GunCrossvec = 1105u,
+        GunCrossvec = 1105,
 
         /// <summary>
         /// The FSP9's base code.
         /// </summary>
-        GunFSP9 = 5193u,
+        GunFSP9 = 5193,
 
         /// <summary>
         /// The Logicer's base code.
         /// </summary>
-        GunLogicer = 273u,
+        GunLogicer = 273,
 
         /// <summary>
         /// The AK's base code.
         /// </summary>
-        GunAK = 70161u,
+        GunAK = 70161,
 
         /// <summary>
         /// The Shotgun's base code.
         /// </summary>
-        GunShotgun = 37u,
+        GunShotgun = 37,
 
         /// <summary>
         /// The Disruptor's base code.
         /// </summary>
-        Disruptor = 1u,
+        Disruptor = 1,
     }
 }
