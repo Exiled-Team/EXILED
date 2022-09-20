feef6f255ab268dd2d11a78c9fa6fd12c50ed8bb
IRacle <zornik2006@yandex.ru>
2022-09-20 18:45:19 +0300

    other room events

--- Exiled.Events/EventArgs/Player/ClosingGeneratorEventArgs.cs
+++ Exiled.Events/EventArgs/Player/ClosingGeneratorEventArgs.cs
@@ -4,27 +4,50 @@
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
-
 namespace Exiled.Events.EventArgs.Player
 {
     using Exiled.API.Features;
+    using Exiled.Events.EventArgs.Interfaces;
 
     using MapGeneration.Distributors;
 
     /// <summary>
     ///     Contains all information before a player closes a generator.
     /// </summary>
-    public class ClosingGeneratorEventArgs : OpeningGeneratorEventArgs
+    public class ClosingGeneratorEventArgs : IPlayerEvent, IDeniableEvent, IGeneratorEvent
     {
         /// <summary>
         ///     Initializes a new instance of the <see cref="ClosingGeneratorEventArgs" /> class.
         /// </summary>
-        /// <param name="player">The player who's closing the generator.</param>
-        /// <param name="generator">The <see cref="Scp079Generator" /> instance.</param>
-        /// <param name="isAllowed">Indicates whether or not the generator can be closed.</param>
+        /// <param name="player">
+        ///     <inheritdoc cref="Player" />
+        /// </param>
+        /// <param name="generator">
+        ///     <inheritdoc cref="Generator" />
+        /// </param>
+        /// <param name="isAllowed">
+        ///     <inheritdoc cref="IsAllowed" />
+        /// </param>
         public ClosingGeneratorEventArgs(Player player, Scp079Generator generator, bool isAllowed = true)
-            : base(player, generator, isAllowed)
         {
+            Player = player;
+            Generator = Generator.Get(generator);
+            IsAllowed = isAllowed;
         }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether or not the generator can be closing.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+
+        /// <summary>
+        ///     Gets the generator that is closing.
+        /// </summary>
+        public Generator Generator { get; }
+
+        /// <summary>
+        ///     Gets the player who's closing the generator.
+        /// </summary>
+        public Player Player { get; }
     }
 }
