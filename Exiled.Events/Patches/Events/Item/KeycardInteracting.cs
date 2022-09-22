b0c5ada7718adc37c106b52abb92fdc699fab01d
rayzer <rayzerbrain@gmail.com>
2022-09-21 23:02:05 -0500

    resolve

--- Exiled.Events/Patches/Events/Item/KeycardInteracting.cs
+++ Exiled.Events/Patches/Events/Item/KeycardInteracting.cs
@@ -13,7 +13,7 @@ namespace Exiled.Events.Patches.Events.Item
     using Exiled.API.Features;
     using Exiled.Events;
     using Exiled.Events.Attributes;
-    using Exiled.Events.EventArgs.Player;
+    using Exiled.Events.EventArgs.Item;
 
     using Footprinting;
 
