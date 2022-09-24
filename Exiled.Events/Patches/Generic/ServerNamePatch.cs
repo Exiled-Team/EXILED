aef4df4d088cdb3924c8d321b706b9f6261ea49a
lifyxus <lifyxus@gmail.com>
2022-09-24 23:12:46 +0300

    init

--- Exiled.Events/Patches/Generic/ServerNamePatch.cs
+++ Exiled.Events/Patches/Generic/ServerNamePatch.cs
@@ -5,24 +5,48 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using Exiled.API.Features;
+
 namespace Exiled.Events.Patches.Generic
 {
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
     using HarmonyLib;
+    using NorthwoodLib.Pools;
 
     using static Exiled.Events.Events;
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patch the <see cref="ServerConsole.ReloadServerName"/>.
+    /// Patches the <see cref="ServerConsole.ReloadServerName"/>.
     /// </summary>
     [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.ReloadServerName))]
     internal static class ServerNamePatch
     {
-        private static void Postfix()
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (!Instance.Config.IsNameTrackingEnabled)
-                return;
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+            Label skipLabel = generator.DefineLabel();
 
-            ServerConsole._serverName += $"<color=#00000000><size=1>Exiled {Instance.Version.ToString(3)}</size></color>";
+            newInstructions.InsertRange(newInstructions.Count - 1, new CodeInstruction[]
+            {
+                new(OpCodes.Call, PropertyGetter(typeof(Exiled.Events.Events), nameof(Instance))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Exiled.Events.Events), nameof(Exiled.Events.Events.Config))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Config), nameof(Config.IsNameTrackingEnabled))),
+                new(OpCodes.Brfalse_S, skipLabel),
+
+                new(OpCodes.Ldstr, "{0}<color=#00000000><size=1>Exiled {1}</size></color>"),
+                new(OpCodes.Ldstr, "yes"),
+                //new(OpCodes.Callvirt, PropertyGetter(typeof(ServerConsole), nameof(ServerConsole._serverName))),
+                new(OpCodes.Ldstr, "0.0.1"),
+
+                new(OpCodes.Call, Method(typeof(string), nameof(string.Format), new[] { typeof(string), typeof(object), typeof(object) })),
+                new CodeInstruction(OpCodes.Ret).WithLabels(skipLabel),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
         }
     }
 }
