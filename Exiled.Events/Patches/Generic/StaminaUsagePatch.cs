38f65dd8009d9b5e55857c8fd8d1d6810f11d778
louis1706 <louismonneyron5@yahoo.com>
2022-09-27 17:31:06 +0200

    Convert Stamina::ProcessStamina Prefix to Transpiler

--- Exiled.Events/Patches/Generic/StaminaUsagePatch.cs
+++ Exiled.Events/Patches/Generic/StaminaUsagePatch.cs
@@ -7,16 +7,51 @@
 
 namespace Exiled.Events.Patches.Generic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
     using Exiled.API.Features;
-#pragma warning disable SA1313
+    using Exiled.Events.EventArgs.Player;
     using HarmonyLib;
+    using Mirror;
+    using NorthwoodLib.Pools;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="Stamina.ProcessStamina()"/>.
     /// </summary>
+    /// Adds <see cref="Player.IsUsingStamina"/> implementation.
     [HarmonyPatch(typeof(Stamina), nameof(Stamina.ProcessStamina))]
     internal class StaminaUsagePatch
     {
-        private static bool Prefix(Stamina __instance) => Player.Get(__instance._hub)?.IsUsingStamina ?? true;
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            Label label1 = generator.DefineLabel();
+            Label label2 = generator.DefineLabel();
+
+            newInstructions.InsertRange(
+                0,
+                new CodeInstruction[]
+                {
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Ldfld, Field(typeof(Stamina), nameof(Stamina._hub))),
+                    new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub), })),
+                    new(OpCodes.Dup),
+                    new(OpCodes.Brtrue_S, label1),
+                    new(OpCodes.Pop),
+                    new(OpCodes.Ldc_I4_0),
+                    new(OpCodes.Br_S, label2),
+                    new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(Player), nameof(Player.IsUsingStamina))).WithLabels(label1),
+                    new CodeInstruction(OpCodes.Nop).WithLabels(label2),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
     }
 }
