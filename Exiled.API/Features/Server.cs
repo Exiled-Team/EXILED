3008caf23cf7c04a15f8c6de5766e554e062fb03
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-11-30 04:16:07 +0100

    Changed Server::Tps to Ticks and added Frametime

--- Exiled.API/Features/Server.cs
+++ Exiled.API/Features/Server.cs
@@ -130,10 +130,12 @@ namespace Exiled.API.Features
         /// <summary>
         /// Gets the actual ticks per second of the server.
         /// </summary>
-        public static double Tps
-        {
-            get => Math.Round(1f / Time.smoothDeltaTime);
-        }
+        public static double Ticks => Math.Round(1f / Time.smoothDeltaTime);
+
+        /// <summary>
+        /// Gets the actual frametime of the server.
+        /// </summary>
+        public static double Frametime => Math.Round(1f / Time.deltaTime);
 
         /// <summary>
         /// Gets or sets a value indicating whether or not friendly fire is enabled.
