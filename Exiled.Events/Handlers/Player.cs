b270a757a2b34cfa0c38d3032d60011139d4398f
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-05 17:29:25 +0100

    ThrownGrenade

--- Exiled.Events/Handlers/Player.cs
+++ Exiled.Events/Handlers/Player.cs
@@ -151,7 +151,7 @@ namespace Exiled.Events.Handlers
         /// <summary>
         /// Invoked afer throwing an <see cref="API.Features.Items.Throwable"/>.
         /// </summary>
-        public static event CustomEventHandler<ThrownItemEventArgs> ThrownItem;
+        public static event CustomEventHandler<ThrownGrenadeEventArgs> ThrownItem;
 
         /// <summary>
         /// Invoked before receving a throwing request an <see cref="API.Features.Items.Throwable"/>.
@@ -559,8 +559,8 @@ namespace Exiled.Events.Handlers
         /// <summary>
         /// Called before throwing a grenade.
         /// </summary>
-        /// <param name="ev">The <see cref="ThrownItemEventArgs"/> instance.</param>
-        public static void OnThrowingItem(ThrownItemEventArgs ev) => ThrownItem.InvokeSafely(ev);
+        /// <param name="ev">The <see cref="ThrownGrenadeEventArgs"/> instance.</param>
+        public static void OnThrowingItem(ThrownGrenadeEventArgs ev) => ThrownItem.InvokeSafely(ev);
 
         /// <summary>
         /// Called before receving a throwing request.
