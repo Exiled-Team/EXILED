2bb13e95b75eb331baa332999e9f58641fc7569c
Ficus-x <lifyxus@gmail.com>
2023-01-05 15:27:07 +0300

    Transpiler

--- Exiled.Events/Patches/Generic/ServerNamePatch.cs
+++ Exiled.Events/Patches/Generic/ServerNamePatch.cs
@@ -7,22 +7,53 @@
 
 namespace Exiled.Events.Patches.Generic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
     using HarmonyLib;
+    using NorthwoodLib.Pools;
 
     using static Exiled.Events.Events;
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patch the <see cref="ServerConsole.ReloadServerName"/>.
+    /// Patches the <see cref="ServerConsole.ReloadServerName"/>.
     /// </summary>
     [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.ReloadServerName))]
     internal static class ServerNamePatch
     {
-        private static void Postfix()
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (!Instance.Config.IsNameTrackingEnabled)
-                return;
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+            Label returnLabel = generator.DefineLabel();
 
-            ServerConsole._serverName += $"<color=#00000000><size=1>Exiled {Instance.Version.ToString(3)}</size></color>";
+            newInstructions.InsertRange(newInstructions.Count - 1, new CodeInstruction[]
+            {
+                new(OpCodes.Call, PropertyGetter(typeof(Exiled.Events.Events), nameof(Instance))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Exiled.Events.Events), nameof(Exiled.Events.Events.Config))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Config), nameof(Config.IsNameTrackingEnabled))),
+                new(OpCodes.Brfalse_S, returnLabel),
+
+                new(OpCodes.Ldstr, "{0}<color=#00000000><size=1>Exiled {1}</size></color>"),
+
+                new(OpCodes.Ldsfld, Field(typeof(ServerConsole), nameof(ServerConsole._serverName))),
+
+                new(OpCodes.Call, PropertyGetter(typeof(Exiled.Events.Events), nameof(Instance))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Exiled.Events.Events), nameof(Exiled.Events.Events.Version))),
+                new(OpCodes.Ldc_I4_3),
+                new(OpCodes.Call, Method(typeof(Version), nameof(Version.ToString), new[] { typeof(int) })),
+
+                new(OpCodes.Call, Method(typeof(string), nameof(string.Format), new[] { typeof(string), typeof(object), typeof(object) })),
+                new(OpCodes.Stsfld, Field(typeof(ServerConsole), nameof(ServerConsole._serverName))),
+            });
+
+            newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
