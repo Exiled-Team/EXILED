956a45a541911701066532b4cdbf57e67adaa63a
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-06 14:58:43 +0100

    PR Finished

--- Exiled.CustomItems/API/Features/CustomGrenade.cs
+++ Exiled.CustomItems/API/Features/CustomGrenade.cs
@@ -60,7 +60,7 @@ namespace Exiled.CustomItems.API.Features
         protected override void SubscribeEvents()
         {
             Exiled.Events.Handlers.Player.ThrowingRequest += OnInternalThrowingRequest;
-            Exiled.Events.Handlers.Player.ThrownItem += OnInternalThrownItem;
+            Exiled.Events.Handlers.Player.ThrownProjectile += OnInternalThrownProjectile;
             Exiled.Events.Handlers.Map.ExplodingGrenade += OnInternalExplodingGrenade;
             Exiled.Events.Handlers.Map.ChangedIntoGrenade += OnInternalChangedIntoGrenade;
 
@@ -71,7 +71,7 @@ namespace Exiled.CustomItems.API.Features
         protected override void UnsubscribeEvents()
         {
             Exiled.Events.Handlers.Player.ThrowingRequest -= OnInternalThrowingRequest;
-            Exiled.Events.Handlers.Player.ThrownItem -= OnInternalThrownItem;
+            Exiled.Events.Handlers.Player.ThrownProjectile -= OnInternalThrownProjectile;
             Exiled.Events.Handlers.Map.ExplodingGrenade -= OnInternalExplodingGrenade;
             Exiled.Events.Handlers.Map.ChangedIntoGrenade -= OnInternalChangedIntoGrenade;
 
@@ -89,8 +89,8 @@ namespace Exiled.CustomItems.API.Features
         /// <summary>
         /// Handles tracking thrown custom grenades.
         /// </summary>
-        /// <param name="ev"><see cref="ThrownItemEventArgs"/>.</param>
-        protected virtual void OnThrownItem(ThrownGrenadeEventArgs ev)
+        /// <param name="ev"><see cref="ThrownProjectileEventArgs"/>.</param>
+        protected virtual void OnThrownProjectile(ThrownProjectileEventArgs ev)
         {
         }
 
@@ -121,12 +121,12 @@ namespace Exiled.CustomItems.API.Features
             return;
         }
 
-        private void OnInternalThrownItem(ThrownGrenadeEventArgs ev)
+        private void OnInternalThrownProjectile(ThrownProjectileEventArgs ev)
         {
             if (!Check(ev.Throwable))
                 return;
 
-            OnThrownItem(ev);
+            OnThrownProjectile(ev);
 
             if (ev.Projectile is TimeGrenadeProjectile timeGrenade)
                 timeGrenade.FuseTime = FuseTime;
