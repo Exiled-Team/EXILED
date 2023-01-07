ea0de94eef1ed4a3ab0471f4d8086e8c24e233f6
IRacle <zornik2006@yandex.ru>
2023-01-07 17:49:01 +0300

    `Scp106Role::UsePortal` fix

--- Exiled.API/Features/Roles/Scp106Role.cs
+++ Exiled.API/Features/Roles/Scp106Role.cs
@@ -158,14 +158,21 @@ namespace Exiled.API.Features.Roles
         /// <summary>
         /// Forces SCP-106 to use its portal, and Teleport to position.
         /// </summary>
-        /// <param name="position"> where player will be Teleport.</param>
-        /// <param name="cost"> how mush Vigor it's required and conssume.</param>
+        /// <param name="position">Where player will be Teleport.</param>
+        /// <param name="cost">How mush Vigor it's required and conssume.</param>
         /// <returns>If the player will be teleport.</returns>
         public bool UsePortal(Vector3 position, float cost = 0f)
         {
+            if (Room.Get(position) is not Room room)
+            {
+                // TODO: normal message
+                throw new System.InvalidOperationException("Room doesn't found");
+            }
+
+            HuntersAtlasAbility._syncRoom = room.Identifier;
             HuntersAtlasAbility._syncPos = position;
 
-            if (Vigor > cost)
+            if (Vigor < cost)
                 return false;
 
             HuntersAtlasAbility._estimatedCost = cost;
