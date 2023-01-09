f81b393234476f0d65cae20d837a5db48afbb359
louis1706 <66829532+louis1706@users.noreply.github.com>
2023-01-09 20:45:04 +0100

    Update Exiled.API/Extensions/ItemExtensions.cs
    Co-authored-by: Pietro <iopietro@hotmail.it>

--- Exiled.API/Extensions/ItemExtensions.cs
+++ Exiled.API/Extensions/ItemExtensions.cs
@@ -330,6 +330,7 @@ namespace Exiled.API.Extensions
             if (pickup is not FirearmPickup firearm)
                 return 0;
 
+
             byte ammo = GetMaxAmmo(firearm.Info.ItemId.GetFirearmType());
 
             if (firearm.Status.Flags.HasFlag(FirearmStatusFlags.Chambered))
