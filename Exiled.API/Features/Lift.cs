da4a136b5c2732ae188e8c20e9419977bbaa4937
Thunder <thundermaker300@gmail.com>
2023-01-16 09:48:46 -0500

    Lift API + Player.Lift property

--- Exiled.API/Features/Lift.cs
+++ Exiled.API/Features/Lift.cs
@@ -12,7 +12,7 @@ namespace Exiled.API.Features
     using System.Linq;
 
     using Exiled.API.Enums;
-
+    using Exiled.API.Features.Pools;
     using Interactables.Interobjects;
     using Interactables.Interobjects.DoorUtils;
 
@@ -34,7 +34,7 @@ namespace Exiled.API.Features
         /// <summary>
         /// Internal list that contains all ElevatorDoor for current group.
         /// </summary>
-        private readonly List<ElevatorDoor> internalDoorsList = new();
+        private readonly List<ElevatorDoor> internalDoorsList = ListPool<ElevatorDoor>.Pool.Get();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Lift"/> class.
@@ -48,6 +48,11 @@ namespace Exiled.API.Features
             foreach (ElevatorDoor door in ElevatorDoor.AllElevatorDoors.First(elevator => elevator.Key == Base.AssignedGroup).Value)
                 internalDoorsList.Add(door);
         }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="Lift"/> class.
+        /// </summary>
+        ~Lift() => ListPool<ElevatorDoor>.Pool.Return(internalDoorsList);
 
         /// <summary>
         /// Gets a <see cref="IEnumerable{T}"/> of <see cref="Lift"/> which contains all the <see cref="Lift"/> instances.
@@ -111,6 +116,11 @@ namespace Exiled.API.Features
             get => Base._curSequence;
             set => Base._curSequence = value;
         }
+
+        /// <summary>
+        /// Gets the <see cref="UnityEngine.Bounds"/> representing the space inside the lift.
+        /// </summary>
+        public Bounds Bounds => Base.WorldspaceBounds;
 
         /// <summary>
         /// Gets the lift's <see cref="ElevatorType"/>.
@@ -217,6 +227,13 @@ namespace Exiled.API.Features
         public static Lift Get(GameObject gameObject) => Get(lift => lift.GameObject == gameObject).FirstOrDefault();
 
         /// <summary>
+        /// Gets the <see cref="Lift"/> belonging to the <see cref="Vector3"/>, if any.
+        /// </summary>
+        /// <param name="position">The <see cref="Vector3"/>.</param>
+        /// <returns>A <see cref="Lift"/> or <see langword="null"/> if not found.</returns>
+        public static Lift Get(Vector3 position) => Get(lift => lift.Bounds.Contains(position)).FirstOrDefault();
+
+        /// <summary>
         /// Gets a <see cref="IEnumerable{T}"/> of <see cref="Lift"/> filtered based on a predicate.
         /// </summary>
         /// <param name="predicate">The condition to satify.</param>
@@ -274,6 +291,13 @@ namespace Exiled.API.Features
                 Base.RefreshLocks(Group, door);
             }
         }
+
+        /// <summary>
+        /// Returns whether or not the provided <see cref="Vector3">position</see> is inside the lift.
+        /// </summary>
+        /// <param name="point">The position.</param>
+        /// <returns><see langword="true"/> if the point is inside the elevator. Otherwise, <see langword="false"/>.</returns>
+        public bool IsInElevator(Vector3 point) => Bounds.Contains(point);
 
         /// <inheritdoc/>
         public override bool Equals(object obj) => Base.Equals(obj);
