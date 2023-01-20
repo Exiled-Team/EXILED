4241566f75d348d294032ce13fd76f28cbd551bb
Thunder <thundermaker300@gmail.com>
2023-01-19 21:41:33 -0500

    Build error

--- Exiled.API/Features/Roles/Role.cs
+++ Exiled.API/Features/Roles/Role.cs
@@ -10,6 +10,7 @@ namespace Exiled.API.Features.Roles
     using System;
     using System.Collections;
     using System.Collections.Generic;
+
     using Enums;
 
     using Exiled.API.Features.Core;
