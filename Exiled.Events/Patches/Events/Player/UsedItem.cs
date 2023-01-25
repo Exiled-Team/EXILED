b4b6e1ce60a5400513b5fa733b5030e68f51ed46
IRacle <zornik2006@yandex.ru>
2023-01-25 17:16:10 +0300

    Revert "items", fucking abstraction
    This reverts commit 149abb120e5b40cca9249a5f70c956dc83bd377a.

--- Exiled.Events/Patches/Events/Player/UsedItem.cs
+++ Exiled.Events/Patches/Events/Player/UsedItem.cs
@@ -22,12 +22,52 @@ namespace Exiled.Events.Patches.Events.Player
 
     using static HarmonyLib.AccessTools;
 
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1402 // File may only contain a single type
+
     /// <summary>
     ///     Patches <see cref="Consumable.ServerOnUsingCompleted" />
     ///     Adds the <see cref="Handlers.Player.UsedItem" /> event.
     /// </summary>
-    [HarmonyPatch(typeof(UsableItem), nameof(UsableItem.ServerOnUsingCompleted))]
+    [HarmonyPatch(typeof(Consumable), nameof(Consumable.ServerOnUsingCompleted))]
     internal static class UsedItem
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            const int index = 0;
+
+            newInstructions.InsertRange(index, InstructionsToInject());
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
+        internal static List<CodeInstruction> InstructionsToInject() => new List<CodeInstruction>
+        {
+            // Player.Get(this.Owner)
+            new(OpCodes.Ldarg_0),
+            new(OpCodes.Callvirt, PropertyGetter(typeof(ItemBase), nameof(ItemBase.Owner))),
+            new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+            // this
+            new(OpCodes.Ldarg_0),
+
+            // UsedItemEventArgs ev = new(Player, UsableItem)
+            new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UsedItemEventArgs))[0]),
+            new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnUsedItem))),
+        };
+    }
+
+    /// <summary>
+    ///     Patches <see cref="Scp268.ServerOnUsingCompleted" />
+    ///     Adds the <see cref="Handlers.Player.UsedItem" /> event.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp268), nameof(Scp268.ServerOnUsingCompleted))]
+    internal static class UsedItem268
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
@@ -35,20 +75,7 @@ namespace Exiled.Events.Patches.Events.Player
 
             const int index = 0;
 
-            newInstructions.InsertRange(index, new[]
-            {
-                // Player.Get(this.Owner)
-                new CodeInstruction(OpCodes.Ldarg_0),
-                new(OpCodes.Callvirt, PropertyGetter(typeof(ItemBase), nameof(ItemBase.Owner))),
-                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
-
-                // this
-                new(OpCodes.Ldarg_0),
-
-                // UsedItemEventArgs ev = new(Player, UsableItem)
-                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UsedItemEventArgs))[0]),
-                new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnUsedItem))),
-            });
+            newInstructions.InsertRange(index, UsedItem.InstructionsToInject());
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
