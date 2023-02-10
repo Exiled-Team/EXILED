2ef921c45f57e73ec07db70d78934c450816bab8
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-02-10 23:56:46 +0100

    Nickname Issue Fix

---
+++ Exiled.Events/Patches/Fixes/PlayerNicknameFix.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerNicknameFix.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Fixes
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using Exiled.API.Features.Pools;
+    using HarmonyLib;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patch <see cref="NicknameSync.CombinedName"/> to fix the player nickname when changing it.
+    /// </summary>
+    [HarmonyPatch(typeof(NicknameSync), nameof(NicknameSync.CombinedName), MethodType.Getter)]
+    internal static class PlayerNicknameFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get();
+
+            newInstructions.AddRange(new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(NicknameSync), nameof(NicknameSync.MyNick))),
+                new CodeInstruction(OpCodes.Ret),
+            });
+
+            foreach (CodeInstruction instruction in newInstructions)
+                yield return instruction;
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
