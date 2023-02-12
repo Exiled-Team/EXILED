038afa375ebda53159be49cc3bd15b8fc684876c
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-02-12 21:11:06 +0100

    SendingCall Event

---
+++ Exiled.Events/EventArgs/Scp049/SendingCallEventArgs.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SendingCallEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Scp049
+{
+    using API.Features;
+
+    using Interfaces;
+
+    /// <summary>
+    /// Contains all information before SCP-049 Call is activated.
+    /// </summary>
+    public class SendingCallEventArgs : IPlayerEvent, IDeniableEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendingCallEventArgs"/> class.
+        /// </summary>
+        /// <param name="player"><inheritdoc cref="Player"/></param>
+        /// <param name="duration"><inheritdoc cref="Duration"/></param>
+        /// <param name="isAllowed"><inheritdoc cref="IsAllowed"/></param>
+        public SendingCallEventArgs(Player player, float duration = 20f, bool isAllowed = true)
+        {
+            Player = player;
+            Duration = duration;
+            IsAllowed = isAllowed;
+        }
+
+        /// <summary>
+        /// Gets the player who is controlling SCP-049.
+        /// </summary>
+        public Player Player { get; }
+
+        /// <summary>
+        /// Gets or sets how long 049 duration during a revive should be.
+        /// </summary>
+        public float Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the server will send 049 information on the recall.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+    }
+}
