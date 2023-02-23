7aeaaa2b2f06195519711640c1dc1c670e5c39da
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-02-23 04:14:23 +0100

    Bring back the old patch

--- Exiled.Events/Patches/Events/Scp106/UsingAtlas.cs
+++ Exiled.Events/Patches/Events/Scp106/UsingAtlas.cs
@@ -26,7 +26,7 @@ namespace Exiled.Events.Patches.Events.Scp106
 
     /// <summary>
     ///     Patches <see cref="Scp106HuntersAtlasAbility.ServerProcessCmd" />.
-    ///     Adds the <see cref="Handlers.Scp106.UsingAtlas" /> event.
+    ///     Adds the <see cref="Handlers.Scp106.Teleporting" /> event.
     /// </summary>
     [HarmonyPatch(typeof(Scp106HuntersAtlasAbility), nameof(Scp106HuntersAtlasAbility.ServerProcessCmd))]
     internal static class UsingAtlas
@@ -41,7 +41,7 @@ namespace Exiled.Events.Patches.Events.Scp106
             int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stloc_0) + offset;
 
             // Declare TeleportingEventArgs, to be able to store its instance.
-            LocalBuilder ev = generator.DeclareLocal(typeof(UsingHunterAtlasEventArgs));
+            LocalBuilder ev = generator.DeclareLocal(typeof(TeleportingEventArgs));
 
             // Get the count to find the previous index
             int oldCount = newInstructions.Count;
@@ -73,22 +73,22 @@ namespace Exiled.Events.Patches.Events.Scp106
                     new(OpCodes.Ldc_I4_1),
 
                     // TeleportingEventArgs ev = new(Player, Vector3, bool)
-                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UsingHunterAtlasEventArgs))[0]),
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(TeleportingEventArgs))[0]),
                     new(OpCodes.Dup),
                     new(OpCodes.Dup),
                     new(OpCodes.Stloc_S, ev.LocalIndex),
 
                     // Scp106.OnTeleporting(ev)
-                    new(OpCodes.Call, Method(typeof(Scp106), nameof(Scp106.OnUsingAtlas))),
+                    new(OpCodes.Call, Method(typeof(Scp106), nameof(Scp106.OnTeleporting))),
 
                     // if (!ev.IsAllowed)
                     //    return;
-                    new(OpCodes.Callvirt, PropertyGetter(typeof(UsingHunterAtlasEventArgs), nameof(UsingHunterAtlasEventArgs.IsAllowed))),
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(TeleportingEventArgs), nameof(TeleportingEventArgs.IsAllowed))),
                     new(OpCodes.Brfalse_S, returnLabel),
 
                     // position = ev.Position
                     new(OpCodes.Ldloc_S, ev.LocalIndex),
-                    new(OpCodes.Callvirt, PropertyGetter(typeof(UsingHunterAtlasEventArgs), nameof(UsingHunterAtlasEventArgs.Position))),
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(TeleportingEventArgs), nameof(TeleportingEventArgs.Position))),
                     new(OpCodes.Stloc_0),
                 });
 
