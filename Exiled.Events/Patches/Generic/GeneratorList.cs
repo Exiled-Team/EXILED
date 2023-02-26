f5a5799aeaa5481dc994be60518b34f6011be703
IRacle <zornik2006@yandex.ru>
2023-02-26 21:45:04 +0300

    generator list consistency

--- Exiled.Events/Patches/Generic/GeneratorList.cs
+++ Exiled.Events/Patches/Generic/GeneratorList.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="GeneratorListRemove.cs" company="Exiled Team">
+// <copyright file="GeneratorList.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -7,6 +7,7 @@
 
 namespace Exiled.Events.Patches.Generic
 {
+#pragma warning disable SA1402
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
@@ -18,6 +19,33 @@ namespace Exiled.Events.Patches.Generic
     using MapGeneration.Distributors;
 
     using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="Scp079Generator.Start"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp079Generator), nameof(Scp079Generator.Start))]
+    internal class GeneratorList
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(codeInstructions);
+
+            // new Generator(this)
+            newInstructions.InsertRange(
+                0,
+                new CodeInstruction[]
+                {
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(Generator))[0]),
+                    new(OpCodes.Pop),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
 
     /// <summary>
     /// Patches <see cref="Scp079Generator.OnDestroy"/>.
