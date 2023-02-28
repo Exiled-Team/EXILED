49b45731da3e16b5539afaad9fc5b5820f1da357
louis1706 <66829532+louis1706@users.noreply.github.com>
2023-02-28 21:31:44 +0100

    Update Exiled.API/Features/Player.cs
    Co-authored-by: Thunder <thundermaker300@gmail.com>

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -493,7 +493,7 @@ namespace Exiled.API.Features
         /// <summary>
         /// Gets or sets the relative player's position.
         /// </summary>
-        /// <remarks>value will be default if the player it's not an <see cref="FpcRole"/>.</remarks>
+        /// <remarks>The value will be default if the player's role is not an <see cref="FpcRole"/>.</remarks>
         public RelativePosition RelativePosition
         {
             get => Role is FpcRole fpcRole ? fpcRole.RelativePosition : default;
