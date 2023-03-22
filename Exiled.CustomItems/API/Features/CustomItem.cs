eb95ce2b3b0851c2874c1e11c42b126a77d93d08
Joker <amathor929@gmail.com>
2023-03-22 12:45:01 -0700

    Fix build errors + version bump

--- Exiled.CustomItems/API/Features/CustomItem.cs
+++ Exiled.CustomItems/API/Features/CustomItem.cs
@@ -808,7 +808,7 @@ namespace Exiled.CustomItems.API.Features
             typeLookupTable.Add(GetType(), this);
             stringLookupTable.Add(Name, this);
             idLookupTable.Add(Id, this);
-            
+
             SubscribeEvents();
         }
 
