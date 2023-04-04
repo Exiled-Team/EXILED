7c4d9923abf9d220474c1651f1ff765716423adc
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-04-04 19:14:42 +0300

    Removing 012 room

--- Exiled.API/Extensions/SpawnExtensions.cs
+++ Exiled.API/Extensions/SpawnExtensions.cs
@@ -78,7 +78,6 @@ namespace Exiled.API.Extensions
         /// <returns>Returns the door name.</returns>
         public static string GetDoorName(this SpawnLocationType spawnLocation) => spawnLocation switch
         {
-            SpawnLocationType.Inside012 => "012",
             SpawnLocationType.Inside096 => "096",
             SpawnLocationType.Inside914 => "914",
             SpawnLocationType.InsideHid => "HID",
@@ -92,8 +91,6 @@ namespace Exiled.API.Extensions
             SpawnLocationType.InsideIntercom => "INTERCOM",
             SpawnLocationType.InsideHidRight => "HID_RIGHT",
             SpawnLocationType.Inside079First => "079_FIRST",
-            SpawnLocationType.Inside012Bottom => "012_BOTTOM",
-            SpawnLocationType.Inside012Locker => "012_LOCKER",
             SpawnLocationType.Inside049Armory => "049_ARMORY",
             SpawnLocationType.Inside173Armory => "173_ARMORY",
             SpawnLocationType.Inside173Bottom => "173_BOTTOM",
