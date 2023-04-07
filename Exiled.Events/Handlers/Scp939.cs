6ba6ff9a14c15fac8f6975b835619352feee8d60
VALERA771 <val.germanov@gmail.com>
2023-04-07 19:36:55 +0300

    Fix doc

--- Exiled.Events/Handlers/Scp939.cs
+++ Exiled.Events/Handlers/Scp939.cs
@@ -23,7 +23,7 @@ namespace Exiled.Events.Handlers
         public static event CustomEventHandler<ChangingFocusEventArgs> ChangingFocus;
 
         /// <summary>
-        ///     Invoked before SCP-939 uses its lunge ability.
+        ///     Invoked after SCP-939 uses its lunge ability.
         /// </summary>
         public static event CustomEventHandler<LungedEventArgs> Lunged;
 
@@ -54,7 +54,7 @@ namespace Exiled.Events.Handlers
         public static void OnChangingFocus(ChangingFocusEventArgs ev) => ChangingFocus.InvokeSafely(ev);
 
         /// <summary>
-        ///     Called before SCP-939 uses its lunge ability.
+        ///     Called after SCP-939 uses its lunge ability.
         /// </summary>
         /// <param name="ev">The <see cref="LungedEventArgs" /> instance.</param>
         public static void OnLunged(LungedEventArgs ev) => Lunged.InvokeSafely(ev);
