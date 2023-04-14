dbacae7470bf2bce425833d1565eaae38777e063
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-04-14 22:03:25 +0300

    Handler

--- Exiled.Events/Handlers/Scp0492.cs
+++ Exiled.Events/Handlers/Scp0492.cs
@@ -22,9 +22,31 @@ namespace Exiled.Events.Handlers
         public static event Events.CustomEventHandler<TriggeringBloodlustEventArgs> TriggeringBloodlust;
 
         /// <summary>
+        /// Called before 049-2 attacks a player.
+        /// </summary>
+        public static event Events.CustomEventHandler<AttackingEventArgs> Atacking;
+
+        /// <summary>
+        /// Called before 049-2 gets his benefits from consuming ability.
+        /// </summary>
+        public static event Events.CustomEventHandler<ConsumingEventArgs> Consuming;
+
+        /// <summary>
         /// Called before a player triggers the bloodlust effect for 049-2.
         /// </summary>
         /// <param name="ev">The <see cref="TriggeringBloodlustEventArgs"/> instance.</param>
         public static void OnTriggeringBloodlust(TriggeringBloodlustEventArgs ev) => TriggeringBloodlust.InvokeSafely(ev);
+
+        /// <summary>
+        /// Invokes before 049-2 attacks a player.
+        /// </summary>
+        /// <param name="ev"><see cref="AttackingEventArgs"/> instance.</param>
+        public static void OnAttacking(AttackingEventArgs ev) => Atacking.InvokeSafely(ev);
+
+        /// <summary>
+        /// Invokes before 049-2 gets his benefits from consuming ability.
+        /// </summary>
+        /// <param name="ev"><inheritdoc cref="ConsumingEventArgs"/> instance.</param>
+        public static void OnConsuming(ConsumingEventArgs ev) => Consuming.InvokeSafely(ev);
     }
 }
