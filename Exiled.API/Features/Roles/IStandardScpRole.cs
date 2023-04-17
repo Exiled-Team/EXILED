6035fd2e1dc85e607d48e6dbee36c96f32494e0c
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-04-17 03:10:42 +0200

    Interface

---
+++ Exiled.API/Features/Roles/IStandardScpRole.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IStandardScpRole.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.Roles
+{
+    /// <summary>
+    /// Represents a Scp Role.
+    /// </summary>
+    public interface IStandardScpRole
+    {
+        /// <summary>
+        /// Trigger the attack ability of a Scp Role.
+        /// </summary>
+        /// <param name="player">The player to attack.</param>
+        public void Attack(Player player);
+    }
+}
