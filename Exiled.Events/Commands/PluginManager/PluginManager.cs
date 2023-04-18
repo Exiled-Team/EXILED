64807227a2080bdb781887cd07a9cb100da46508
Monaldcry7788 <monaldi.christian27@gmail.com>
2023-04-18 15:44:32 +0200

    Added plugin manager command section. Show, enable or disable a plugin.

---
+++ Exiled.Events/Commands/PluginManager/PluginManager.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginManager.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Commands.PluginManager
+{
+    using System;
+
+    using CommandSystem;
+
+    /// <summary>
+    /// The plugin manager.
+    /// </summary>
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    public class PluginManager : ParentCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginManager"/> class.
+        /// </summary>
+        public PluginManager()
+        {
+            LoadGeneratedCommands();
+        }
+
+        /// <inheritdoc/>
+        public override string Command { get; } = "pluginmanager";
+
+        /// <inheritdoc/>
+        public override string[] Aliases { get; } = new[] { "plymanager", "plmanager", "pmanager", "plym" };
+
+        /// <inheritdoc/>
+        public override string Description { get; } = "Manage plugin. Enable, disable and show plugins.";
+
+        /// <inheritdoc/>
+        public override void LoadGeneratedCommands()
+        {
+            RegisterCommand(Show.Instance);
+            RegisterCommand(Enable.Instance);
+            RegisterCommand(Disable.Instance);
+        }
+
+        /// <inheritdoc/>
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Please, specify a valid subcommand! Available ones: enable, disable, show";
+            return false;
+        }
+    }
+}
