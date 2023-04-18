9a44a94a91ed8608e33f6a4c47a9a3b7b9c19497
Monaldcry7788 <monaldi.christian27@gmail.com>
2023-04-18 15:59:57 +0200

    Removed old voice chat patch from ChangingSpeakerStatusAndVoiceChatting.cs and created VoiceChatting.cs for use instead of VoiceModuleBase VoiceTransceiver.ServerReceiveMessage for get the actual message.

---
+++ Exiled.Events/Patches/Events/Player/VoiceChatting.cs
@@ -0,0 +1,73 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VoiceChatting.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Player
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using API.Features.Pools;
+
+    using Exiled.Events.EventArgs.Player;
+
+    using HarmonyLib;
+
+    using Mirror;
+
+    using VoiceChat.Networking;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// patches <see cref="VoiceTransceiver.ServerReceiveMessage(NetworkConnection, VoiceMessage)"/> to add the <see cref="Handlers.Player.VoiceChatting"/> event.
+    /// </summary>
+    [HarmonyPatch(typeof(VoiceTransceiver), nameof(VoiceTransceiver.ServerReceiveMessage))]
+    internal static class VoiceChatting
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            Label retLabel = generator.DefineLabel();
+
+            LocalBuilder ev = generator.DeclareLocal(typeof(VoiceChattingEventArgs));
+
+            newInstructions.InsertRange(0, new CodeInstruction[]
+            {
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Ldfld, Field(typeof(VoiceMessage), nameof(VoiceMessage.Speaker))),
+                new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+
+                new(OpCodes.Ldarg_1),
+
+                new(OpCodes.Ldc_I4_1),
+
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(VoiceChattingEventArgs))[0]),
+                new(OpCodes.Dup),
+                new(OpCodes.Dup),
+                new(OpCodes.Stloc_S, ev.LocalIndex),
+
+                new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnVoiceChatting))),
+
+                new(OpCodes.Callvirt, PropertyGetter(typeof(VoiceChattingEventArgs), nameof(VoiceChattingEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retLabel),
+
+                new(OpCodes.Ldloc_S, ev.LocalIndex),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(VoiceChattingEventArgs), nameof(VoiceChattingEventArgs.VoiceMessage))),
+                new(OpCodes.Stloc_1),
+
+            });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(retLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
