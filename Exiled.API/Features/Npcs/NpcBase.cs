7ee748586b2c4ea14db9db4e6d7f95c267e7af77
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-04-20 13:12:04 +0200

    WIP

---
+++ Exiled.API/Features/Npcs/NpcBase.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NpcBase.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.Npcs
+{
+    /// <summary>
+    /// Npc API Base
+    /// </summary>
+    public class NpcBase // WIP
+    {
+    }
+}
