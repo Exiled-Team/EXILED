c5b607b93842d4c07bba6e859510d43dc75a5bb5
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-04-26 00:29:51 +0300

    and again

--- Exiled.Events/EventArgs/Server/RespawningTeamEventArgs.cs
+++ Exiled.Events/EventArgs/Server/RespawningTeamEventArgs.cs
@@ -41,7 +41,6 @@ namespace Exiled.Events.EventArgs.Server
         /// </param>
         public RespawningTeamEventArgs(List<Player> players, int maxRespawn, SpawnableTeamType nextKnownTeam, bool isAllowed = true)
         {
-            players.RemoveRange(maxRespawn, players.Count - maxRespawn);
             Players = players;
             MaximumRespawnAmount = maxRespawn;
 
