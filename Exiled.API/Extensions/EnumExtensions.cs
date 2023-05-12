0cd7aeb995a793eea6a0a4d692bf48f9b8f7ac4c
louis1706 <louismonneyron5@yahoo.com>
2023-05-12 18:44:26 +0200

    Doc

--- Exiled.API/Extensions/EnumExtensions.cs
+++ Exiled.API/Extensions/EnumExtensions.cs
@@ -1,13 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// -----------------------------------------------------------------------
+// <copyright file="EnumExtensions.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace Exiled.API.Extensions
 {
+    using System;
+
+    /// <summary>
+    /// Contains an extension method to <see cref="System.Enum"/>.
+    /// </summary>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Converting an Enum to a Bool Array.
+        /// </summary>
+        /// <typeparam name="T">The returned object type.</typeparam>
+        /// <param name="enum">The enum to convert.</param>
+        /// <returns>A Array of <see cref="bool"/> determining the value.</returns>
         public static bool[] ToBoolArray<T>(this T @enum)
             where T : Enum
         {
@@ -20,6 +32,12 @@ namespace Exiled.API.Extensions
             return arr;
         }
 
+        /// <summary>
+        /// Converting a Bool Array to an Enum.
+        /// </summary>
+        /// <typeparam name="T">The returned object type.</typeparam>
+        /// <param name="arr">The array of bool to convert.</param>
+        /// <returns>The <typeparamref name="T"/> module that was requested.</returns>
         public static T ToFlagEnum<T>(this bool[] arr)
             where T : struct
         {
