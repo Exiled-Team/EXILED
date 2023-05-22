14c2cd5f7c8fa934f73f8bf32dfe18e0a44df131
louis1706 <louismonneyron5@yahoo.com>
2023-05-22 14:51:28 +0200

    Fix

--- Exiled.Events/Patches/Events/Scp939/PlayingSound.cs
+++ Exiled.Events/Patches/Events/Scp939/PlayingSound.cs
@@ -36,6 +36,7 @@ namespace Exiled.Events.Patches.Events.Scp939
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
             LocalBuilder option = generator.DeclareLocal(typeof(byte));
+            LocalBuilder ev = generator.DeclareLocal(typeof(PlayingSoundEventArgs));
 
             Label ret = generator.DefineLabel();
 
@@ -45,12 +46,12 @@ namespace Exiled.Events.Patches.Events.Scp939
             newInstructions.InsertRange(index, new[]
             {
                 // option = reader.ReadByte()
-                new(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Ldarg_1),
                 new(OpCodes.Callvirt, Method(typeof(NetworkReader), nameof(NetworkReader.ReadByte))),
                 new(OpCodes.Stloc_S, option.LocalIndex),
 
                 // this.Owner
-                new CodeInstruction(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_0),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(EnvironmentalMimicry), nameof(EnvironmentalMimicry.Owner))),
 
                 // EnvironmentalMimicry.Sequences[option]
@@ -60,12 +61,12 @@ namespace Exiled.Events.Patches.Events.Scp939
                 new(OpCodes.Ldelem_Ref),
 
                 // this.Cooldown.IsReady
-                new CodeInstruction(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(EnvironmentalMimicry), nameof(EnvironmentalMimicry.Cooldown))),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(AbilityCooldown), nameof(AbilityCooldown.IsReady))),
 
                 // this._activationCooldown
-                new CodeInstruction(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(EnvironmentalMimicry), nameof(EnvironmentalMimicry._activationCooldown))),
 
                 // true
@@ -74,6 +75,8 @@ namespace Exiled.Events.Patches.Events.Scp939
                 // PlayingSoundEventArgs ev = new(referenceHub, sound, isReady, cooldown, isAllowed);
                 new(OpCodes.Newobj, GetDeclaredConstructors(typeof(PlayingSoundEventArgs))[0]),
                 new(OpCodes.Dup),
+                new(OpCodes.Dup),
+                new(OpCodes.Stloc_S, ev.LocalIndex),
 
                 // Scp939.OnPlayingSound(ev);
                 new(OpCodes.Call, Method(typeof(Scp939), nameof(Scp939.OnPlayingSound))),
@@ -84,12 +87,26 @@ namespace Exiled.Events.Patches.Events.Scp939
                 new(OpCodes.Brfalse_S, ret),
             });
 
+            // Replace reader.ReadByte() to option
             offset = -1;
-            index = newInstructions.FindIndex(i => i.operand == (object)PropertyGetter(typeof(NetworkReader), nameof(NetworkReader.ReadByte))) + offset;
+            index = newInstructions.FindIndex(i => i.operand == (object)Method(typeof(NetworkReader), nameof(NetworkReader.ReadByte))) + offset;
 
             newInstructions.RemoveRange(index, 2);
 
             newInstructions.Insert(index, new(OpCodes.Ldloc, option.LocalIndex));
+
+            offset = -1;
+
+            // Replace this._activationCooldown to ev.Cooldown
+            index = newInstructions.FindIndex(i => i.operand == (object)Field(typeof(EnvironmentalMimicry), nameof(EnvironmentalMimicry._activationCooldown))) + offset;
+
+            newInstructions.RemoveRange(index, 2);
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc, ev.LocalIndex),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(PlayingSoundEventArgs), nameof(PlayingSoundEventArgs.Cooldown))),
+            });
 
             newInstructions[newInstructions.Count - 1].labels.Add(ret);
 
