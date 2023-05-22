7d293588e4662c0a2664cb312e077a0a72ec4ab8
VALERA771 <val.germanov@gmail.com>
2023-05-22 15:10:44 +0300

    doc

--- Exiled.Events/Patches/Events/Scp049/Attacking.cs
+++ Exiled.Events/Patches/Events/Scp049/Attacking.cs
@@ -41,21 +41,28 @@ namespace Exiled.Events.Patches.Events.Scp049
                 index,
                 new CodeInstruction[]
                 {
+                    // Player player = Player.Get(this.Owner);
                     new(OpCodes.Ldarg_0),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(Scp049AttackAbility), nameof(Scp049AttackAbility.Owner))),
                     new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
+                    // Player target = Player.Get(this._target);
                     new(OpCodes.Ldarg_0),
                     new(OpCodes.Ldfld, Field(typeof(Scp049AttackAbility), nameof(Scp049AttackAbility._target))),
                     new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
+                    // true
                     new(OpCodes.Ldc_I4_1),
 
+                    // AttackingEventArgs ev = new(player, target, true);
                     new(OpCodes.Newobj, GetDeclaredConstructors(typeof(AttackingEventArgs))[0]),
                     new(OpCodes.Dup),
 
+                    // Handlers.Scp049.OnAttacking(ev);
                     new(OpCodes.Call, Method(typeof(Handlers.Scp049), nameof(Handlers.Scp049.OnAttacking))),
 
+                    // if (!ev.IsAllowed)
+                    //      return;
                     new(OpCodes.Callvirt, PropertyGetter(typeof(AttackingEventArgs), nameof(AttackingEventArgs.IsAllowed))),
                     new(OpCodes.Brtrue_S, continueLabel),
 
