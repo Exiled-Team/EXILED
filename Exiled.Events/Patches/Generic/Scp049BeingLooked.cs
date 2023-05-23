e77a315bcd9ff42c2648fecbfa47f6cfd3c6bc3e
louis1706 <louismonneyron5@yahoo.com>
2023-05-24 01:56:52 +0200

    Fix error

--- Exiled.Events/Patches/Generic/Scp049BeingLooked.cs
+++ Exiled.Events/Patches/Generic/Scp049BeingLooked.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="Scp173BeingLooked.cs" company="Exiled Team">
+// <copyright file="Scp049BeingLooked.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
