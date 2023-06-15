d1b5258b80407f714d072750eef9f6b798bb8520
NotIntense <mbro8210@gmail.com>
2023-06-15 19:22:37 -0400

    Updated

--- Exiled.API/Features/Ragdoll.cs
+++ Exiled.API/Features/Ragdoll.cs
@@ -141,7 +141,7 @@ namespace Exiled.API.Features
         /// <summary>
         /// Gets the time that the ragdoll was spawned.
         /// </summary>
-        public double CreationTime => NetworkInfo.CreationTime;
+         public DateTime CreationTime => DateTime.Now - TimeSpan.FromSeconds(NetworkInfo.CreationTime);
 
         /// <summary>
         /// Gets the <see cref="RoleTypeId"/> of the ragdoll.
