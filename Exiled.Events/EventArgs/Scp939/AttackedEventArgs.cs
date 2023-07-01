12ec615e88872571917710a02a385b58b24e9638
VALERA771 <val.germanov@gmail.com>
2023-07-01 20:09:37 +0300

    commit

---
+++ Exiled.Events/EventArgs/Scp939/AttackedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AttackedEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Scp939
+{
+    using API.Features;
+    using Exiled.Events.EventArgs.Interfaces;
+
+    /// <summary>
+    /// Contains all information after SCP-939 attacks.
+    /// </summary>
+    public class AttackedEventArgs : IPlayerEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttackedEventArgs"/> class.
+        /// </summary>
+        /// <param name="player"><inheritdoc cref="Player"/></param>
+        public AttackedEventArgs(Player player)
+        {
+            Player = player;
+        }
+
+        /// <summary>
+        /// Gets the SCP-939.
+        /// </summary>
+        public Player Player { get; }
+    }
+}
---
+++ Exiled.Events/Patches/Events/Scp939/Attacked.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Attacked.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Scp939
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using Exiled.API.Features.Pools;
+    using Exiled.Events.EventArgs.Scp939;
+    using HarmonyLib;
+    using PlayerRoles.PlayableScps.Scp939;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="Scp939ClawAbility.ServerProcessCmd(Mirror.NetworkReader)"/>
+    /// to add <see cref="Handlers.Scp939.Attacked"/> event.
+    /// </summary>
+    [HarmonyPatch(typeof(Scp939ClawAbility), nameof(Scp939ClawAbility.ServerProcessCmd))]
+    internal class Attacked
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            Label continueLabel = generator.DefineLabel();
+
+            newInstructions[0].labels.Add(continueLabel);
+
+            newInstructions.InsertRange(
+                0, new CodeInstruction[]
+                {
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(Scp939ClawAbility), nameof(Scp939ClawAbility.Owner))),
+                    new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(AttackedEventArgs))[0]),
+
+                    new(OpCodes.Call, Method(typeof(Handlers.Scp939), nameof(Handlers.Scp939.OnAttacking))),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
