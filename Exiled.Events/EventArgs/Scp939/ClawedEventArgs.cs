8d3d1beffe06db67ceefdc9a040504c061db9f07
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-07-02 18:48:49 +0300

    fux errors

--- Exiled.Events/EventArgs/Scp939/ClawedEventArgs.cs
+++ Exiled.Events/EventArgs/Scp939/ClawedEventArgs.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="AttackedEventArgs.cs" company="Exiled Team">
+// <copyright file="ClawedEventArgs.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
