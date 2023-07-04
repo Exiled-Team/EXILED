2cc760043e66630a11b302148d1f286994ac4bdc
louis1706 <louismonneyron5@yahoo.com>
2023-07-04 14:08:14 +0200

    More Information

--- Exiled.Loader/LoaderPlugin.cs
+++ Exiled.Loader/LoaderPlugin.cs
@@ -52,7 +52,7 @@ namespace Exiled.Loader
                     GameCore.Version.BackwardCompatibility,
                     GameCore.Version.BackwardRevision))
             {
-                ServerConsole.AddLog("Exiled is outdated, please update to the latest version. Wait for release if still shows after update.", ConsoleColor.DarkRed);
+                ServerConsole.AddLog($"Exiled is outdated, please update to the latest version. Wait for release if still shows after update.\nSCP:SL = {GameCore.Version.VersionString} Exiled Supported Version = {AutoUpdateFiles.RequiredSCPSLVersion}", ConsoleColor.DarkRed);
                 return;
             }
 
