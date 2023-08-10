7e8387f4d78b6b0301f901c0925a495128070978
Yamato <66829532+louis1706@users.noreply.github.com>
2023-08-10 20:08:56 +0200

    Update Exiled.API/Features/Items/Armor.cs
    Co-authored-by: Misaka-ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>

--- Exiled.API/Features/Items/Armor.cs
+++ Exiled.API/Features/Items/Armor.cs
@@ -90,7 +90,7 @@ namespace Exiled.API.Features.Items
             get => Base.HelmetEfficacy;
             set
             {
-                if (value is > 101 or < 0)
+                if (value is > 100 or < 0)
                 {
                     Log.Warn($"{nameof(HelmetEfficacy)} Value of armor efficacy must be between 0 and 100.");
                     value = Mathf.Clamp(value, 0, 100);
