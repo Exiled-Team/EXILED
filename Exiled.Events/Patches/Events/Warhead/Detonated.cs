cbe2e121e782aa54844240dd3c8b04c8e9221ac5
louis1706 <louismonneyron5@yahoo.com>
2023-08-13 21:44:13 +0200

    removing space

--- Exiled.Events/Patches/Events/Warhead/Detonated.cs
+++ Exiled.Events/Patches/Events/Warhead/Detonated.cs
@@ -11,10 +11,8 @@ namespace Exiled.Events.Patches.Events.Warhead
     using System.Reflection.Emit;
 
     using API.Features.Pools;
-  
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Warhead;
-  
     using Handlers;
     using HarmonyLib;
 
