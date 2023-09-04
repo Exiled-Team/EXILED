8f614a9adfdb47af057db8eaab92ea9e31dcdc92
Rue <135553058+Ruemena@users.noreply.github.com>
2023-09-04 11:41:36 -0500

    Update FirearmRequestReceived.cs

--- Exiled.Events/Patches/Events/Player/FirearmRequestReceived.cs
+++ Exiled.Events/Patches/Events/Player/FirearmRequestReceived.cs
@@ -296,7 +296,7 @@ namespace Exiled.Events.Patches.Events.Player
                     new(OpCodes.Brfalse_S, returnLabel),
                 });
 
-            newInstructions[newInstructions.Count - 1].Labels.Add(returnLabel);
+            newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
