8437ef651026d4f59cb57900b41f8172bd32ef9a
louis1706 <louismonneyron5@yahoo.com>
2023-09-06 11:07:22 +0200

    Fix changed file

--- Exiled.Events/Patches/Events/Scp0492/Consuming.cs
+++ Exiled.Events/Patches/Events/Scp0492/Consuming.cs
@@ -5,81 +5,91 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Exiled.Events.Patches.Events.Scp0492
+namespace Exiled.Events.Patches.Events.Scp049
 {
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
-    using API.Features.Pools;
+    using Exiled.API.Features;
+    using Exiled.API.Features.Pools;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Scp0492;
     using HarmonyLib;
+
     using PlayerRoles.PlayableScps.Scp049;
     using PlayerRoles.PlayableScps.Scp049.Zombies;
     using PlayerRoles.PlayableScps.Subroutines;
+    using PlayerStatsSystem;
 
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    ///     Patches <see cref="RagdollAbilityBase{T}.ServerProcessCmd"/>
-    ///     to add <see cref="Handlers.Scp0492.ConsumingCorpse" /> event.
+    ///     Adds the <see cref="Handlers.Scp0492.ConsumingCorpse" /> event.
     /// </summary>
     [EventPatch(typeof(Handlers.Scp0492), nameof(Handlers.Scp0492.ConsumingCorpse))]
-    [HarmonyPatch(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>.ServerProcessCmd))]
+    [HarmonyPatch(typeof(ZombieConsumeAbility), nameof(ZombieConsumeAbility.ServerProcessCmd))]
     public class Consuming
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            int offset = 2;
-            int index = newInstructions.FindIndex(instrc => instrc.Calls(Method(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>.ServerValidateBegin)))) + offset;
-
-            Label retLabel = generator.DefineLabel();
             LocalBuilder ev = generator.DeclareLocal(typeof(ConsumingCorpseEventArgs));
 
-            newInstructions.InsertRange(index, new CodeInstruction[]
-            {
-                // this.Owner
-                new (OpCodes.Ldarg_0),
-                new (OpCodes.Callvirt, PropertyGetter(typeof(ScpStandardSubroutine<ZombieRole>), nameof(ScpStandardSubroutine<ZombieRole>.Owner))),
+            int offset = 1;
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret) + offset;
 
-                // this.CurRagdoll
-                new(OpCodes.Ldarg_0),
-                new(OpCodes.Callvirt, PropertyGetter(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>.CurRagdoll))),
+            Label returnLabel = generator.DefineLabel();
 
-                // this._errorCode
-                new(OpCodes.Ldarg_0),
-                new(OpCodes.Ldfld, Field(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>._errorCode))),
+            newInstructions.InsertRange(
+                index,
+                new[]
+                {
+                    // Player.Get(base.Owner)
+                    new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                    new(OpCodes.Call, PropertyGetter(typeof(ScpStandardSubroutine<ZombieRole>), nameof(ScpStandardSubroutine<ZombieRole>.Owner))),
+                    new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
-                // true
-                new(OpCodes.Ldc_I4_1),
+                    // base.CurRagdoll
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new(OpCodes.Call, PropertyGetter(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>.CurRagdoll))),
 
-                // ConsumingCorpseEventArgs = new(this.Owner, this.Ragdoll, this._errorCode, true)
-                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ConsumingCorpseEventArgs))[0]),
-                new(OpCodes.Dup),
-                new(OpCodes.Dup),
-                new(OpCodes.Stloc, ev.LocalIndex),
+                    // true
+                    new(OpCodes.Ldc_I4_1),
 
-                // Handlers.Scp0492.OnConsumingCorpse(ev)
-                new(OpCodes.Call, Method(typeof(Handlers.Scp0492), nameof(Handlers.Scp0492.OnConsumingCorpse))),
+                    // ConsumingCorpseEventArgs ev = new(Player, Ragdoll, bool)
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ConsumingCorpseEventArgs))[0]),
+                    new(OpCodes.Dup),
+                    new(OpCodes.Dup),
+                    new(OpCodes.Stloc_S, ev.LocalIndex),
 
-                // if (!ev.IsAllowed)
-                //   return
-                new(OpCodes.Callvirt, PropertyGetter(typeof(ConsumingCorpseEventArgs), nameof(ConsumingCorpseEventArgs.IsAllowed))),
-                new(OpCodes.Brtrue_S, retLabel),
+                    // Handlers.Scp0492.OnSendingCall(ev)
+                    new(OpCodes.Call, Method(typeof(Handlers.Scp0492), nameof(Handlers.Scp0492.OnConsumingCorpse))),
 
-                new(OpCodes.Ret),
+                    // if (!ev.IsAllowed)
+                    //    return;
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(ConsumingCorpseEventArgs), nameof(ConsumingCorpseEventArgs.IsAllowed))),
+                    new(OpCodes.Brfalse_S, returnLabel),
+                });
 
-                // this._errorCode = ev.ErrorCode
-                new CodeInstruction(OpCodes.Ldloc, ev.LocalIndex).WithLabels(retLabel),
-                new(OpCodes.Ldarg_0),
-                new(OpCodes.Callvirt, PropertyGetter(typeof(ConsumingCorpseEventArgs), nameof(ConsumingCorpseEventArgs.ErrorCode))),
-                new(OpCodes.Stfld, Field(typeof(RagdollAbilityBase<ZombieRole>), nameof(RagdollAbilityBase<ZombieRole>._errorCode))),
-            });
+            // replace "base.Owner.playerStats.GetModule<HealthStat>().ServerHeal(100f)" with "base.Owner.playerStats.GetModule<HealthStat>().ServerHeal(ev.ConsumeHeal)"
+            offset = -1;
+            index = newInstructions.FindIndex(instruction => instruction.operand == (object)Method(typeof(HealthStat), nameof(HealthStat.ServerHeal))) + offset;
+            newInstructions.RemoveAt(index);
 
-            foreach (var instruction in newInstructions)
-                yield return instruction;
+            newInstructions.InsertRange(
+                index,
+                new CodeInstruction[]
+                {
+                    // ev.ConsumeHeal
+                    new(OpCodes.Ldloc_S, ev.LocalIndex),
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(ConsumingCorpseEventArgs), nameof(ConsumingCorpseEventArgs.ConsumeHeal))),
+                });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
 
             ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
