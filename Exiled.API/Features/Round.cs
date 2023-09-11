52d368a570707a4d39d73fedb362a2e92ba0e27c
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-09-11 18:54:05 +0200

    Added ChaosTargetCount

--- Exiled.API/Features/Round.cs
+++ Exiled.API/Features/Round.cs
@@ -9,14 +9,15 @@ namespace Exiled.API.Features
 {
     using System;
     using System.Collections.Generic;
-
+    using System.Runtime.Remoting.Messaging;
     using Enums;
 
     using GameCore;
 
     using PlayerRoles;
-
+    using PluginAPI.Core;
     using RoundRestarting;
+    using UnityEngine;
 
     /// <summary>
     /// A set of tools to handle the round more easily.
@@ -60,6 +61,16 @@ namespace Exiled.API.Features
         /// Gets a value indicating whether the round is lobby or not.
         /// </summary>
         public static bool IsLobby => !(IsEnded || IsStarted);
+
+        /// <summary>
+        /// Gets or sets a value indicating the amount of Chaos Targets remaining.
+        /// </summary>
+        public static int ChaosTargetCount
+        {
+            get => RoundSummary.singleton.Network_chaosTargetCount;
+            set => RoundSummary.singleton.Network_chaosTargetCount = value;
+        }
+
 
         /// <summary>
         /// Gets or sets a value indicating whether the round is locked or not.
