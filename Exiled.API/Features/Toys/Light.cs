7fc50c19b408575e1d836af2e1b38880bf944e43
SmusiJarviss <84509587+SmusiJarviss@users.noreply.github.com>
2023-09-12 15:44:06 +0200

    Light::Create overload + Signature fix

--- Exiled.API/Features/Toys/Light.cs
+++ Exiled.API/Features/Toys/Light.cs
@@ -7,6 +7,7 @@
 
 namespace Exiled.API.Features.Toys
 {
+    using System;
     using System.Linq;
 
     using AdminToys;
@@ -80,9 +81,10 @@ namespace Exiled.API.Features.Toys
         /// <param name="scale">The scale of the <see cref="Light"/>.</param>
         /// <param name="spawn">Whether the <see cref="Light"/> should be initially spawned.</param>
         /// <returns>The new <see cref="Light"/>.</returns>
+        [Obsolete("Deprecated due to new methods. Use Create(Vector3, Vector3, Vector3, bool, Color) instead.", true)]
         public static Light Create(Vector3? position = null, Vector3? rotation = null, Vector3? scale = null, bool spawn = true)
         {
-            Light light = new(Object.Instantiate(ToysHelper.LightBaseObject));
+            Light light = new(UnityEngine.Object.Instantiate(ToysHelper.LightBaseObject));
 
             light.AdminToyBase.transform.position = position ?? Vector3.zero;
             light.AdminToyBase.transform.eulerAngles = rotation ?? Vector3.zero;
@@ -90,6 +92,31 @@ namespace Exiled.API.Features.Toys
 
             if (spawn)
                 light.Spawn();
+
+            return light;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Light"/>.
+        /// </summary>
+        /// <param name="position">The position of the <see cref="Light"/>.</param>
+        /// <param name="rotation">The rotation of the <see cref="Light"/>.</param>
+        /// <param name="scale">The scale of the <see cref="Light"/>.</param>
+        /// <param name="spawn">Whether the <see cref="Light"/> should be initially spawned.</param>
+        /// <param name="color">The color of the <see cref="Light"/>.</param>
+        /// <returns>The new <see cref="Light"/>.</returns>
+        public static Light Create(Vector3? position = null, Vector3? rotation = null, Vector3? scale = null, bool spawn = true, Color? color = null)
+        {
+            Light light = new(UnityEngine.Object.Instantiate(ToysHelper.LightBaseObject));
+
+            light.AdminToyBase.transform.position = position ?? Vector3.zero;
+            light.AdminToyBase.transform.eulerAngles = rotation ?? Vector3.zero;
+            light.AdminToyBase.transform.localScale = scale ?? Vector3.one;
+
+            if (spawn)
+                light.Spawn();
+
+            light.Color = color ?? Color.white;
 
             return light;
         }
