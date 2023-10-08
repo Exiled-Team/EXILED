db01dfdbecf04fcd43e6ce5511ae815e625e3bbc
louis1706 <louismonneyron5@yahoo.com>
2023-10-08 16:32:18 +0200

    Preventing Error + add a new ctor for Effect class

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -2691,7 +2691,7 @@ namespace Exiled.API.Features
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
         /// <returns>A bool indicating whether or not the effect was valid and successfully enabled.</returns>
         public bool EnableEffect<T>(float duration = 0f, bool addDurationIfActive = false)
-                    where T : StatusEffectBase => EnableEffect<T>(duration, 1, addDurationIfActive);
+                    where T : StatusEffectBase => EnableEffect<T>(1, duration, addDurationIfActive);
 
         /// <summary>
         /// Enables a <see cref="StatusEffectBase">status effect</see> on the player.
@@ -2701,7 +2701,7 @@ namespace Exiled.API.Features
         /// <param name="duration">The amount of time the effect will be active for.</param>
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
         /// <returns>A bool indicating whether or not the effect was valid and successfully enabled.</returns>
-        public bool EnableEffect<T>(byte intensity = 1, float duration = 0f, bool addDurationIfActive = false)
+        public bool EnableEffect<T>(byte intensity, float duration = 0f, bool addDurationIfActive = false)
             where T : StatusEffectBase => ReferenceHub.playerEffectsController.ChangeState<T>(intensity, duration, addDurationIfActive);
 
         /// <summary>
@@ -2722,7 +2722,7 @@ namespace Exiled.API.Features
         /// <param name="duration">The amount of time the effect will be active for.</param>
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
         /// <returns>A bool indicating whether or not the effect was valid and successfully enabled.</returns>
-        public bool EnableEffect(StatusEffectBase statusEffect, byte intensity = 1, float duration = 0f, bool addDurationIfActive = false)
+        public bool EnableEffect(StatusEffectBase statusEffect, byte intensity, float duration = 0f, bool addDurationIfActive = false)
         {
             if (statusEffect is null)
                 return false;
@@ -2749,7 +2749,7 @@ namespace Exiled.API.Features
         /// <param name="duration">The amount of time the effect will be active for.</param>
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
         /// <returns>The <see cref="StatusEffectBase"/> instance of the activated effect.</returns>
-        public StatusEffectBase EnableEffect(string effectName, byte intensity = 1, float duration = 0f, bool addDurationIfActive = false)
+        public StatusEffectBase EnableEffect(string effectName, byte intensity, float duration = 0f, bool addDurationIfActive = false)
             => ReferenceHub.playerEffectsController.ChangeState(effectName, intensity, duration, addDurationIfActive);
 
         /// <summary>
@@ -2768,18 +2768,18 @@ namespace Exiled.API.Features
         /// <param name="intensity">The intensity of the effect will be active for.</param>
         /// <param name="duration">The amount of time the effect will be active for.</param>
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
-        public void EnableEffect(EffectType type, byte intensity = 1, float duration = 0f, bool addDurationIfActive = false)
+        public void EnableEffect(EffectType type, byte intensity, float duration = 0f, bool addDurationIfActive = false)
         {
             switch (type)
             {
                 case EffectType.AmnesiaItems:
-                    EnableEffect<AmnesiaItems>(duration, addDurationIfActive);
+                    EnableEffect<AmnesiaItems>(intensity, duration, addDurationIfActive);
                     break;
                 case EffectType.AmnesiaVision:
-                    EnableEffect<AmnesiaVision>(duration, addDurationIfActive);
+                    EnableEffect<AmnesiaVision>(intensity, duration, addDurationIfActive);
                     break;
                 case EffectType.SoundtrackMute:
-                    EnableEffect<SoundtrackMute>(duration, addDurationIfActive);
+                    EnableEffect<SoundtrackMute>(intensity, duration, addDurationIfActive);
                     break;
                 default:
                     if (TryGetEffect(type, out StatusEffectBase statusEffect))
@@ -2793,7 +2793,14 @@ namespace Exiled.API.Features
         /// Enables a <see cref="Effect">status effect</see> on the player.
         /// </summary>
         /// <param name="effect">The <see cref="Effect"/> to enable.</param>
-        public void EnableEffect(Effect effect)
+        [Obsolete("Use SyncEffect(Effect) instead of this")]
+        public void EnableEffect(Effect effect) => SyncEffect(effect);
+
+        /// <summary>
+        /// Sync the <see cref="Effect">status effect</see> on the player.
+        /// </summary>
+        /// <param name="effect">The <see cref="Effect"/> to sync.</param>
+        public void SyncEffect(Effect effect)
         {
             if (effect.IsEnabled)
             {
@@ -2812,12 +2819,23 @@ namespace Exiled.API.Features
         /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
         /// <returns>A <see cref="EffectType"/> that was given to the player.</returns>
         public EffectType ApplyRandomEffect(EffectCategory category = EffectCategory.None, float duration = 0f, bool addDurationIfActive = false)
+            => ApplyRandomEffect(category, 1, duration, addDurationIfActive);
+
+        /// <summary>
+        /// Enables a random <see cref="EffectType"/> on the player.
+        /// </summary>
+        /// <param name="category">An optional category to filter the applied effect. Set to <see cref="EffectCategory.None"/> for any effect.</param>
+        /// <param name="intensity">The intensity of the effect will be active for.</param>
+        /// <param name="duration">The amount of time the effect will be active for.</param>
+        /// <param name="addDurationIfActive">If the effect is already active, setting to <see langword="true"/> will add this duration onto the effect.</param>
+        /// <returns>A <see cref="EffectType"/> that was given to the player.</returns>
+        public EffectType ApplyRandomEffect(EffectCategory category, byte intensity, float duration = 0f, bool addDurationIfActive = false)
         {
             Array effectTypes = Enum.GetValues(typeof(EffectType));
             IEnumerable<EffectType> validEffects = effectTypes.ToArray<EffectType>().Where(effect => effect.GetCategories().HasFlag(category));
             EffectType effectType = validEffects.GetRandomValue();
 
-            EnableEffect(effectType, duration, addDurationIfActive);
+            EnableEffect(effectType, intensity, duration, addDurationIfActive);
 
             return effectType;
         }
@@ -2841,10 +2859,17 @@ namespace Exiled.API.Features
         /// Enables a <see cref="IEnumerable{T}"/> of <see cref="Effect"/> on the player.
         /// </summary>
         /// <param name="effects">The <see cref="IEnumerable{T}"/> of <see cref="Effect"/> to enable.</param>
-        public void EnableEffects(IEnumerable<Effect> effects)
+        [Obsolete("Use SyncEffects(IEnumerable<Effect>) instead of this")]
+        public void EnableEffects(IEnumerable<Effect> effects) => SyncEffects(effects);
+
+        /// <summary>
+        /// Sync a <see cref="IEnumerable{T}"/> of <see cref="Effect"/> on the player.
+        /// </summary>
+        /// <param name="effects">The <see cref="IEnumerable{T}"/> of <see cref="Effect"/> to enable.</param>
+        public void SyncEffects(IEnumerable<Effect> effects)
         {
             foreach (Effect effect in effects)
-                EnableEffect(effect);
+                SyncEffect(effect);
         }
 
         /// <summary>
