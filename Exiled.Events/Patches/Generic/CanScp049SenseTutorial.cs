568d1b59da44cab868f6e89fffad57dbb372ab5a
louis1706 <louismonneyron5@yahoo.com>
2023-10-25 17:46:05 +0200

    FixCanScp049SenseTutorial

--- Exiled.Events/Patches/Generic/CanScp049SenseTutorial.cs
+++ Exiled.Events/Patches/Generic/CanScp049SenseTutorial.cs
@@ -35,7 +35,7 @@ namespace Exiled.Events.Patches.Generic
 
             int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Brfalse);
 
-            Label returnLabel = (Label)newInstructions[index].operand;
+            Label continueLabel = (Label)newInstructions[index].operand;
             Label skip = generator.DefineLabel();
 
             index += 1;
@@ -47,7 +47,7 @@ namespace Exiled.Events.Patches.Generic
                 new[]
                 {
                     // if (referenceHub.roleManager.CurrentRole.RoleTypeId == RoleTypeId.Tutorial)
-                    new(OpCodes.Ldloca_S, 6),
+                    new(OpCodes.Ldloc_S, 6),
                     new(OpCodes.Ldfld, Field(typeof(ReferenceHub), nameof(ReferenceHub.roleManager))),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(PlayerRoleManager), nameof(PlayerRoleManager.CurrentRole))),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(PlayerRoleBase), nameof(PlayerRoleBase.RoleTypeId))),
@@ -58,14 +58,14 @@ namespace Exiled.Events.Patches.Generic
                     new(OpCodes.Call, PropertyGetter(typeof(ExiledEvents), nameof(ExiledEvents.Instance))),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(Plugin<Config>), nameof(Plugin<Config>.Config))),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(Config), nameof(Config.CanScp049SenseTutorial))),
-                    new(OpCodes.Brfalse_S, returnLabel),
+                    new(OpCodes.Brfalse_S, continueLabel),
 
                     // if (Scp049Role.TurnedPlayers.Contains(Player.Get(referenceHub)))
                     new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(API.Features.Roles.Scp049Role), nameof(API.Features.Roles.Scp049Role.TurnedPlayers))).WithLabels(skip),
-                    new(OpCodes.Ldloca_S, 6),
+                    new(OpCodes.Ldloc_S, 6),
                     new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
                     new(OpCodes.Callvirt, Method(typeof(HashSet<Player>), nameof(HashSet<Player>.Contains))),
-                    new(OpCodes.Brtrue_S, returnLabel),
+                    new(OpCodes.Brtrue_S, continueLabel),
                 });
 
             for (int z = 0; z < newInstructions.Count; z++)
