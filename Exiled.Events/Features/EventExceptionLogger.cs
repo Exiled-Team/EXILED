5c7e4f5dd5194ef5a2af2bbaf2322994a8326ea8
Nekonyx <nekonyx@proton.me>
2023-10-29 09:48:59 +0600

    feat(events): implement EventExceptionLogger to capture event exceptions

---
+++ Exiled.Events/Features/EventExceptionLogger.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EventExceptionLogger.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Features
+{
+    using System;
+    using System.Reflection;
+
+    using Exiled.API.Features;
+
+    /// <summary>
+    /// A class that logs exceptions that occur when handling events.
+    /// </summary>
+    public static class EventExceptionLogger
+    {
+        /// <summary>
+        /// Logs an exception that occurred when handling an event.
+        /// </summary>
+        /// <param name="ex">The exception that occurred.</param>
+        /// <param name="eventName">The name of the event.</param>
+        /// <param name="method">The method that caused the exception.</param>
+        public static void Capture(Exception ex, string eventName, MethodInfo method)
+        {
+            Log.Error($"Method \"{method.Name}\" of the class \"{method.ReflectedType.FullName}\" caused an exception when handling the event \"{eventName}\"\n{ex}");
+        }
+    }
+}
