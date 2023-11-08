cfc36d6550decb1bf9a0c88c8ab7b890451781b3
VALERA771 <val.germanov@gmail.com>
2023-11-08 23:03:29 +0300

    misc changes

--- Exiled.CustomItems/API/Features/CustomCandy.cs
+++ Exiled.CustomItems/API/Features/CustomCandy.cs
@@ -1,4 +1,11 @@
-﻿namespace Exiled.CustomItems.API.Features
+﻿// -----------------------------------------------------------------------
+// <copyright file="CustomCandy.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.CustomItems.API.Features
 {
     using System;
     using System.Collections.Generic;
@@ -16,7 +23,7 @@
     /// </summary>
     public abstract class CustomCandy : CustomItem
     {
-        internal static new readonly Dictionary<ushort, List<int>> TrackedSerials = new();
+        private readonly Dictionary<ushort, List<int>> trackedSerials = new();
 
         /// <inheritdoc/>
         /// <exception cref="NotSupportedException">Throws when trying to set value to property.</exception>
@@ -31,25 +38,43 @@
         /// </summary>
         public abstract CandyKindID CandyType { get; set; }
 
-        /// <summary>
-        /// Applies an effects to player who eaten a <see cref="CustomCandy"/>.
-        /// </summary>
-        /// <param name="player">Player who eaten <see cref="CustomCandy"/>.</param>
-        public abstract void ApplyEffect(Player player);
+        /// <inheritdoc/>
+        public override bool Check(Item? item) => item != null && trackedSerials.ContainsKey(item!.Serial);
 
         /// <inheritdoc/>
-        public override bool Check(Item? item) => item != null && TrackedSerials.ContainsKey(item!.Serial);
-
-        /// <inheritdoc/>
-        public override bool Check(Pickup? pickup) => pickup != null && TrackedSerials.ContainsKey(pickup!.Serial);
+        public override bool Check(Pickup? pickup) => pickup != null && trackedSerials.ContainsKey(pickup!.Serial);
 
         /// <summary>
         /// Checks the specified inventory item to see if it is a custom item.
         /// </summary>
-        /// <param name="scp330"></param>
-        /// <param name="candyKindID"></param>
-        /// <returns></returns>
-        public bool Check(Scp330? scp330, CandyKindID candyKindID) => scp330 != null && TrackedSerials.TryGetValue(scp330.Serial, out List<int> candies) && candies.Contains(scp330.Candies.ToList().IndexOf(candyKindID));
+        /// <param name="scp330">Bag to check.</param>
+        /// <param name="candyKindID">Candy type to find.</param>
+        /// <returns><see langword="true"/> if any candy from <see cref="Scp330"/> is custom. Otherwise, <see langword="false"/>.</returns>
+        public bool Check(Scp330? scp330, CandyKindID candyKindID) => scp330 != null && trackedSerials.TryGetValue(scp330.Serial, out List<int> candies) && candies.Contains(scp330.Candies.ToList().IndexOf(candyKindID));
+
+        /// <summary>
+        /// Applies an effects to player who eaten a <see cref="CustomCandy"/>.
+        /// </summary>
+        /// <param name="player">Player who eaten <see cref="CustomCandy"/>.</param>
+        protected abstract void ApplyEffect(Player player);
+
+        /// <inheritdoc/>
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+
+            Exiled.Events.Handlers.Scp330.InteractingScp330 += OnInternalUsingScp330;
+            Exiled.Events.Handlers.Scp330.EatingScp330 += OnInternalUsingCandy;
+        }
+
+        /// <inheritdoc/>
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+
+            Exiled.Events.Handlers.Scp330.InteractingScp330 -= OnInternalUsingScp330;
+            Exiled.Events.Handlers.Scp330.EatingScp330 -= OnInternalUsingCandy;
+        }
 
         private void OnInternalUsingScp330(InteractingScp330EventArgs ev)
         {
@@ -60,10 +85,10 @@
                     if (!ev.Player.TryGetItem(ItemType.SCP330, out Item item) || !item.Is(out Scp330 scp330))
                         return;
 
-                    if (TrackedSerials.ContainsKey(scp330.Serial))
-                        TrackedSerials[scp330.Serial].Add(scp330.Candies.Count - 1);
+                    if (trackedSerials.ContainsKey(scp330.Serial))
+                        trackedSerials[scp330.Serial].Add(scp330.Candies.Count - 1);
                     else
-                        TrackedSerials.Add(scp330.Serial, new List<int> { 0 });
+                        trackedSerials.Add(scp330.Serial, new List<int> { 0 });
                 });
             }
         }
@@ -73,7 +98,7 @@
             if (Check(ev.Scp330Bag, ev.Candy.Kind))
             {
                 ev.IsAllowed = false;
-                TrackedSerials[ev.Scp330Bag.Serial].Remove(ev.Scp330Bag.Candies.ToList().IndexOf(ev.Candy.Kind));
+                trackedSerials[ev.Scp330Bag.Serial].Remove(ev.Scp330Bag.Candies.ToList().IndexOf(ev.Candy.Kind));
                 ApplyEffect(ev.Player);
             }
         }
