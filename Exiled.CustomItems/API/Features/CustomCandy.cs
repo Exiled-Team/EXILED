b606d0de58938a1146e7c17d2dfa71f06dc6ef30
VALERA771 <val.germanov@gmail.com>
2023-11-09 18:46:15 +0300

    something new

--- Exiled.CustomItems/API/Features/CustomCandy.cs
+++ Exiled.CustomItems/API/Features/CustomCandy.cs
@@ -14,6 +14,7 @@ namespace Exiled.CustomItems.API.Features
     using Exiled.API.Features;
     using Exiled.API.Features.Items;
     using Exiled.API.Features.Pickups;
+    using Exiled.API.Features.Spawn;
     using Exiled.Events.EventArgs.Scp330;
     using InventorySystem.Items.Usables.Scp330;
     using MEC;
@@ -25,12 +26,38 @@ namespace Exiled.CustomItems.API.Features
     {
         private readonly Dictionary<ushort, List<int>> trackedSerials = new();
 
+        private float weight = 100;
+
         /// <inheritdoc/>
         /// <exception cref="NotSupportedException">Throws when trying to set value to property.</exception>
         public override ItemType Type
         {
             get => ItemType.SCP330;
             set => throw new NotSupportedException("Cannot set ItemType for CustomCandy. To set candy use CandyType.");
+        }
+
+        /// <summary>
+        /// Gets or sets a chance that candy will become custom.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <see langword="value"/> is less that 0 or above 100.</exception>
+        public override float Weight
+        {
+            get => weight;
+            set
+            {
+                if (value is > 100 or < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Value must be above zero and less that 100.");
+
+                weight = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="NotSupportedException">Throws when you try to access getter or setter.</exception>
+        public override SpawnProperties? SpawnProperties
+        {
+            get => throw new NotSupportedException("Cannot get a SpawnProperties of a CustomCandy.");
+            set => throw new NotSupportedException("Cannot set a SpawnProperties of a CustomCandy.");
         }
 
         /// <summary>
@@ -43,6 +70,32 @@ namespace Exiled.CustomItems.API.Features
 
         /// <inheritdoc/>
         public override bool Check(Pickup? pickup) => pickup != null && trackedSerials.ContainsKey(pickup!.Serial);
+
+        /// <inheritdoc/>
+        public override void Give(Player player, bool displayMessage = true)
+        {
+            player.TryAddCandy(CandyType);
+
+            if (player.TryGetItem(ItemType.SCP330, out Item item) && item.Is(out Scp330 scp330))
+            {
+                if (!trackedSerials.ContainsKey(scp330.Serial))
+                    trackedSerials.Add(scp330.Serial, new List<int>() { scp330.Candies.Count - 1 });
+                else
+                    trackedSerials[scp330.Serial].Add(scp330.Candies.Count - 1);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Give(Player player, Pickup pickup, bool displayMessage = true)
+        {
+            if (pickup.Is(out Exiled.API.Features.Pickups.Scp330Pickup scp330Pickup))
+            {
+                Give(player, displayMessage);
+
+                foreach (var candy in scp330Pickup.Candies)
+                    player.TryAddCandy(candy);
+            }
+        }
 
         /// <summary>
         /// Checks the specified inventory item to see if it is a custom item.
@@ -76,19 +129,35 @@ namespace Exiled.CustomItems.API.Features
             Exiled.Events.Handlers.Scp330.EatingScp330 -= OnInternalUsingCandy;
         }
 
+        /// <inheritdoc cref="OnAcquired(Player, Item, bool)"/>
+        protected virtual void OnAcquired(Player player, Scp330 scp330, CandyKindID candyKindID, bool displayMessage)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void OnAcquired(Player player, Item item, bool displayMessage)
+        {
+            if (item.Is(out Scp330 scp330))
+            {
+                OnAcquired(player, scp330, scp330.Candies.Last(), displayMessage);
+            }
+        }
+
         private void OnInternalUsingScp330(InteractingScp330EventArgs ev)
         {
             if (ev.Candy == CandyType)
             {
                 Timing.CallDelayed(.5f, () =>
                 {
-                    if (!ev.Player.TryGetItem(ItemType.SCP330, out Item item) || !item.Is(out Scp330 scp330))
-                        return;
+                    if (ev.Player.TryGetItem(ItemType.SCP330, out Item item) && item.Is(out Scp330 scp330))
+                    {
+                        if (!trackedSerials.ContainsKey(scp330.Serial))
+                            trackedSerials.Add(scp330.Serial, new List<int>() { scp330.Candies.Count - 1 });
+                        else
+                            trackedSerials[scp330.Serial].Add(scp330.Candies.Count - 1);
 
-                    if (trackedSerials.ContainsKey(scp330.Serial))
-                        trackedSerials[scp330.Serial].Add(scp330.Candies.Count - 1);
-                    else
-                        trackedSerials.Add(scp330.Serial, new List<int> { 0 });
+                        OnAcquired(ev.Player, scp330, ev.Candy, true);
+                    }
                 });
             }
         }
