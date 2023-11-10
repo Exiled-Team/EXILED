a0afe1e27205c02ba57883fea9424a6060085bac
FUTURE-SL <future.games.scp@gmail.com>
2023-11-10 22:04:53 +0300

    My brain fix

--- Exiled.Loader/Features/LoaderMessages.cs
+++ Exiled.Loader/Features/LoaderMessages.cs
@@ -83,7 +83,7 @@ namespace Exiled.Loader.Features
         /// <returns>The correspondent loader message.</returns>
         public static string GetMessage()
         {
-            if (Loader.Version.ToString().Contains("6.9") || 0.069.Chance())
+            if (Loader.Version.ToString().Contains("6.9") || 6.9.Chance())
                 return EasterEgg;
 
             return DateTime.Today.Month switch
