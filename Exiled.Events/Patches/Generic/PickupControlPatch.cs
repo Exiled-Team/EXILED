d5b71facd801c04c6a58234d8ca85beea3994958
VALERA771 <val.germanov@gmail.com>
2023-11-11 21:10:33 +0300

    patch fix

--- Exiled.Events/Patches/Generic/PickupControlPatch.cs
+++ Exiled.Events/Patches/Generic/PickupControlPatch.cs
@@ -26,7 +26,6 @@ namespace Exiled.Events.Patches.Generic
 
     using static HarmonyLib.AccessTools;
 
-#pragma warning disable SA1402 /// File may only contain a single type.
     /// <summary>
     /// Patches <see cref="InventoryExtensions.ServerCreatePickup(ItemBase, PickupSyncInfo, Vector3, Quaternion, bool, Action{ItemPickupBase})"/> to save scale for pickups and control <see cref="Pickup.IsSpawned"/> property.
     /// </summary>
@@ -44,9 +43,10 @@ namespace Exiled.Events.Patches.Generic
 
             newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                // pickup = Pickup.Get(pickupBase);
+                // Pickup.Get(pickupBase);
                 new(OpCodes.Ldloc_0),
                 new(OpCodes.Call, Method(typeof(Pickup), nameof(Pickup.Get), new[] { typeof(ItemPickupBase) })),
+                new(OpCodes.Pop),
 
                 /*
                 // Item.Get(itemBase);
@@ -55,36 +55,6 @@ namespace Exiled.Events.Patches.Generic
 
                 // pickup.GetItemInfo(item);
                 new(OpCodes.Callvirt, Method(typeof(Pickup), nameof(Pickup.GetItemInfo))),*/
-
-                // pickup.IsSpawned = spawn
-                new(OpCodes.Ldarg_S, 4),
-                new(OpCodes.Callvirt, PropertySetter(typeof(Pickup), nameof(Pickup.IsSpawned))),
-            });
-
-            for (int z = 0; z < newInstructions.Count; z++)
-                yield return newInstructions[z];
-
-            ListPool<CodeInstruction>.Pool.Return(newInstructions);
-        }
-    }
-
-    /// <summary>
-    /// Patches <see cref="ItemDistributor.SpawnPickup"/> to control <see cref="Pickup.IsSpawned"/> property for delayed spawned pickup.
-    /// </summary>
-    [HarmonyPatch(typeof(ItemDistributor), nameof(ItemDistributor.SpawnPickup))]
-    internal static class TriggerPickupControlPatch
-    {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
-        {
-            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
-
-            newInstructions.InsertRange(newInstructions.Count - 1, new CodeInstruction[]
-            {
-                // Pickup.Get(pickupBase).IsSpawned = true
-                new(OpCodes.Ldarg_0),
-                new(OpCodes.Call, Method(typeof(Pickup), nameof(Pickup.Get), new[] { typeof(ItemPickupBase) })),
-                new(OpCodes.Ldc_I4_1),
-                new(OpCodes.Callvirt, PropertySetter(typeof(Pickup), nameof(Pickup.IsSpawned))),
             });
 
             for (int z = 0; z < newInstructions.Count; z++)
