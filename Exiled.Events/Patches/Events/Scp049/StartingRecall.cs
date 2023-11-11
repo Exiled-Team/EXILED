240a691627ff920a72c7d5b2a9514889649739f7
白泽 <96401952+XKaguya@users.noreply.github.com>
2023-11-12 02:43:30 +0800

    Uploaded correct file

--- Exiled.Events/Patches/Events/Scp049/StartingRecall.cs
+++ Exiled.Events/Patches/Events/Scp049/StartingRecall.cs
@@ -7,62 +7,74 @@
 
 namespace Exiled.Events.Patches.Events.Scp049
 {
-	using System.Collections.Generic;
-	using System.Reflection.Emit;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
 
-	using API.Features;
-	using API.Features.Pools;
-	using Exiled.Events.Attributes;
-	using Exiled.Events.EventArgs.Scp049;
+    using API.Features;
+    using API.Features.Pools;
+    using Exiled.Events.Attributes;
+    using Exiled.Events.EventArgs.Scp049;
 
-	using HarmonyLib;
+    using HarmonyLib;
 
-	using PlayerRoles.PlayableScps.Scp049;
+    using PlayerRoles.PlayableScps.Scp049;
 
-	using static HarmonyLib.AccessTools;
+    using static HarmonyLib.AccessTools;
 
-	/// <summary>
-	///		Patches <see cref="Scp049ResurrectAbility.ServerValidateBegin" />.
-	///		Adds the <see cref="Handlers.Scp049.StartingRecall" /> event.
-	/// </summary>
-	[EventPatch(typeof(Handlers.Scp049), nameof(Handlers.Scp049.StartingRecall))]
-    [HarmonyPatch(typeof(Scp049ResurrectAbility), "ServerValidateBegin")]
-    internal static class Scp049ResurrectAbilityPatch
+    /// <summary>
+    ///     Patches <see cref="Scp049ResurrectAbility.ServerValidateBegin" />.
+    ///     Adds the <see cref="Handlers.Scp049.StartingRecall" /> event.
+    /// </summary>
+    [EventPatch(typeof(Handlers.Scp049), nameof(Handlers.Scp049.StartingRecall))]
+    [HarmonyPatch(typeof(Scp049ResurrectAbility), nameof(Scp049ResurrectAbility.ServerValidateBegin))]
+    internal static class StartingRecall
     {
-        static bool Prefix(ref byte __result, Scp049ResurrectAbility __instance, BasicRagdoll ragdoll)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            Player player = Player.Get(__instance.Owner);
+            Label continueLabel = generator.DefineLabel();
+            Label retLabel = generator.DefineLabel();
 
-            Ragdoll doll = Ragdoll.Get(ragdoll);
-
-            StartingRecallEventArgsMod ev = new(player, doll, true);
-
-            LimitZombie.ResurrectHandler.OnResurrect(ev);
-
-            bool isAllowed = ev.IsAllowed;
-
-            if (!isAllowed)
+            newInstructions.InsertRange(0, new CodeInstruction[]
             {
-                __result = 1;
-                return false;
-            }
+                // Player player = Player.Get(this.Owner);
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Scp049ResurrectAbility), nameof(Scp049ResurrectAbility.Owner))),
+                new(OpCodes.Call, Method(typeof(Exiled.API.Features.Player), nameof(Exiled.API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
 
-            ResurrectError resurrectError = __instance.CheckBeginConditions(ragdoll);
+                // Ragdoll doll = Ragdoll.Get(ragdoll);
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, Method(typeof(Ragdoll), nameof(Ragdoll.Get), new[] { typeof(BasicRagdoll) })),
 
-            if (resurrectError != 0)
-            {
-                __result = (byte)resurrectError;
-                return false;
-            }
+                // true
+                new(OpCodes.Ldc_I4_1),
 
-            if (!__instance.ServerValidateAny())
-            {
-                __result = 1;
-                return false;
-            }
+                // StartingRecallEventArgs ev = new StartingRecallEventArgsMod(target, player, doll, true);
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(StartingRecallEventArgs))[0]),
+                new(OpCodes.Dup),
 
-            return true;
+                // Handlers.Scp049.OnStartingRecall(ev);
+                new(OpCodes.Call, Method(typeof(Handlers.Scp049), nameof(Handlers.Scp049.OnStartingRecall))),
+
+                // if (!ev.IsAllowed)
+                //      return 1;
+                // else
+                //      continue;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(StartingRecallEventArgs), nameof(StartingRecallEventArgs.IsAllowed))),
+                new(OpCodes.Brtrue_S, continueLabel),
+                new(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Ret).WithLabels(retLabel),
+                
+                new CodeInstruction(OpCodes.Nop).WithLabels(continueLabel),
+            });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(retLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
     }
 }
