6ae3d480313c879a038ce7925900775e96ac2de2
Yamato <66829532+louis1706@users.noreply.github.com>
2023-11-12 20:02:45 +0100

    Rework Scp049 Event (#1978)

--- Exiled.Events/EventArgs/Scp049/FinishingRecallEventArgs.cs
+++ Exiled.Events/EventArgs/Scp049/FinishingRecallEventArgs.cs
@@ -34,20 +34,20 @@ namespace Exiled.Events.EventArgs.Scp049
         /// </param>
         public FinishingRecallEventArgs(Player target, Player scp049, BasicRagdoll ragdoll, bool isAllowed = true)
         {
-            Target = target;
             Player = scp049;
             Scp049 = Player.Role.As<Scp049Role>();
+            Target = target;
             Ragdoll = Ragdoll.Get(ragdoll);
             IsAllowed = isAllowed;
         }
+
+        /// <inheritdoc/>
+        public Scp049Role Scp049 { get; }
 
         /// <summary>
         ///     Gets the player who is controlling SCP-049.
         /// </summary>
         public Player Player { get; }
-
-        /// <inheritdoc/>
-        public Scp049Role Scp049 { get; }
 
         /// <summary>
         ///     Gets the player who's getting recalled.
