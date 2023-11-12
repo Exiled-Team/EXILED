2d2b8f8d77c9d0d4363671d7976cbef96e497e29
白泽 <96401952+XKaguya@users.noreply.github.com>
2023-11-12 13:04:52 +0800

    Fixed Transpiler issue

--- Exiled.Events/Patches/Events/Scp049/StartingRecall.cs
+++ Exiled.Events/Patches/Events/Scp049/StartingRecall.cs
@@ -18,7 +18,6 @@ namespace Exiled.Events.Patches.Events.Scp049
     using HarmonyLib;
 
     using PlayerRoles.PlayableScps.Scp049;
-    using PlayerRoles.Ragdolls;
 
     using static HarmonyLib.AccessTools;
 
@@ -28,35 +27,30 @@ namespace Exiled.Events.Patches.Events.Scp049
     /// </summary>
     [EventPatch(typeof(Handlers.Scp049), nameof(Handlers.Scp049.StartingRecall))]
     [HarmonyPatch(typeof(Scp049ResurrectAbility), nameof(Scp049ResurrectAbility.ServerValidateBegin))]
-    internal static class StartingRecall
+    internal static class Scp049ResurrectAbilityPatch
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
+            Label continueLabel = generator.DefineLabel();
             Label retLabel = generator.DefineLabel();
 
             newInstructions.InsertRange(0, new CodeInstruction[]
             {
-                // Player target = Player.Get(ragdoll.Info.OwnerHub);
-                new(OpCodes.Ldarg_1),
-                new(OpCodes.Ldfld, Field(typeof(BasicRagdoll), nameof(BasicRagdoll.Info))),
-                new(OpCodes.Ldfld, Field(typeof(RagdollData), nameof(RagdollData.OwnerHub))),
-                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
-
                 // Player player = Player.Get(this.Owner);
                 new(OpCodes.Ldarg_0),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(Scp049ResurrectAbility), nameof(Scp049ResurrectAbility.Owner))),
-                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Call, Method(typeof(Exiled.API.Features.Player), nameof(Exiled.API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
 
-                // Ragdoll doll = Rangdoll.Get(ragdoll);
+                // Ragdoll doll = Ragdoll.Get(ragdoll);
                 new(OpCodes.Ldarg_1),
                 new(OpCodes.Call, Method(typeof(Ragdoll), nameof(Ragdoll.Get), new[] { typeof(BasicRagdoll) })),
 
                 // true
                 new(OpCodes.Ldc_I4_1),
 
-                // StartingRecallEventArgs ev = new(target, player, doll, true);
+                // StartingRecallEventArgs ev = new StartingRecallEventArgsMod(target, player, doll, true);
                 new(OpCodes.Newobj, GetDeclaredConstructors(typeof(StartingRecallEventArgs))[0]),
                 new(OpCodes.Dup),
 
@@ -64,10 +58,17 @@ namespace Exiled.Events.Patches.Events.Scp049
                 new(OpCodes.Call, Method(typeof(Handlers.Scp049), nameof(Handlers.Scp049.OnStartingRecall))),
 
                 // if (!ev.IsAllowed)
-                //      return;
+                //      return 1;
+                // else
+                //      continue;
                 new(OpCodes.Callvirt, PropertyGetter(typeof(StartingRecallEventArgs), nameof(StartingRecallEventArgs.IsAllowed))),
-                new(OpCodes.Brfalse_S, retLabel),
+                new(OpCodes.Brtrue_S, continueLabel),
+                new(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Ret).WithLabels(retLabel),
+                
+                new CodeInstruction(OpCodes.Nop).WithLabels(continueLabel),
             });
+
             newInstructions[newInstructions.Count - 1].WithLabels(retLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
