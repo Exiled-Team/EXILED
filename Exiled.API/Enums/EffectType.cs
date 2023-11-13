35eb83e3eb8a0909d50cf71e52995455977b2e82
Misaka-ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>
2023-11-12 21:29:17 -0500

    per french person instruction

--- Exiled.API/Enums/EffectType.cs
+++ Exiled.API/Enums/EffectType.cs
@@ -211,15 +211,6 @@ namespace Exiled.API.Enums
         Strangled,
 #pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
 #pragma warning disable SA1602 // Enumeration items should be documented
-        OrangeCandy,
-        Spicy,
-        SugarCrave,
-        SugarHigh,
-        SugarRush,
-        TraumatizedByEvil,
-        Metal,
-        Prismatic,
-        SlowMetabolism,
         Marshmallow,
 #pragma warning restore SA1602 // Enumeration items should be documented
 #pragma warning restore CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
