4c7c3466612d39705afcf5add13d2829f72bb162
Misaka-ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>
2023-11-14 11:25:32 -0500

    Update moveFile.ps1

--- moveFile.ps1
+++ moveFile.ps1
@@ -50,5 +50,5 @@ function Move-File {
 # Usage example for the Move-File function
  
 # Move a file from one location to another
-dir read *.*
+dir *.*
 Move-File -sourcePath ".\EXILED-DLL-Archiver.exe" -destinationPath ".\bin\Release\EXILED-DLL-Archiver.exe"
