afedf0cc2147ee21ce06a1994bd56aa85bae8762
louis1706 <louismonneyron5@yahoo.com>
2023-11-20 00:27:21 +0100

    Fix Patch

--- Exiled.Events/Patches/Generic/StaminaUsage.cs
+++ Exiled.Events/Patches/Generic/StaminaUsage.cs
@@ -10,20 +10,21 @@ namespace Exiled.Events.Patches.Generic
 #pragma warning disable SA1313
 
     using Exiled.API.Features;
+    using Exiled.API.Features.Roles;
     using HarmonyLib;
     using InventorySystem;
 
     /// <summary>
     /// Patches <see cref="Inventory.StaminaUsageMultiplier"/>.
-    /// Implements <see cref="Player.IsUsingStamina"/> and <see cref="Player.StaminaUsageMultiplier"/>.
+    /// Implements <see cref="Player.IsUsingStamina"/>, <see cref="FpcRole.IsUsingStamina"/> and <see cref="FpcRole.StaminaUsageMultiplier"/>.
     /// </summary>
     [HarmonyPatch(typeof(Inventory), nameof(Inventory.StaminaUsageMultiplier), MethodType.Getter)]
     internal class StaminaUsage
     {
         private static void Postfix(Inventory __instance, ref float __result)
         {
-            if (Player.TryGet(__instance._hub, out Player player))
-                __result *= player.IsUsingStamina ? player.StaminaUsageMultiplier : 0;
+            if (Player.TryGet(__instance._hub, out Player player) && player.Role.Is(out FpcRole fpc))
+                __result *= player.IsUsingStamina || fpc.IsUsingStamina ? fpc.StaminaUsageMultiplier : 0;
         }
     }
 }
