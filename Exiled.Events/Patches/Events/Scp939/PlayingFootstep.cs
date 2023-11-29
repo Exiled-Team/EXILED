9bbdb537e620be599235ec59f0fd753414c755ec
白泽 <rx700@vip.qq.com>
2023-11-29 09:58:03 +0800

    Add files via upload

---
+++ Exiled.Events/Patches/Events/Scp939/PlayingFootstep.cs
@@ -0,0 +1,68 @@
+// -----------------------------------------------------------------------
+// <copyright file="PlayingFootstep.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Scp939
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using Exiled.API.Features.Pools;
+    using Exiled.Events.Attributes;
+    using Exiled.Events.EventArgs.Scp939;
+    using Exiled.Events.Handlers;
+
+    using HarmonyLib;
+    using PlayerRoles.FirstPersonControl.Thirdperson;
+    using PlayerRoles.PlayableScps.Scp939.Ripples;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    ///     Patches <see cref="FootstepRippleTrigger.OnFootstepPlayed" />
+    ///     to add the <see cref="Scp939.PlayingFootstep" /> event.
+    /// </summary>
+    [HarmonyPatch(typeof(FootstepRippleTrigger), nameof(FootstepRippleTrigger.OnFootstepPlayed))]
+    internal static class PlayingFootstep
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            Label returnLabel = generator.DefineLabel();
+
+            newInstructions.InsertRange(0, new CodeInstruction[]
+                {
+                    // Player player = Player.Get(hub);
+                    new(OpCodes.Ldarg_1),
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(CharacterModel), nameof(CharacterModel.OwnerHub))),
+                    new(OpCodes.Call, Method(typeof(Exiled.API.Features.Player), nameof(Exiled.API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+
+                    // true;
+                    new(OpCodes.Ldc_I4_1),
+
+                    // PlayingFootstepEventArgs ev = (player, true);
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(PlayingFootstepEventArgs))[0]),
+                    new(OpCodes.Dup),
+
+                    // Handlers.Scp939.OnPlayingFootstep(ev);
+                    new(OpCodes.Call, Method(typeof(Handlers.Scp939), nameof(Handlers.Scp939.OnPlayingFootstep))),
+
+                    // if (!IsAllowed)
+                    //      return;
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(PlayingFootstepEventArgs), nameof(PlayingFootstepEventArgs.IsAllowed))),
+                    new(OpCodes.Brfalse_S, returnLabel),
+                });
+
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+
+            foreach (var instruction in newInstructions)
+                yield return instruction;
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
