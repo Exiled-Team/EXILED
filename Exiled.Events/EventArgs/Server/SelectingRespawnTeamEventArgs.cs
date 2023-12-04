5a1becc465019f65f87a2a54976e426240d3c104
louis1706 <louismonneyron5@yahoo.com>
2023-12-04 10:27:42 +0100

    SelectingRespawnTeamEventArgs

---
+++ Exiled.Events/EventArgs/Server/SelectingRespawnTeamEventArgs.cs
@@ -0,0 +1,35 @@
+// -----------------------------------------------------------------------
+// <copyright file="SelectingRespawnTeamEventArgs.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Server
+{
+    using System;
+
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs.Interfaces;
+    using Respawning;
+
+    /// <summary>
+    /// Contains all information before selecting the team to respawn next.
+    /// </summary>
+    public class SelectingRespawnTeamEventArgs : IExiledEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectingRespawnTeamEventArgs"/> class.
+        /// </summary>
+        /// <param name="type">The <see cref="SpawnableTeamType"/> used as the starting value for this event.</param>
+        public SelectingRespawnTeamEventArgs(SpawnableTeamType type)
+        {
+            ChosenTeam = type;
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="SpawnableTeamType"/> that represents the team chosen to spawn.
+        /// </summary>
+        public SpawnableTeamType ChosenTeam { get; set; }
+    }
+}
