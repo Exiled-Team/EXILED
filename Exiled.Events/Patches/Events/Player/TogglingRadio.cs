a4166c6e7e9fa0aa335056434ca40eac4617c8f2
BoltonDev <boltonii.mc@gmail.com>
2023-12-15 20:44:24 +0100

    Convert prefix to transpiler + fix docs

--- Exiled.Events/Patches/Events/Player/TogglingRadio.cs
+++ Exiled.Events/Patches/Events/Player/TogglingRadio.cs
@@ -4,82 +4,114 @@
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
+
 namespace Exiled.Events.Patches.Events.Player
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
 
-    using Exiled.API.Features;
+    using Exiled.API.Features.Items;
+    using Exiled.API.Features.Pools;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+
     using HarmonyLib;
+
     using InventorySystem.Items;
     using InventorySystem.Items.Radio;
+
     using PluginAPI.Events;
 
+    using static HarmonyLib.AccessTools;
+
     /// <summary>
-    ///     Patches <see cref="RadioItem.ServerProcessCmd" />.
-    ///     Adds the <see cref="Handlers.Player.TogglingRadio" /> event.
+    /// Patches <see cref="RadioItem.ServerProcessCmd" />.
+    /// Adds the <see cref="Handlers.Player.TogglingRadio" /> event.
     /// </summary>
     [EventPatch(typeof(Handlers.Player), nameof(Handlers.Player.TogglingRadio))]
     [HarmonyPatch(typeof(RadioItem), nameof(RadioItem.ServerProcessCmd))]
     internal static class TogglingRadio
     {
-        private static bool Prefix(RadioItem __instance, RadioMessages.RadioCommand command)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (__instance.Owner.HasBlock(BlockedInteraction.ItemUsage))
-                return false;
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            switch (command)
+            Label retLabel = generator.DefineLabel();
+
+            LocalBuilder ev = generator.DeclareLocal(typeof(TogglingRadioEventArgs));
+
+            Predicate<CodeInstruction> match = instruction => instruction.opcode == OpCodes.Newobj && (ConstructorInfo)instruction.operand == GetDeclaredConstructors(typeof(PlayerRadioToggleEvent))[0];
+            int offset = -4;
+            int index = newInstructions.FindIndex(match) + offset;
+
+            newInstructions.InsertRange(index, new[]
             {
-                case RadioMessages.RadioCommand.Enable:
-                    {
-                        if (__instance._enabled || __instance._battery <= 0f || !EventManager.ExecuteEvent(new PlayerRadioToggleEvent(__instance.Owner, __instance, true)))
-                            return false;
+                // Player.Get(this.Hub)
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                new(OpCodes.Call, PropertyGetter(typeof(RadioItem), nameof(RadioItem.Owner))),
+                new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
 
-                        TogglingRadioEventArgs ev = new(Player.Get(__instance.Owner), __instance, true);
-                        Handlers.Player.OnTogglingRadio(ev);
-                        if (!ev.IsAllowed)
-                            return false;
+                // this
+                new(OpCodes.Ldarg_0),
 
-                        __instance._enabled = ev.NewState == __instance._enabled ? true : ev.NewState;
-                        break;
-                    }
+                // true
+                new(OpCodes.Ldc_I4_1),
 
-                case RadioMessages.RadioCommand.Disable:
-                    {
-                        if (!__instance._enabled || !EventManager.ExecuteEvent(new PlayerRadioToggleEvent(__instance.Owner, __instance, false)))
-                            return false;
+                // true
+                new(OpCodes.Ldc_I4_1),
 
-                        TogglingRadioEventArgs ev = new(Player.Get(__instance.Owner), __instance, false);
-                        Handlers.Player.OnTogglingRadio(ev);
-                        if (!ev.IsAllowed)
-                            return false;
+                // TogglingRadioEventArgs ev = new TogglingRadioEventArgs(Player, Radio, bool);
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(TogglingRadioEventArgs))[0]),
+                new(OpCodes.Dup),
 
-                        __instance._enabled = ev.NewState == __instance._enabled ? false : ev.NewState;
-                        break;
-                    }
+                // Handlers.Player.OnTogglingRadio(ev);
+                new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnTogglingRadio))),
 
-                case RadioMessages.RadioCommand.ChangeRange:
-                    {
-                        byte b = (byte)(__instance._rangeId + 1);
-                        if (b >= __instance.Ranges.Length)
-                        {
-                            b = 0;
-                        }
+                // if(!ev.IsAllowed)
+                //    return;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(TogglingRadioEventArgs), nameof(TogglingRadioEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retLabel),
+            });
 
-                        PlayerChangeRadioRangeEvent playerChangeRadioRangeEvent = new PlayerChangeRadioRangeEvent(__instance.Owner, __instance, (RadioMessages.RadioRangeLevel)b);
-                        if (!EventManager.ExecuteEvent(playerChangeRadioRangeEvent))
-                        {
-                            return false;
-                        }
+            index = newInstructions.FindLastIndex(match) + offset;
 
-                        b = __instance._rangeId = (byte)playerChangeRadioRangeEvent.Range;
-                        break;
-                    }
-            }
+            newInstructions.InsertRange(index, new[]
+            {
+                // Player.Get(this.Hub)
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                new(OpCodes.Call, PropertyGetter(typeof(RadioItem), nameof(RadioItem.Owner))),
+                new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
 
-            __instance.SendStatusMessage();
-            return false;
+                // this
+                new(OpCodes.Ldarg_0),
+
+                // false
+                new(OpCodes.Ldc_I4_0),
+
+                // true
+                new(OpCodes.Ldc_I4_1),
+
+                // TogglingRadioEventArgs ev = new TogglingRadioEventArgs(Player, Radio, bool);
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(TogglingRadioEventArgs))[0]),
+                new(OpCodes.Dup),
+
+                // Handlers.Player.OnTogglingRadio(ev);
+                new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnTogglingRadio))),
+
+                // if(!ev.IsAllowed)
+                //    return;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(TogglingRadioEventArgs), nameof(TogglingRadioEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retLabel),
+            });
+
+            newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+
+            foreach (var instruction in newInstructions)
+                yield return instruction;
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
     }
 }
