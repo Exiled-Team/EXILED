1a610b914a6797d4d0efd54463b5d1e5cd6f5f79
Yamato <louismonneyron5@yahoo.com>
2023-12-17 17:27:21 +0100

    More fix for NPC

--- Exiled.Events/Patches/Fixes/NpcServerCountFix.cs
+++ Exiled.Events/Patches/Fixes/NpcServerCountFix.cs
@@ -7,6 +7,8 @@
 
 namespace Exiled.Events.Patches.Fixes
 {
+#pragma warning disable SA1402 // File may only contain a single type
+
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
@@ -31,6 +33,8 @@ namespace Exiled.Events.Patches.Fixes
 
             Label labelcontinue = (Label)newInstructions[index - 1].operand;
 
+            // if (Player.Get(referenceHub).IsNPC)
+            //  continue;
             newInstructions.InsertRange(index, new[]
             {
                 new CodeInstruction(OpCodes.Ldloc_1),
@@ -38,6 +42,40 @@ namespace Exiled.Events.Patches.Fixes
                 new(OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.IsNPC))),
                 new(OpCodes.Brtrue_S, labelcontinue),
             });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+
+    /// <summary>
+    /// Patches <see cref="ServerConsole.HandlePlayerJoin"/> to prevent NPC's from being send to NW Info.
+    /// </summary>
+    [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.HandlePlayerJoin))]
+    internal static class NpcServerConsoleFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            int offset = 0;
+            int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ldsflda) + offset;
+
+            Label ret = generator.DefineLabel();
+
+            // if (Player.Get(referenceHub).IsNPC)
+            //  return;
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_1),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.IsNPC))),
+                new(OpCodes.Brtrue_S, ret),
+            });
+
+            newInstructions[index].labels.Add(ret);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
