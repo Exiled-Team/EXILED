cb45d0ac216133f7b58ab44969899bb93f55a204
skyyt15 <doublecraft76ytb@gmail.com>
2024-01-13 11:05:33 +0100

    adding AddingObserver event and RemovingObserver event for Scp173 (again)
    the adding observer is triggered when a user just saw scp 173. for example, if ev.IsAllowed is set to false and the user is the only one who can saw scp173, the peanut can keep moving (it will be not added in the Observers list)
    
    an example when the ev.IsAllowed is set to false
    ![image](https://github.com/Exiled-Team/EXILED/assets/99112969/35454fc1-938a-4c89-bc63-deb0d4dbfbf1)
    
    he RemovingObserver is the same as the AddingObserver event but there is no a IsAllowed parameter (it's weird in this situation) and it's trigger when the player no longer saw scp173.
    
    special thanks to Bolton who help me to find stupid IL errors
    special thing to yamato: i have compile this xmas-2023 version in release build :trollface:
    
    edit: ho i forgot to tell: i have build all change and tested the events with a different plugin in my personnal server

---
+++ Exiled.Events/Patches/Events/Scp173/Observers.cs
@@ -0,0 +1,116 @@
+// -----------------------------------------------------------------------
+// <copyright file="Observers.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Scp173
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using API.Features;
+    using Attributes;
+    using Exiled.API.Features.Pools;
+    using Exiled.Events.EventArgs.Scp173;
+    using HarmonyLib;
+    using PlayerRoles.PlayableScps.Scp173;
+    using PlayerRoles.Subroutines;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="Scp173ObserversTracker.UpdateObserver(ReferenceHub)" />.
+    /// Adds the <see cref="Handlers.Scp173.AddingObserver" /> event.
+    /// </summary>
+    [EventPatch(typeof(Handlers.Scp173), nameof(Handlers.Scp173.AddingObserver))]
+    [HarmonyPatch(typeof(Scp173ObserversTracker), nameof(Scp173ObserversTracker.UpdateObserver))]
+    internal static class Observers
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            // AddingObserver patch
+            Label returnLabel = generator.DefineLabel();
+            LocalBuilder ev = generator.DeclareLocal(typeof(AddingObserverEventArgs));
+
+            int index = newInstructions.FindIndex(x => x.Calls(Method(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Add)))) + 2;
+            newInstructions[index].WithLabels(returnLabel);
+
+            newInstructions.InsertRange(index, new CodeInstruction[]
+            {
+                // Player.Get(Owner);
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(StandardSubroutine<Scp173Role>), nameof(StandardSubroutine<Scp173Role>.Owner))),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+                // Player.Get(ply);
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+                // true
+                new(OpCodes.Ldc_I4_1),
+
+                // AddingObserverEventArgs ev = new(Player.Get(Owner), Player.Get(ply), true);
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(AddingObserverEventArgs))[0]),
+                new(OpCodes.Stloc, ev),
+
+                // Scp173.OnAddingObserver(ev);
+                new(OpCodes.Ldloc, ev),
+                new(OpCodes.Call, Method(typeof(Handlers.Scp173), nameof(Handlers.Scp173.OnAddingObserver))),
+
+                // if (!ev.IsAllowed)
+                new(OpCodes.Ldloc, ev),
+                new(OpCodes.Callvirt,
+                    PropertyGetter(typeof(AddingObserverEventArgs), nameof(AddingObserverEventArgs.IsAllowed))),
+                new(OpCodes.Brtrue_S, returnLabel),
+
+                // Observers.Remove(ply);
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldfld, Field(typeof(Scp173ObserversTracker), nameof(Scp173ObserversTracker.Observers))),
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Callvirt, Method(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Remove))),
+                new(OpCodes.Pop),
+
+                // return 0;
+                new(OpCodes.Ldc_I4_0),
+                new(OpCodes.Ret),
+            });
+
+            // RemoveObserver patch
+            int index2 = newInstructions.FindLastIndex(x => x.Calls(Method(typeof(HashSet<ReferenceHub>), nameof(HashSet<ReferenceHub>.Remove)))) + 2;
+
+            LocalBuilder ev2 = generator.DeclareLocal(typeof(RemoveObserverEventArgs));
+
+            newInstructions.InsertRange(index2, new CodeInstruction[]
+            {
+                // Player.Get(Owner);
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Callvirt,
+                    PropertyGetter(typeof(StandardSubroutine<Scp173Role>), nameof(StandardSubroutine<Scp173Role>.Owner))),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+                // Player.Get(ply);
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+                // new RemoveObserverEventArgs(Player.Get(Owner), Player.Get(ply));
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(RemoveObserverEventArgs))[0]),
+                new(OpCodes.Stloc, ev2),
+
+                // Scp173.OnRemovingObserver(new RemoveObserverEventArgs(Player.Get(Owner), Player.Get(ply)));
+                new(OpCodes.Ldloc, ev2),
+                new(OpCodes.Call, Method(typeof(Handlers.Scp173), nameof(Handlers.Scp173.OnRemoveObserver))),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+            {
+                yield return newInstructions[z];
+            }
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
