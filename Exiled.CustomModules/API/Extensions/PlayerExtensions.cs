436f08e4beff36a8244d9bb7e0b1a1d925a0e3a3
SmusiJarviss <SmusiJarvis@hotmail.com>
2024-01-14 20:05:03 +0100

    Added more extensions

--- Exiled.CustomModules/API/Extensions/PlayerExtensions.cs
+++ Exiled.CustomModules/API/Extensions/PlayerExtensions.cs
@@ -7,8 +7,11 @@
 
 namespace Exiled.CustomModules.API.Extensions
 {
+    using System;
+
     using Exiled.API.Features;
     using Exiled.CustomModules.API.Features;
+    using Exiled.CustomModules.API.Features.CustomItems;
     using Exiled.CustomModules.API.Features.CustomRoles;
 
     /// <summary>
@@ -43,5 +46,14 @@ namespace Exiled.CustomModules.API.Extensions
 
         /// <inheritdoc cref="CustomRole.TryGet(Pawn, out CustomRole)"/>
         public static bool TryGet(this Player player, out CustomRole customRole) => CustomRole.TryGet(player.Cast<Pawn>(), out customRole);
+
+        /// <inheritdoc cref="CustomItem.TryGive(Player, string, bool)"/>
+        public static bool TryGive(this Player player, string name, bool displayMessage = true) => CustomItem.TryGive(player, name, displayMessage);
+
+        /// <inheritdoc cref="CustomItem.TryGive(Player, uint, bool)"/>
+        public static bool TryGive(this Player player, uint id, bool displayMessage = true) => CustomItem.TryGive(player, id, displayMessage);
+
+        /// <inheritdoc cref="CustomItem.TryGive(Player, Type, bool)"/>
+        public static bool TryGive(this Player player, Type type, bool displayMessage) => CustomItem.TryGive(player, type, displayMessage);
     }
 }
