34f604bd7f4264e44a7d4ae50750d64af0e044d3
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-01-20 13:37:19 +0100

    Update Exiled.API/Features/Player.cs
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -1441,7 +1441,7 @@ namespace Exiled.API.Features
         /// <param name="args">The array segment of strings representing the input arguments to be processed.</param>
         /// <param name="startIndex">The starting index within the array segment.</param>
         /// <returns>An <see cref="IEnumerable{Player}"/> representing the processed players.</returns>
-        public static IEnumerable<Player> GetProcessed(ArraySegment<string> args, int startIndex = 0) => GetProcessed(args, startIndex, out string[] _);
+        public static IEnumerable<Player> GetProcessedData(ArraySegment<string> args, int startIndex = 0) => GetProcessedDataargs, startIndex, out string[] _);
 
         /// <summary>
         /// Adds a player's UserId to the list of reserved slots.
