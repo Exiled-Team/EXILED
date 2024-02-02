cb37c05a76beec521e24aceeff81d9eb88ff20af
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-02-02 20:43:28 +0100

    GetNear & GetFar

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -1168,6 +1168,22 @@ namespace Exiled.API.Features
         public static IEnumerable<Player> Get(Func<Player, bool> predicate) => List.Where(predicate);
 
         /// <summary>
+        /// Get all players near the <param name="vector"></param>.
+        /// </summary>
+        /// <param name="vector">The <see cref="Vector3"/> to compare.</param>
+        /// <param name="distance">The max distance the player can be from the <param name="vector"></param> to be included.</param>
+        /// <returns>The filtered <see cref="IEnumerable{T}"/>.</returns>
+        public static IEnumerable<Player> GetNear(Vector3 vector, float distance) => List.Where(p => Vector3.Distance(vector, p.Position) <= 5);
+
+        /// <summary>
+        /// Get all players that have a further distance than the <param name="distance"></param>.
+        /// </summary>
+        /// <param name="vector">The <see cref="Vector3"/> to compare.</param>
+        /// <param name="distance">The minimum distance the player can be from the <param name="vector"></param> to be included.</param>
+        /// <returns>The filtered <see cref="IEnumerable{T}"/>.</returns>
+        public static IEnumerable<Player> GetFar(Vector3 vector, float distance) => List.Where(p => Vector3.Distance(vector, p.Position) >= 5);
+
+        /// <summary>
         /// Gets the <see cref="Player"/> belonging to the <see cref="CommandSystem.ICommandSender"/>, if any.
         /// </summary>
         /// <param name="sender">The command sender.</param>
