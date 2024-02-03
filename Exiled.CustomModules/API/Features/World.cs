068cd759f8b6aa446906aabc95831a17be63b2cb
NaoUnderscore <info@naounderscore.dev>
2024-02-03 09:26:13 +0100

    Implemented custom ending conditions to `World`.

--- Exiled.CustomModules/API/Features/World.cs
+++ Exiled.CustomModules/API/Features/World.cs
@@ -5,16 +5,21 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Exiled.CustomModules.API.Features.CustomGameModes
+namespace Exiled.CustomModules.API.Features
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     using Exiled.API.Extensions;
+    using Exiled.API.Features;
     using Exiled.API.Features.Core;
     using Exiled.API.Features.Core.Generic;
     using Exiled.CustomModules.API.Enums;
+    using Exiled.CustomModules.API.Features.CustomGameModes;
+    using Exiled.CustomModules.API.Features.CustomRoles;
+    using Exiled.CustomModules.Events.EventArgs.CustomRoles;
+    using Exiled.Events.EventArgs.Server;
     using MEC;
-    using PluginAPI.Core;
 
     using Server = Exiled.API.Features.Server;
 
@@ -33,14 +38,19 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
         private GameState gameState;
         private CustomGameMode enqueuedGameMode;
         private CoroutineHandle queueHandle;
+        private SummaryInfo summaryInfo;
 
         /// <summary>
         /// Gets the <see cref="CustomGameModes.GameState"/>.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         public GameState GameState => gameState ??= GetComponent<GameState>();
 
         /// <summary>
         /// Gets or sets the dequeue rate, expressed in seconds.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// <para/>
         /// If enqueued, the game mode will be continuously evaluated until it starts or the enqueued game mode changes.
         /// <br/>
@@ -51,16 +61,34 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
 
         /// <summary>
         /// Gets the enqueued game mode.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         public uint EnqueuedGameMode => enqueuedGameMode.Id;
 
         /// <summary>
         /// Gets or sets the running game mode.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         public uint RunningGameMode { get; protected internal set; }
 
         /// <summary>
+        /// Gets the current summary info.
+        /// </summary>
+        public SummaryInfo SummaryInfo
+        {
+            get
+            {
+                summaryInfo.Update();
+                return summaryInfo;
+            }
+        }
+
+        /// <summary>
         /// Enqueues a custom game mode for execution.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         /// <param name="customGameMode">The custom game mode to enqueue.</param>
         /// <param name="continuously">Whether to continuously enqueue the game mode.</param>
@@ -78,6 +106,8 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
 
         /// <summary>
         /// Enqueues a custom game mode by its ID for execution.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         /// <param name="id">The ID of the custom game mode to enqueue.</param>
         /// <param name="continuously">Whether to continuously enqueue the game mode.</param>
@@ -91,6 +121,8 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
 
         /// <summary>
         /// Clears the current game mode queue.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         public virtual void ClearQueue()
         {
@@ -105,6 +137,8 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
 
         /// <summary>
         /// Starts the execution of the enqueued game mode.
+        /// <para/>
+        /// Requires <see cref="ModuleType.CustomGameModes"/> to be enabled.
         /// </summary>
         /// <param name="customGameMode">The custom game mode to start.</param>
         /// <param name="isForced">Whether to force-start the game mode.</param>
@@ -154,9 +188,68 @@ namespace Exiled.CustomModules.API.Features.CustomGameModes
             Timing.KillCoroutines(queueHandle);
         }
 
+        /// <inheritdoc />
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+
+            if (CustomModules.IsModuleLoaded(ModuleType.CustomRoles))
+            {
+                CustomRole.ChangedCustomRoleDispatcher.Bind(this, OnChangedCustomRole);
+
+                Exiled.Events.Handlers.Server.EndingRound += OnEndingRound;
+            }
+
+            if (CustomModules.IsModuleLoaded(ModuleType.CustomGameModes))
+            {
+                Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+
+            if (CustomModules.IsModuleLoaded(ModuleType.CustomRoles))
+            {
+                CustomRole.ChangedCustomRoleDispatcher.Unbind(this);
+
+                Exiled.Events.Handlers.Server.EndingRound -= OnEndingRound;
+            }
+
+            if (CustomModules.IsModuleLoaded(ModuleType.CustomGameModes))
+            {
+                Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            }
+        }
+
         private void OnRoundStarted()
         {
             EnqueueGameMode(enqueuedGameMode);
+        }
+
+        private void OnEndingRound(EndingRoundEventArgs ev)
+        {
+            if (!ev.IsAllowed)
+                return;
+
+            foreach (CustomRole role in CustomRole.Get(cr => cr.TeamsOwnership.Length != 1 && cr.Instances > 1))
+            {
+                if (role.Owners.First().RoleBehaviour.EvaluateEndingConditions())
+                    continue;
+
+                ev.IsAllowed = false;
+                return;
+            }
+        }
+
+        private void OnChangedCustomRole(ChangedCustomRoleEventArgs ev)
+        {
+            if (!CustomRole.TryGet(ev.Role, out CustomRole role) || role.TeamsOwnership.Count() == 1)
+                return;
+
+            Round.IgnoredPlayers.Add(ev.Player);
         }
 
         private IEnumerator<float> DequeueGameMode()
