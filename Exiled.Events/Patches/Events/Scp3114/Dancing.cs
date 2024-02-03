a29dd4144e4997b3313519a5de46f7b7b10adcf2
VALERA771 <val.germanov@gmail.com>
2024-02-04 01:29:24 +0300

    fx

--- Exiled.Events/Patches/Events/Scp3114/Dancing.cs
+++ Exiled.Events/Patches/Events/Scp3114/Dancing.cs
@@ -11,7 +11,7 @@ namespace Exiled.Events.Patches.Events.Scp3114
     using System.Reflection.Emit;
 
     using Exiled.API.Features;
-    using Exiled.API.Features.Pools;
+    using Exiled.API.Features.Core.Generic.Pools;
     using Exiled.Events.EventArgs.Scp3114;
     using HarmonyLib;
     using PlayerRoles.PlayableScps.Scp3114;
