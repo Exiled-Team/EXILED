9f9f62d13bd43a47edc50bf32d85eef88bee7922
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-03-09 00:50:33 +0100

    Spawning.cs

--- Exiled.Events/Patches/Events/Player/Spawning.cs
+++ Exiled.Events/Patches/Events/Player/Spawning.cs
@@ -10,13 +10,16 @@ namespace Exiled.Events.Patches.Events.Player
     using System.Reflection;
 
     using API.Features;
-    using Exiled.API.Extensions;
-    using Exiled.API.Features.Spawn;
+    using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
+
     using HarmonyLib;
+
     using PlayerRoles;
     using PlayerRoles.FirstPersonControl;
+    using PlayerRoles.FirstPersonControl.NetworkMessages;
     using PlayerRoles.FirstPersonControl.Spawnpoints;
+
     using UnityEngine;
 
     using static HarmonyLib.AccessTools;
@@ -24,7 +27,7 @@ namespace Exiled.Events.Patches.Events.Player
     /// <summary>
     /// Patches <see cref="RoleSpawnpointManager.Init"/> delegate.
     /// Adds the <see cref="Handlers.Player.Spawning"/> event.
-    /// Fix for spawning in wrong location.
+    /// Fix for spawning in void.
     /// </summary>
     [HarmonyPatch]
     internal static class Spawning
@@ -36,7 +39,7 @@ namespace Exiled.Events.Patches.Events.Player
 
         private static bool Prefix(ReferenceHub hub, PlayerRoleBase prevRole, PlayerRoleBase newRole)
         {
-            if (newRole.ServerSpawnReason == RoleChangeReason.Destroyed || !Player.TryGet(hub, out Player player))
+            if (newRole.ServerSpawnReason == RoleChangeReason.Destroyed || Player.TryGet(hub, out Player player))
                 return true;
 
             Vector3 oldPosition = hub.transform.position;
@@ -44,14 +47,10 @@ namespace Exiled.Events.Patches.Events.Player
 
             if (newRole is IFpcRole fpcRole)
             {
-                if (newRole.ServerSpawnFlags.HasFlag(RoleSpawnFlags.UseSpawnpoint))
+                if (newRole.ServerSpawnFlags.HasFlag(RoleSpawnFlags.UseSpawnpoint) && fpcRole.SpawnpointHandler != null && fpcRole.SpawnpointHandler.TryGetSpawnpoint(out Vector3 position, out float horizontalRot))
                 {
-                    SpawnLocation spawnLocation = newRole.RoleTypeId.GetRandomSpawnLocation();
-                    if (spawnLocation != null)
-                    {
-                        oldPosition = spawnLocation.Position;
-                        oldRotation = spawnLocation.HorizontalRotation;
-                    }
+                    oldPosition = position;
+                    oldRotation = horizontalRot;
                 }
 
                 SpawningEventArgs ev = new(player, oldPosition, oldRotation, prevRole);
