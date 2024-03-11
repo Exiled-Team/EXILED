53ba6004869bdc5ef8aadbbec11b941630339ad6
VALERA771 <val.germanov@gmail.com>
2024-03-11 23:29:52 +0300

    fix + comments

--- Exiled.Events/Patches/Events/Scp106/PassingDoor.cs
+++ Exiled.Events/Patches/Events/Scp106/PassingDoor.cs
@@ -31,32 +31,43 @@ namespace Exiled.Events.Patches.Events.Scp106
 
             int index = newInstructions.FindIndex(x => x.opcode == OpCodes.Ldloc_0);
 
-            Label returnLabel = generator.DefineLabel();
+            Label continueLabel = generator.DefineLabel();
 
             newInstructions.InsertRange(
                 index,
                 new[]
                 {
+                    // Player.Get(this.Hub);
                     new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(Scp106MovementModule), nameof(Scp106MovementModule.Hub))),
                     new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
+                    // Door.Get(comp);
                     new(OpCodes.Ldloc_0),
                     new(OpCodes.Call, Method(typeof(Door), nameof(Door.Get), new[] { typeof(DoorVariant) })),
 
+                    // true
                     new(OpCodes.Ldc_I4_1),
 
+                    // PassingDoorEventArgs ev = new(Player, Door, true);
                     new(OpCodes.Newobj, GetDeclaredConstructors(typeof(PassingDoorEventArgs))[0]),
                     new(OpCodes.Dup),
                     new(OpCodes.Dup),
 
+                    // Handlers.Scp106.OnPassingDoor(ev);
                     new(OpCodes.Call, Method(typeof(Handlers.Scp106), nameof(Handlers.Scp106.OnPassingDoor))),
 
+                    // if (ev.IsAllowed)
+                    //    goto continueLabel;
                     new(OpCodes.Callvirt, PropertyGetter(typeof(PassingDoorEventArgs), nameof(PassingDoorEventArgs.IsAllowed))),
-                    new(OpCodes.Brfalse_S, returnLabel),
+                    new(OpCodes.Brtrue_S, continueLabel),
+
+                    // return 0;
+                    new(OpCodes.Ldc_R4, 0.0f),
+                    new(OpCodes.Ret),
                 });
 
-            newInstructions[index].labels.Add(returnLabel);
+            newInstructions[index].labels.Add(continueLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
