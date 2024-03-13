816ab2a6da5b22a14620e890bbfffe9645b751c8
Yamato <louismonneyron5@yahoo.com>
2024-03-13 14:42:31 +0100

    Combine AddExperience(int, Scp079HudTranslation) with AddExperience(int, Scp079HudTranslation, RoleTypeId)

--- Exiled.API/Features/Roles/Scp079Role.cs
+++ Exiled.API/Features/Roles/Scp079Role.cs
@@ -478,15 +478,8 @@ namespace Exiled.API.Features.Roles
         /// </summary>
         /// <param name="amount">The amount to grant.</param>
         /// <param name="reason">The reason to grant experience.</param>
-        public void AddExperience(int amount, Scp079HudTranslation reason = Scp079HudTranslation.ExpGainAdminCommand) => TierManager.ServerGrantExperience(amount, reason);
-
-        /// <summary>
-        /// Grants SCP-079 experience.
-        /// </summary>
-        /// <param name="amount">The amount to grant.</param>
-        /// <param name="reason">The reason to grant experience.</param>
         /// <param name="subject">The RoleType of the player that's causing it to happen.</param>
-        public void AddExperience(int amount, Scp079HudTranslation reason, RoleTypeId subject) => TierManager.ServerGrantExperience(amount, reason, subject);
+        public void AddExperience(int amount, Scp079HudTranslation reason = Scp079HudTranslation.ExpGainAdminCommand, RoleTypeId subject = RoleTypeId.None) => TierManager.ServerGrantExperience(amount, reason, subject);
 
         /// <summary>
         /// Locks the provided <paramref name="door"/>.
