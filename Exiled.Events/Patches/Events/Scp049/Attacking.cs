df43e902f3204dbb056680a7356c5a80d14188b9
Yamato <66829532+louis1706@users.noreply.github.com>
2024-04-06 19:22:28 +0200

    Update Exiled.Events/Patches/Events/Scp049/Attacking.cs
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>

--- Exiled.Events/Patches/Events/Scp049/Attacking.cs
+++ Exiled.Events/Patches/Events/Scp049/Attacking.cs
@@ -67,7 +67,7 @@ namespace Exiled.Events.Patches.Events.Scp049
                     new(OpCodes.Brfalse_S, ret),
                 });
 
-            newInstructions[newInstructions.Count - 1].WithLabels(ret);
+            newInstructions[newInstructions.Count - 1].labels.Add(ret);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
