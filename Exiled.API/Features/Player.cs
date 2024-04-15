7b244ab41016d25bbfb45b5683952117280203b3
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2024-04-15 21:21:07 +0200

    changed to lambda expression + docs

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -1813,13 +1813,8 @@ namespace Exiled.API.Features
         /// Tries to remove a <see cref="IEnumerable{T}"/> of <see cref="RoleTypeId"/> from FriendlyFire rules.
         /// </summary>
         /// <param name="roles"> The <see cref="IEnumerable{T}"/> of <see cref="RoleTypeId"/> to remove. </param>
-        public void TryRemoveFriendlyFire(List<RoleTypeId> roles)
-        {
-            foreach(RoleTypeId roleType in roles)
-            {
-                FriendlyFireMultiplier.Remove(roleType);
-            }
-        }
+        /// <returns> Whether or not all roles were removed from FriendlyFire rules successfully.</returns>
+        public bool TryRemoveFriendlyFire(List<RoleTypeId> roles) => roles.All(role => TryRemoveFriendlyFire(role));
 
         /// <summary>
         /// Tries to remove <see cref="RoleTypeId"/> from FriendlyFire rules.
