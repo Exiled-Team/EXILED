bf41bd33f55173923c86f3d86cac7b0d3cee0d62
Yamato <louismonneyron5@yahoo.com>
2024-04-17 20:11:07 +0200

    Cringe 2

--- Exiled.Events/Patches/Generic/GetCustomCategoryLimit.cs
+++ Exiled.Events/Patches/Generic/GetCustomCategoryLimit.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="GetCustomAmmoLimit.cs" company="Exiled Team">
+// <copyright file="GetCustomCategoryLimit.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -13,19 +13,19 @@ namespace Exiled.Events.Patches.Fixes
     using InventorySystem.Configs;
 
     /// <summary>
-    /// Patches the <see cref="InventoryLimits.GetAmmoLimit(ItemType, ReferenceHub)"/> delegate.
+    /// Patches the <see cref="InventoryLimits.GetCategoryLimit(ItemCategory, ReferenceHub)"/> delegate.
     /// Sync <see cref="API.Features.Player.SetAmmoLimit(API.Enums.AmmoType, ushort)"/>.
     /// Changes <see cref="ushort.MaxValue"/> to <see cref="ushort.MinValue"/>.
     /// </summary>
-    [HarmonyPatch(typeof(InventoryLimits), nameof(InventoryLimits.GetAmmoLimit), new Type[] { typeof(ItemType), typeof(ReferenceHub), })]
-    internal class GetCustomAmmoLimit
+    [HarmonyPatch(typeof(InventoryLimits), nameof(InventoryLimits.GetCategoryLimit), new Type[] { typeof(ItemCategory), typeof(ReferenceHub), })]
+    internal class GetCustomCategoryLimit
     {
         private int Postfix(API.Features.Player player, int value, ItemType ammotype)
         {
-            if (player?.ammoLimits is null)
+            if (player?.categoryLimits is null)
                 return value;
 
-            return player.ammoLimits.Find(x => x.AmmoType == ammotype).Limit + value - InventoryLimits.GetAmmoLimit(null, ammotype);
+            return player.categoryLimits[(int)ammotype] + value - InventoryLimits.GetAmmoLimit(null, ammotype);
         }
     }
 }
