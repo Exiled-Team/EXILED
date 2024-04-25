94f185321398e237d5825f9d44a37338ec4b32d4
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-04-25 12:34:20 +0200

    commit consistency

--- Exiled.API/Features/Npc.cs
+++ Exiled.API/Features/Npc.cs
@@ -217,12 +217,12 @@ namespace Exiled.API.Features
         /// <param name="position">The position to look at.</param>
         public void LookAt(Vector3 position)
         {
-            if (Role is FpcRole fpc)
-            {
-                Vector3 direction = position - Position;
-                Quaternion quat = Quaternion.LookRotation(direction, Vector3.up);
-                LookAt(quat);
-            }
+            if (Role is not FpcRole)
+                return;
+
+            Vector3 direction = position - Position;
+            Quaternion quat = Quaternion.LookRotation(direction, Vector3.up);
+            LookAt(quat);
         }
 
         /// <summary>
