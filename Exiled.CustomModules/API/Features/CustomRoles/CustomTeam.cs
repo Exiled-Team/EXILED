ec25037b75fa79bce81f8307e7e426910ad2a14f
Yamato <louismonneyron5@yahoo.com>
2024-05-13 13:52:49 +0200

    FixNaoSpawningOverwatch player

--- Exiled.CustomModules/API/Features/CustomRoles/CustomTeam.cs
+++ Exiled.CustomModules/API/Features/CustomRoles/CustomTeam.cs
@@ -15,6 +15,7 @@ namespace Exiled.CustomModules.API.Features.CustomRoles
     using Exiled.API.Extensions;
     using Exiled.API.Features;
     using Exiled.API.Features.Core;
+    using Exiled.API.Features.Roles;
     using Exiled.CustomModules.API.Enums;
     using Exiled.CustomModules.API.Features.Attributes;
     using HarmonyLib;
@@ -326,11 +327,10 @@ namespace Exiled.CustomModules.API.Features.CustomRoles
         /// Tries to spawn the specified <see cref="CustomTeam"/>.
         /// </summary>
         /// <param name="customTeam">The <see cref="CustomTeam"/> to be spawned.</param>
-        /// <param name="isForced">Forces the respawn wave regardless any conditions, including tickets.</param>
         /// <returns><see langword="true"/> if the <see cref="CustomTeam"/> was successfully spawned; otherwise, <see langword="false"/>.</returns>
-        public static bool TryRespawn(CustomTeam customTeam, bool isForced = false)
+        public static bool TryRespawn(CustomTeam customTeam)
         {
-            if (!Player.Get(p => p.IsDead).Any() || customTeam is null)
+            if (!Player.List.Any(x => x.Role is SpectatorRole { IsReadyToRespawn: true }) || customTeam is null)
                 return false;
 
             customTeam.Respawn();
@@ -341,11 +341,10 @@ namespace Exiled.CustomModules.API.Features.CustomRoles
         /// Tries to spawn a <see cref="CustomTeam"/> given the specified id.
         /// </summary>
         /// <param name="id">The specified id.</param>
-        /// <param name="isForced">Forces the respawn wave regardless any conditions, including tickets.</param>
         /// <returns><see langword="true"/> if the <see cref="CustomTeam"/> was successfully spawned; otherwise, <see langword="false"/>.</returns>
-        public static bool TryRespawn(uint id, bool isForced = false)
+        public static bool TryRespawn(uint id)
         {
-            if (!Player.Get(p => p.IsDead).Any() || TryGet(id, out CustomTeam customTeam))
+            if (!Player.List.Any(x => x.Role is SpectatorRole { IsReadyToRespawn: true }) || TryGet(id, out CustomTeam customTeam))
                 return false;
 
             customTeam.Respawn();
