99799b6f7755f33ed8a938f5ff37f4e005c743a9
Yamato <louismonneyron5@yahoo.com>
2024-05-13 13:59:53 +0200

    StaminaSyncValue

--- Exiled.API/Features/CustomStats/CustomStaminaStat.cs
+++ Exiled.API/Features/CustomStats/CustomStaminaStat.cs
@@ -1,5 +1,13 @@
-﻿namespace Exiled.API.Features.CustomStats
+// -----------------------------------------------------------------------
+// <copyright file="CustomStaminaStat.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.CustomStats
 {
+    using Mirror;
     using PlayerStatsSystem;
 
     /// <summary>
@@ -14,5 +22,27 @@
         /// Gets or sets the maximum amount of health the player will have.
         /// </summary>
         public float CustomMaxValue { get; set; }
+
+        /// <summary>
+        /// Overiding NW Method to sync Player percentage of Stamina.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        public override void WriteValue(NetworkWriter writer)
+        {
+            if (CustomMaxValue == default)
+            {
+                base.WriteValue(writer);
+                return;
+            }
+
+            writer.WriteByte(ToByte(CurValue / CustomMaxValue));
+        }
+
+        /// <summary>
+        /// Overiding NW Method to sync Player percentage of Stamina.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns>the float value sync to server.</returns>
+        public override float ReadValue(NetworkReader reader) => CustomMaxValue == default ? base.ReadValue(reader) : CurValue * CustomMaxValue;
     }
 }
