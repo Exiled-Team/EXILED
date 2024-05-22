253e62288cbaac12beb39d8898ab21bc212e313f
VALERA771 <val.germanov@gmail.com>
2024-05-22 13:20:05 +0300

    new command №1

--- Exiled.Events/Commands/PluginManager/Patches.cs
+++ Exiled.Events/Commands/PluginManager/Patches.cs
@@ -8,16 +8,16 @@
 namespace Exiled.Events.Commands.PluginManager
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
 
     using CommandSystem;
-
+    using Exiled.API.Features;
     using Exiled.Events.Features;
     using Exiled.Permissions.Extensions;
-
     using NorthwoodLib.Pools;
-
     using RemoteAdmin;
 
     /// <summary>
@@ -51,19 +51,45 @@ namespace Exiled.Events.Commands.PluginManager
 
             StringBuilder sb = StringBuilderPool.Shared.Rent();
 
-            sb.AppendLine("All patches:");
-            sb.AppendLine("Patched:");
-
-            foreach (Type patch in Patcher.GetAllPatchTypes().Where((type) => !Patcher.UnpatchedTypes.Contains(type)))
+            if (arguments.Count == 0)
             {
-                sb.AppendLine($"\t{patch.FullName}");
+                sb.AppendLine("All patches:");
+                sb.AppendLine("Patched:");
+
+                foreach (Type patch in Patcher.GetAllPatchTypes().Where((type) => !Patcher.UnpatchedTypes.Contains(type)))
+                {
+                    sb.AppendLine($"\t{patch.FullName}");
+                }
+
+                sb.AppendLine("Unpatched: ");
+
+                foreach (Type patch in Patcher.UnpatchedTypes)
+                {
+                    sb.AppendLine($"\t{patch.FullName}");
+                }
             }
-
-            sb.AppendLine("Unpatched: ");
-
-            foreach (Type patch in Patcher.UnpatchedTypes)
+            else
             {
-                sb.AppendLine($"\t{patch.FullName}");
+                string name = arguments.At(0);
+                sb.AppendLine($"Events to which plugin {name} subscribed:");
+
+                foreach (PropertyInfo handler in typeof(Patches).Assembly.GetTypes().Where(x => x.Namespace != null && x.Namespace.Contains("Exiled.Events.Handlers")).SelectMany(x => x.GetProperties()))
+                {
+                    if (!handler.GetMethod.IsStatic)
+                        continue;
+
+                    object handlerInstance = handler.GetValue(null);
+                    IReadOnlyCollection<string> list = (IReadOnlyCollection<string>)handler.PropertyType.GetProperty("SubscribedPlugins")?.GetValue(handlerInstance);
+
+                    if (list == null)
+                    {
+                        Log.Error("List of subscribed plugins is null!");
+                        continue;
+                    }
+
+                    if (list.Contains(name))
+                        sb.AppendLine($"{handler.DeclaringType?.Name}.{handler.Name}");
+                }
             }
 
             response = sb.ToString();
