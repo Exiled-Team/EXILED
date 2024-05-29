892b6f571bcddf1c1a4d6f55ccce599178441f4d
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-05-29 17:11:20 +0200

    Extensions

--- Exiled.API/Extensions/CoroutineExtensions.cs
+++ Exiled.API/Extensions/CoroutineExtensions.cs
@@ -19,5 +19,26 @@ namespace Exiled.API.Extensions
         /// </summary>
         /// <param name="coroutine">The coroutine to kill.</param>
         public static void Kill(this CoroutineHandle coroutine) => Timing.KillCoroutines(coroutine);
+
+        /// <summary>
+        /// Gets whether or not a coroutine is running.
+        /// </summary>
+        /// <param name="handle">The handle.</param>
+        /// <returns>A bool indicating whether or not the coroutine is running.</returns>
+        public static bool IsRunning(this CoroutineHandle handle) => Timing.IsRunning(handle);
+
+        /// <summary>
+        /// Gets whether or not a coroutine is running or paused.
+        /// </summary>
+        /// <param name="handle">The handle.</param>
+        /// <returns>A bool indicating whether or not the coroutine is running or paused.</returns>
+        public static bool IsRunningOrPaused(this CoroutineHandle handle) => Timing.IsRunning(handle) || Timing.IsAliveAndPaused(handle);
+
+        /// <summary>
+        /// Sets the tag of a coroutine.
+        /// </summary>
+        /// <param name="coroutine">The coroutine to set the tag of.</param>
+        /// <param name="newTag">The new tag to set.</param>
+        public static void SetTag(this CoroutineHandle coroutine, string newTag) => Timing.SetTag(coroutine, newTag);
     }
 }
