0cd9459d935b59e53daabb035458468c709d284a
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-05-30 21:41:29 +0200

    PrefabHelper

---
+++ Exiled.API/Enums/PrefabType.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PrefabType.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Enums
+{
+    /// <summary>
+    /// Type of prefab.
+    /// </summary>
+    public enum PrefabType
+    {
+        /// <summary>
+        /// Used for <see cref="Features.Workstation"/>.
+        /// </summary>
+        Workstation,
+    }
+}
---
+++ Exiled.API/Features/PrefabHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Exiled.API.Features
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using Exiled.API.Enums;
+    using Mirror;
+    using UnityEngine;
+
+    /// <summary>
+    /// Helper for Prefabs.
+    /// </summary>
+    public static class PrefabHelper
+    {
+        private static Dictionary<PrefabType, GameObject> stored = new();
+
+        /// <summary>
+        /// Gets a dictionary of <see cref="PrefabType"/> to <see cref="GameObject"/>.
+        /// </summary>
+        public static ReadOnlyDictionary<PrefabType, GameObject> PrefabToGameObject => new(stored);
+
+        /// <summary>
+        /// Loads the prefabs.
+        /// </summary>
+        internal static void LoadPrefabs()
+        {
+            stored.Clear();
+
+            stored.Add(PrefabType.Workstation, NetworkClient.prefabs.Values.First(x => x.name.Contains("Work Station")));
+        }
+    }
+}
