28e16d03bb5ee98a832c0558fd57eddbabea20ba
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-05-30 10:18:04 +0200

    Stats

---
+++ Exiled.API/Features/CustomStats/CustomStaminaStat.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CustomStaminaStat.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.CustomStats
+{
+    using Mirror;
+    using PlayerStatsSystem;
+
+    /// <summary>
+    /// A custom version of <see cref="StaminaStat"/> which allows the player's maximum amount of Stamina to be changed.
+    /// </summary>
+    public class CustomStaminaStat : StaminaStat
+    {
+        /// <inheritdoc/>
+        public override float MaxValue => CustomMaxValue == default ? base.MaxValue : CustomMaxValue;
+
+        /// <summary>
+        /// Gets or sets the maximum amount of stamina the player will have.
+        /// </summary>
+        public float CustomMaxValue { get; set; } = 10;
+
+        /// <summary>
+        /// Overiding NW Method to sync Player percentage of Stamina.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        public override void WriteValue(NetworkWriter writer)
+        {
+            if (CustomMaxValue == default)
+            {
+                base.WriteValue(writer);
+                return;
+            }
+
+            writer.WriteByte(ToByte(CurValue / CustomMaxValue));
+        }
+
+        /// <summary>
+        /// Overriding NW Method to sync Player percentage of Stamina.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns>the float value sync to server.</returns>
+        public override float ReadValue(NetworkReader reader) => CustomMaxValue == default ? base.ReadValue(reader) : CurValue * CustomMaxValue;
+    }
+}
