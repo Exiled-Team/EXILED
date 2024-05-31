ee96294c459d78338251e189142327479ec9b1a9
Bolton <48883340+BoltonDev@users.noreply.github.com>
2024-05-31 13:25:40 +0200

    `[Exiled::API]` Helper prefab system (#1)

--- Exiled.API/Features/PrefabHelper.cs
+++ Exiled.API/Features/PrefabHelper.cs
@@ -7,11 +7,14 @@
 
 namespace Exiled.API.Features
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using System.Reflection;
 
     using Exiled.API.Enums;
+    using Exiled.API.Features.Attributes;
     using Mirror;
     using UnityEngine;
 
@@ -20,21 +23,63 @@ namespace Exiled.API.Features
     /// </summary>
     public static class PrefabHelper
     {
-        private static Dictionary<PrefabType, GameObject> stored = new();
+        private static readonly Dictionary<PrefabType, GameObject> Stored = new();
 
         /// <summary>
         /// Gets a dictionary of <see cref="PrefabType"/> to <see cref="GameObject"/>.
         /// </summary>
-        public static ReadOnlyDictionary<PrefabType, GameObject> PrefabToGameObject => new(stored);
+        public static ReadOnlyDictionary<PrefabType, GameObject> PrefabToGameObject => new(Stored);
 
         /// <summary>
-        /// Loads the prefabs.
+        /// Gets the prefab attribute of a prefab type.
+        /// </summary>
+        /// <param name="prefabType">The prefab type.</param>
+        /// <returns>The <see cref="PrefabAttribute" />.</returns>
+        public static PrefabAttribute GetPrefabAttribute(this PrefabType prefabType)
+        {
+            Type type = prefabType.GetType();
+            return type.GetField(Enum.GetName(type, prefabType)).GetCustomAttribute<PrefabAttribute>();
+        }
+
+        /// <summary>
+        /// Tries to get a <see cref="GameObject"/> of the specified <see cref="PrefabType"/>.
+        /// </summary>
+        /// <param name="prefabType">The prefab type.</param>
+        /// <param name="prefab">The corresponding <see cref="GameObject"/>.</param>
+        /// <returns><see langword="true"/> if a <see cref="GameObject"/> of the specified <see cref="PrefabType"/> was found; otherwise, <see langword="false"/>.</returns>
+        public static bool TryGetGameObject(PrefabType prefabType, out GameObject prefab)
+        {
+            return Stored.TryGetValue(prefabType, out prefab);
+        }
+
+        /// <summary>
+        /// Spawns a prefab on server.
+        /// </summary>
+        /// <param name="prefabType">The prefab type.</param>
+        /// <param name="position">The position to spawn the prefab.</param>
+        /// <param name="rotation">The rotation of the prefab.</param>
+        /// <returns>The <see cref="GameObject"/> instantied.</returns>
+        public static GameObject Spawn(PrefabType prefabType, Vector3 position = default, Quaternion rotation = default)
+        {
+            if (!TryGetGameObject(prefabType, out GameObject gameObject))
+                return null;
+            GameObject newGameObject = UnityEngine.Object.Instantiate(gameObject, position, rotation);
+            NetworkServer.Spawn(newGameObject);
+            return newGameObject;
+        }
+
+        /// <summary>
+        /// Loads all prefabs.
         /// </summary>
         internal static void LoadPrefabs()
         {
-            stored.Clear();
+            Stored.Clear();
 
-            stored.Add(PrefabType.Workstation, NetworkClient.prefabs.Values.First(x => x.name.Contains("Work Station")));
+            foreach (PrefabType prefabType in Enum.GetValues(typeof(PrefabType)))
+            {
+                PrefabAttribute attribute = prefabType.GetPrefabAttribute();
+                Stored.Add(prefabType, NetworkClient.prefabs.FirstOrDefault(prefab => prefab.Key == attribute.AssetId || prefab.Value.name.Contains(attribute.Name)).Value);
+            }
         }
     }
 }
