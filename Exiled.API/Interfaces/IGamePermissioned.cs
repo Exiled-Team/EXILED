07d06b61af292874039d0aa0b9c4a0b5aa26a076
Vladislav-CS <vladislavcs@proton.me>
2024-06-01 18:03:30 +0300

    Initial commit

--- Exiled.API/Interfaces/IGamePermissioned.cs
+++ Exiled.API/Interfaces/IGamePermissioned.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="IPermissionCommand.cs" company="Exiled Team">
+// <copyright file="IGamePermissioned.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -7,16 +7,14 @@
 
 namespace Exiled.API.Interfaces
 {
-    using CommandSystem;
-
     /// <summary>
-    /// Represents an interface for commands with permissions.
+    /// Represents an interface for commands with game permissions.
     /// </summary>
-    public interface IPermissionCommand : ICommand
+    public interface IGamePermissioned
     {
         /// <summary>
         /// Gets the permissions of a command.
         /// </summary>
-        public string Permission { get; }
+        public PlayerPermissions[] Permissions { get; }
     }
 }
--- Exiled.API/Interfaces/IPermissioned.cs
+++ Exiled.API/Interfaces/IPermissioned.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="IGamePermissionCommand.cs" company="Exiled Team">
+// <copyright file="IPermissioned.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -7,16 +7,14 @@
 
 namespace Exiled.API.Interfaces
 {
-    using CommandSystem;
-
     /// <summary>
-    /// Represents an interface for commands with game permissions.
+    /// Represents an interface for commands with permissions.
     /// </summary>
-    public interface IGamePermissionCommand : ICommand
+    public interface IPermissioned
     {
         /// <summary>
         /// Gets the permissions of a command.
         /// </summary>
-        public PlayerPermissions[] Permissions { get; }
+        public string Permission { get; }
     }
 }
