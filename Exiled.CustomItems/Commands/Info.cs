475048abed4ee3a37af273d492bce055a06738ec
Bolton <boltonii.mc@gmail.com>
2024-06-05 19:22:58 +0200

    fix: missing member

--- Exiled.CustomItems/Commands/Info.cs
+++ Exiled.CustomItems/Commands/Info.cs
@@ -40,6 +40,9 @@ namespace Exiled.CustomItems.Commands
         /// <inheritdoc/>
         public string Description { get; } = "Gets more information about the specified custom item.";
 
+        /// <inheritdoc />
+        public bool SanitizeResponse { get; }
+
         /// <inheritdoc/>
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
