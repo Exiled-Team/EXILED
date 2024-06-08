b12f7c93f51abac91838670cf8ad009359ad7fe2
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2024-06-08 14:28:29 +0200

    fix typo

--- Exiled.API/Features/Core/NullableObject.cs
+++ Exiled.API/Features/Core/NullableObject.cs
@@ -1,5 +1,4 @@
 // -----------------------------------------------------------------------
-// // -----------------------------------------------------------------------
 // <copyright file="NullableObject.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
