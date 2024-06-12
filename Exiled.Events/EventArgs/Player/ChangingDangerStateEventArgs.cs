f9e72a2f7dbc0796d62443ba13aa673694b5ddc2
Ika <36999341+IkaOverride@users.noreply.github.com>
2024-06-13 00:38:26 +0200

    EncounteredPlayer danger state event

--- Exiled.Events/EventArgs/Player/ChangingDangerStateEventArgs.cs
+++ Exiled.Events/EventArgs/Player/ChangingDangerStateEventArgs.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="ChangingDangerStateEventArgs.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -24,18 +24,20 @@ namespace Exiled.Events.EventArgs.Player
         /// <param name="danger">The <see cref="DangerStackBase"/>.</param>
         /// <param name="type">The <see cref="DangerType"/>.</param>
         /// <param name="activating">Whether or not the danger is being activated. If false it is ending.</param>
+        /// <param name="encounteredPlayer">The player that has been encountered if the danger is an encounter or null if it isn't.</param>
         /// <param name="isAllowed">Whether or not it is allowed to activate/deactivate.</param>
-        public ChangingDangerStateEventArgs(Player player, DangerStackBase danger, DangerType type, bool activating, bool isAllowed = true)
+        public ChangingDangerStateEventArgs(Player player, DangerStackBase danger, DangerType type, bool activating, Player encounteredPlayer = null, bool isAllowed = true)
         {
             Player = player;
             Danger = danger;
             Type = type;
             IsActivating = activating;
+            EncounteredPlayer = encounteredPlayer;
             IsAllowed = isAllowed;
         }
 
         /// <inheritdoc />
-        public API.Features.Player Player { get; }
+        public Player Player { get; }
 
         /// <summary>
         /// Gets the <see cref="DangerStackBase"/>.
@@ -56,6 +58,11 @@ namespace Exiled.Events.EventArgs.Player
         /// Gets a value indicating whether or not the danger is ending.
         /// </summary>
         public bool IsEnding => !IsActivating;
+
+        /// <summary>
+        /// Gets the encountered player if the danger is an encounter or null if it isn't.
+        /// </summary>
+        public Player EncounteredPlayer { get; }
 
         /// <inheritdoc />
         public bool IsAllowed { get; set; }
