6b6785dbb1a8f3d218764b8bec6911f517528178
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2024-06-23 15:13:52 +0200

    Revert "[EXILED9] (#2635)"
    This reverts commit 99abf6c00371454242859622a549ef6e4a76e590.

--- Exiled.Loader/Loader.cs
+++ Exiled.Loader/Loader.cs
@@ -87,8 +87,6 @@ namespace Exiled.Loader
         public static void LoadPlugins()
         {
             File.Delete(Path.Combine(Paths.Plugins, "Exiled.Updater.dll"));
-            File.Delete(Path.Combine(Paths.Plugins, "Exiled.CustomRoles.dll"));
-            File.Delete(Path.Combine(Paths.Plugins, "Exiled.CustomItems.dll"));
 
             foreach (string assemblyPath in Directory.GetFiles(Paths.Plugins, "*.dll"))
             {
