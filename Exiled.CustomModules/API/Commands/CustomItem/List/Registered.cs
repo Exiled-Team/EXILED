d9f596f2cc124f45b7847273a11d900562018291
Monald <63928518+Monaldcry7788@users.noreply.github.com>
2024-07-15 20:51:30 +0200

    Re-added customrole and customitem commands. (#2715)
    * Re-added customrole and customitem commands.
    
    * fix stylecop momento
    
    ---------
    
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>

---
+++ Exiled.CustomModules/API/Commands/CustomItem/List/Registered.cs
@@ -0,0 +1,76 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Registered.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.CustomModules.API.Commands.CustomItem.List
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using CommandSystem;
+    using Exiled.API.Features.Core.Generic.Pools;
+    using Exiled.Permissions.Extensions;
+
+    using CustomItem = Exiled.CustomModules.API.Features.CustomItems.CustomItem;
+
+    /// <inheritdoc/>
+    internal sealed class Registered : ICommand
+    {
+        private Registered()
+        {
+        }
+
+        /// <summary>
+        /// Gets the command instance.
+        /// </summary>
+        public static Registered Instance { get; } = new();
+
+        /// <inheritdoc/>
+        public string Command { get; } = "registered";
+
+        /// <inheritdoc/>
+        public string[] Aliases { get; } = { "r", "reg" };
+
+        /// <inheritdoc cref="SanitizeResponse" />
+        public bool SanitizeResponse { get; } = true;
+
+        /// <inheritdoc/>
+        public string Description { get; } = "Gets a list of registered custom items.";
+
+        /// <inheritdoc/>
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!sender.CheckPermission("customitems.list.registered"))
+            {
+                response = "Permission Denied, required: customitems.list.registered";
+                return false;
+            }
+
+            if (arguments.Count != 0)
+            {
+                response = "list registered";
+                return false;
+            }
+
+            if (!CustomItem.List.Any())
+            {
+                response = "There are no custom items currently on this server.";
+                return false;
+            }
+
+            StringBuilder message = StringBuilderPool.Pool.Get().AppendLine();
+
+            message.Append("[Registered custom items (").Append(CustomItem.List.Count()).AppendLine(")]");
+
+            foreach (CustomItem customItem in CustomItem.List.OrderBy(item => item.Id))
+                message.Append('[').Append(customItem.Id).Append(". ").Append(customItem.Name).Append(" (").Append(customItem.ItemType).Append(')').AppendLine("]");
+
+            response = StringBuilderPool.Pool.ToStringReturn(message);
+            return true;
+        }
+    }
+}
---
+++ Exiled.CustomModules/API/Commands/CustomItem/Spawn.cs
@@ -0,0 +1,109 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Spawn.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.CustomModules.API.Commands.CustomItem
+{
+    using System;
+
+    using CommandSystem;
+    using Exiled.API.Enums;
+    using Exiled.API.Extensions;
+    using Exiled.API.Features;
+    using Exiled.Permissions.Extensions;
+    using UnityEngine;
+
+    using CustomItem = Exiled.CustomModules.API.Features.CustomItems.CustomItem;
+
+    /// <summary>
+    /// The command to spawn a specific item.
+    /// </summary>
+    internal sealed class Spawn : ICommand
+    {
+        private Spawn()
+        {
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Spawn"/> instance.
+        /// </summary>
+        public static Spawn Instance { get; } = new();
+
+        /// <inheritdoc/>
+        public string Command { get; } = "spawn";
+
+        /// <inheritdoc/>
+        public string[] Aliases { get; } = { "sp" };
+
+        /// <inheritdoc/>
+        public string Description { get; } = "Spawn an item at the specified Spawn Location, coordinates, or at the designated player's feet.";
+
+        /// <inheritdoc cref="SanitizeResponse" />
+        public bool SanitizeResponse { get; } = true;
+
+        /// <inheritdoc/>
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!sender.CheckPermission("customitems.spawn"))
+            {
+                response = "Permission Denied, required: customitems.spawn";
+                return false;
+            }
+
+            if (arguments.Count < 2)
+            {
+                response = "spawn [Custom item name] [Location name]\nspawn [Custom item name] [Nickname/PlayerID/UserID]\nspawn [Custom item name] [X] [Y] [Z]";
+                return false;
+            }
+
+            if (!CustomItem.TryGet(arguments.At(0), out CustomItem item))
+            {
+                response = $" {arguments.At(0)} is not a valid custom item.";
+                return false;
+            }
+
+            Vector3 position;
+
+            if (Enum.TryParse(arguments.At(1), out SpawnLocationType location))
+            {
+                position = location.GetPosition();
+            }
+            else
+            {
+                if (Player.Get(arguments.At(1)) is Player player)
+                {
+                    if (player.IsDead)
+                    {
+                        response = $"Cannot spawn custom items under dead players!";
+                        return false;
+                    }
+
+                    position = player.Position;
+                }
+                else if (arguments.Count > 3)
+                {
+                    if (!float.TryParse(arguments.At(1), out float x) || !float.TryParse(arguments.At(2), out float y) || !float.TryParse(arguments.At(3), out float z))
+                    {
+                        response = "Invalid coordinates selected.";
+                        return false;
+                    }
+
+                    position = new Vector3(x, y, z);
+                }
+                else
+                {
+                    response = $"Unable to find spawn location: {arguments.At(1)}.";
+                    return false;
+                }
+            }
+
+            item?.Spawn(position);
+
+            response = $"{item?.Name} ({item?.ItemType}) has been spawned at {position}.";
+            return true;
+        }
+    }
+}
