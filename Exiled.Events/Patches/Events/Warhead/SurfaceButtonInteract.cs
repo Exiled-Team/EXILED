0acf7f8921da47320594ce354282681547c5fe20
SlejmUr <slejmur@protonmail.com>
2024-07-16 18:08:35 +0200

    resolving requests

--- Exiled.Events/Patches/Events/Warhead/SurfaceButtonInteract.cs
+++ Exiled.Events/Patches/Events/Warhead/SurfaceButtonInteract.cs
@@ -33,12 +33,7 @@ namespace Exiled.Events.Patches.Events.Warhead
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            // Getting the start of BypassMode (IL_0028: ldarg.0)
-            int index = newInstructions.FindLastIndex(instruction => instruction.ToString().Contains("BypassMode")) - 2;
-
-            // Getting the ChckDis jump to the new label. (IL_0025: brtrue.s)
-            int chckDis_call_after_indx = newInstructions.FindLastIndex(instruction => instruction.ToString().Contains("ChckDis")) + 1;
-            CodeInstruction chckDis_instruction = newInstructions[chckDis_call_after_indx];
+            int index = newInstructions.FindIndex(instruction => instruction.LoadsField(Field(typeof(AlphaWarheadOutsitePanel), nameof(AlphaWarheadOutsitePanel.keycardEntered)))) + 3;
 
             Label thisFunctionLabel = generator.DefineLabel();
 
@@ -50,10 +45,10 @@ namespace Exiled.Events.Patches.Events.Warhead
                 index,
                 new[]
                 {
-                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
 
                     // PlayerInteract._hub
-                    new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayerInteract), "_hub")),
+                    new CodeInstruction(OpCodes.Ldfld, Field(typeof(PlayerInteract), nameof(PlayerInteract._hub))),
 
                     // Player.Get(PlayerInteract._hub)
                     new CodeInstruction(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
@@ -70,14 +65,10 @@ namespace Exiled.Events.Patches.Events.Warhead
 
                     // if (!ev.IsAllowed)
                     // return;
-                    new CodeInstruction(chckDis_instruction),
-                    new CodeInstruction(OpCodes.Ret),
+                    new(OpCodes.Brfalse_S, thisFunctionLabel),
                 });
 
-            // Getting the new index of our function.
-            int hub_indx = newInstructions.FindLastIndex(instruction => instruction.ToString().Contains("Player::Get")) - 2;
-            newInstructions[hub_indx].WithLabels(thisFunctionLabel);
-            chckDis_instruction.operand = thisFunctionLabel;
+            newInstructions[newInstructions.Count - 1].labels.Add(thisFunctionLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
