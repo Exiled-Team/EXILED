a9d987f2f9e9e334e1df652f2026d8ef3d2812c0
theDevJade <jadeisstolen@gmail.com>
2024-07-16 15:20:35 -0700

    Added FeatureGroups (dynamic register) (#2718)
    * Added FeatureGroups (dynamic register)
    
    * Updated System
    
    * Fixed Directory.build.props
    
    * Update EventGroup.cs
    
    * Update CoroutineManager.cs
    
    * Update EventScanner.cs
    
    * Fix build errors
    
    * Update FeatureGroup.cs
    
    * Update CoroutineManager.cs
    
    * Update EventGroup.cs
    
    * Update EventScanner.cs
    
    * Update FeatureGroup.cs
    
    ---------
    
    Co-authored-by: xNexusACS <83370388+xNexusACS@users.noreply.github.com>
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>

---
+++ Exiled.Events/Extra/CoroutineManager.cs
@@ -0,0 +1,79 @@
+// -----------------------------------------------------------------------
+// <copyright file="CoroutineManager.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Extra
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    using MEC;
+
+    /// <summary>
+    /// A simple coroutine manager wrapping MEC.
+    /// </summary>
+    public static class CoroutineManager
+    {
+        private static readonly Dictionary<string, CoroutineHandle> RunningCoroutines = new();
+
+        /// <summary>
+        /// Starts a single coroutine and keeps track of it.
+        /// </summary>
+        /// <param name="coroutine">The coroutine to start.</param>
+        /// <param name="key">A unique key to identify the coroutine. Defaults to the name of the caller method.</param>
+        public static void StartCoroutine(Func<IEnumerator<float>> coroutine, [CallerMemberName] string key = "")
+        {
+            if (RunningCoroutines.ContainsKey(key))
+                StopCoroutine(key);
+            CoroutineHandle handle = Timing.RunCoroutine(coroutine());
+            RunningCoroutines[key] = handle;
+        }
+
+        /// <summary>
+        /// Starts multiple coroutines and keeps track of them.
+        /// </summary>
+        /// <param name="coroutines">A list of tuples containing coroutines and their unique keys.</param>
+        public static void StartCoroutines(params (Func<IEnumerator<float>> coroutine, string key)[] coroutines)
+        {
+            foreach ((Func<IEnumerator<float>> coroutine, string key) in coroutines)
+                StartCoroutine(coroutine, key);
+        }
+
+        /// <summary>
+        /// Stops a running coroutine.
+        /// </summary>
+        /// <param name="key">The unique key of the coroutine to stop.</param>
+        public static void StopCoroutine(string key)
+        {
+            if (!RunningCoroutines.TryGetValue(key, out CoroutineHandle handle))
+                return;
+            Timing.KillCoroutines(handle);
+            RunningCoroutines.Remove(key);
+        }
+
+        /// <summary>
+        /// Stops all running coroutines.
+        /// </summary>
+        public static void StopAllCoroutines()
+        {
+            foreach (CoroutineHandle handle in RunningCoroutines.Values)
+                Timing.KillCoroutines(handle);
+
+            RunningCoroutines.Clear();
+        }
+
+        /// <summary>
+        /// Checks if a coroutine is running.
+        /// </summary>
+        /// <param name="key">The unique key of the coroutine to check.</param>
+        /// <returns>True if the coroutine is running, otherwise false.</returns>
+        public static bool IsCoroutineRunning(string key)
+        {
+            return RunningCoroutines.ContainsKey(key);
+        }
+    }
+}
---
+++ Exiled.Events/Extra/Events/EventGroup.cs
@@ -0,0 +1,96 @@
+// -----------------------------------------------------------------------
+// <copyright file="EventGroup.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Extra.Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Exiled.API.Features;
+    using Features;
+
+    /// <summary>
+    /// A versatile tool for dynamically and bulk registering/unregistering events.
+    /// </summary>
+    public class EventGroup
+    {
+        private readonly List<Tuple<Delegate, object, MethodInfo>> dynamicHandlers = new();
+
+        /// <summary>
+        /// Gets a value indicating whether events have been registered.
+        /// </summary>
+        public bool IsHandlerAdded { get; private set; }
+
+        /// <summary>
+        /// Registers all methods in the specified instances marked with <see cref="ListenerAttribute"/>.
+        /// </summary>
+        /// <param name="classes">The instances of classes containing listener methods to be registered.</param>
+        public void AddEvents(params object[] classes)
+        {
+            foreach (object @class in classes)
+                AddEventHandlers(@class);
+        }
+
+        /// <summary>
+        /// Unregisters all currently active event handlers in this event group.
+        /// </summary>
+        public void RemoveEvents()
+        {
+            RemoveEventHandlers();
+        }
+
+        /// <summary>
+        /// Registers all methods in the specified instance that have the [Listener] attribute.
+        /// </summary>
+        /// <param name="instance">The instance containing listener methods to be registered.</param>
+        public void AddEventHandlers(object instance)
+        {
+            IEnumerable<MethodInfo> methods = instance.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)
+                .Where(m => m.GetCustomAttributes(typeof(ListenerAttribute), false).Length > 0);
+
+            List<MethodInfo> methodInfos = methods.ToList();
+            Log.Warn($"Beginning event handlers, total of {methodInfos.Count} found");
+
+            foreach (MethodInfo method in methodInfos)
+            {
+                Type eventType = method.GetParameters().First().ParameterType;
+                object eventInstance = EventScanner.Get()[eventType];
+                Type delegateType = typeof(CustomEventHandler<>).MakeGenericType(eventType);
+                Delegate handler = Delegate.CreateDelegate(delegateType, instance, method);
+                MethodInfo[] eventMethods = eventInstance.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public);
+                eventMethods.First(e => e.Name == "Subscribe").Invoke(eventInstance, new object[] { handler });
+
+                dynamicHandlers.Add(Tuple.Create(handler, instance, method));
+            }
+
+            IsHandlerAdded = true;
+        }
+
+        /// <summary>
+        /// Unregisters all event handlers that were previously registered.
+        /// </summary>
+        private void RemoveEventHandlers()
+        {
+            if (!IsHandlerAdded)
+                return;
+
+            foreach (Tuple<Delegate, object, MethodInfo> handler in dynamicHandlers)
+            {
+                Type eventType = handler.Item3.GetParameters().First().ParameterType;
+                object eventInstance = EventScanner.Get()[eventType];
+                MethodInfo[] eventMethods = eventInstance.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public);
+                eventMethods.First(e => e.Name == "Unsubscribe").Invoke(eventInstance, new object[] { handler.Item1 });
+            }
+
+            dynamicHandlers.Clear();
+            IsHandlerAdded = false;
+        }
+    }
+}
