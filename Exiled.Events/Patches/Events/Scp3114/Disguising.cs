46c656881fcb8adc21d4c9c46162fed5f1597644
xNexusACS <83370388+xNexusACS@users.noreply.github.com>
2024-07-20 06:44:04 +0200

    Fix Strangling & Rework Disguising Patch

--- Exiled.Events/Patches/Events/Scp3114/Disguising.cs
+++ Exiled.Events/Patches/Events/Scp3114/Disguising.cs
@@ -7,16 +7,19 @@
 
 namespace Exiled.Events.Patches.Events.Scp3114
 {
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
     using Exiled.API.Features;
+    using Exiled.API.Features.Core.Generic.Pools;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Scp3114;
     using Exiled.Events.Handlers;
-
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-
     using HarmonyLib;
     using PlayerRoles.PlayableScps.Scp3114;
     using PlayerRoles.Ragdolls;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="Scp3114Disguise.ServerComplete()" />.
@@ -25,26 +28,79 @@ namespace Exiled.Events.Patches.Events.Scp3114
     [EventPatch(typeof(Scp3114), nameof(Scp3114.Disguising))]
     [EventPatch(typeof(Scp3114), nameof(Scp3114.Disguised))]
     [HarmonyPatch(typeof(Scp3114Disguise), nameof(Scp3114Disguise.ServerComplete))]
-    internal class Disguising
+    internal static class Disguising
     {
-        private static bool Prefix(Scp3114Disguise __instance)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (__instance.CurRagdoll != null && __instance.CurRagdoll is DynamicRagdoll ragdoll)
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            Label retLabel = generator.DefineLabel();
+            LocalBuilder player = generator.DeclareLocal(typeof(API.Features.Player));
+            LocalBuilder ragdoll = generator.DeclareLocal(typeof(Ragdoll));
+
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ldarg_0);
+
+            // Remove Scp3114Disguise::CastRole::IsDisguised = true;
+            newInstructions.RemoveRange(index, 4);
+
+            index = newInstructions.FindIndex(instruction => instruction.Calls(Method(typeof(Scp3114RagdollToBonesConverter), nameof(Scp3114RagdollToBonesConverter.ServerConvertNew)))) - 3;
+
+            newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                DisguisingEventArgs disguising = new(API.Features.Player.Get(__instance.Owner), Ragdoll.Get(ragdoll));
-                Scp3114.OnDisguising(disguising);
+                // Scp3114Disguise::Owner
+                new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Scp3114Disguise), nameof(Scp3114Disguise.Owner))),
 
-                if (!disguising.IsAllowed)
-                    return false;
+                // Player::Get(Scp3114Disguise::Owner)
+                new(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Stloc_S, player.LocalIndex),
 
-                __instance.CastRole.Disguised = true;
-                Scp3114RagdollToBonesConverter.ServerConvertNew(__instance.CastRole, ragdoll);
+                // curRagdoll
+                new(OpCodes.Ldloc_0),
 
-                DisguisedEventArgs disguised = new(API.Features.Player.Get(__instance.Owner), Ragdoll.Get(ragdoll));
-                Scp3114.OnDisguised(disguised);
-            }
+                // Ragdoll::Get(curRagdoll)
+                new(OpCodes.Call, Method(typeof(Ragdoll), nameof(Ragdoll.Get), new[] { typeof(DynamicRagdoll) })),
+                new(OpCodes.Stloc_S, ragdoll.LocalIndex),
 
-            return false;
+                // true
+                new(OpCodes.Ldc_I4_1),
+
+                // DisguisingEventArgs args = new(player, ragdoll, true)
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(DisguisingEventArgs))[0]),
+                new(OpCodes.Dup),
+
+                // Scp3114::OnDisguising(args)
+                new(OpCodes.Call, Method(typeof(Scp3114), nameof(Scp3114.OnDisguising))),
+
+                // if (!args.IsAllowed) return;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(DisguisingEventArgs), nameof(DisguisingEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retLabel),
+
+                // Scp3114Disguise::CastRole::IsDisguised = true;
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Scp3114Disguise), nameof(Scp3114Disguise.Owner))),
+                new(OpCodes.Ldc_I4_1),
+                new(OpCodes.Callvirt, PropertySetter(typeof(Scp3114Role), nameof(Scp3114Role.Disguised))),
+            });
+
+            newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+
+            newInstructions.InsertRange(newInstructions.Count - 1, new CodeInstruction[]
+            {
+                new(OpCodes.Ldloc_S, player.LocalIndex),
+                new(OpCodes.Ldloc_S, ragdoll.LocalIndex),
+
+                // DisguisedEventArgs args = new(player, ragdoll)
+                new(OpCodes.Newobj, GetDeclaredConstructors(typeof(DisguisedEventArgs))[0]),
+
+                // Scp3114::OnDisguised(args)
+                new(OpCodes.Call, Method(typeof(Scp3114), nameof(Scp3114.OnDisguised))),
+            });
+
+            foreach (CodeInstruction codeInstruction in newInstructions)
+                yield return codeInstruction;
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
     }
 }
