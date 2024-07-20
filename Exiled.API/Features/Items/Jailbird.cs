5556b15e3f1a1cf53eae3b3d74ed41ad952ead49
Artem Košilinskis <46756889+ITeMbI4@users.noreply.github.com>
2024-07-20 18:42:43 +0200

    [Resonance] Fix FlashDuration and add ConcussionDuration (by Yamato) (#2740)
    * Fix FlashDuration and add ConcussionDuration
    
    * Update Exiled.API/Features/Items/Jailbird.cs
    
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>
    
    * Update Exiled.API/Features/Items/Jailbird.cs
    
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nao <60253860+NaoUnderscore@users.noreply.github.com>

--- Exiled.API/Features/Items/Jailbird.cs
+++ Exiled.API/Features/Items/Jailbird.cs
@@ -76,6 +76,16 @@ namespace Exiled.API.Features.Items
         [EProperty(category: nameof(Jailbird))]
         public float FlashDuration
         {
+            get => Base._hitreg._flashedDuration;
+            set => Base._hitreg._flashedDuration = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the amount of time in seconds that the <see cref="CustomPlayerEffects.Concussed"/> effect will be applied on being hit.
+        /// </summary>
+        [EProperty(category: nameof(Jailbird))]
+        public float ConcussionDuration
+        {
             get => Base._hitreg._concussionDuration;
             set => Base._hitreg._concussionDuration = value;
         }
@@ -200,6 +210,7 @@ namespace Exiled.API.Features.Items
                 MeleeDamage = jailbirdPickup.MeleeDamage;
                 ChargeDamage = jailbirdPickup.ChargeDamage;
                 FlashDuration = jailbirdPickup.FlashDuration;
+                ConcussionDuration = jailbirdPickup.ConcussionDuration;
                 Radius = jailbirdPickup.Radius;
             }
         }
