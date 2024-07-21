06b3533f85fb38b9fc30b03692aaa22239bf64a5
NaoUnderscore <info@naounderscore.dev>
2024-07-21 21:43:47 +0200

    Fixed `ChangingItemEventArgs.set_Item` NRE

--- Exiled.Events/EventArgs/Player/ChangingItemEventArgs.cs
+++ Exiled.Events/EventArgs/Player/ChangingItemEventArgs.cs
@@ -46,7 +46,7 @@ namespace Exiled.Events.EventArgs.Player
             get => newItem;
             set
             {
-                if (!Player.Inventory.UserInventory.Items.TryGetValue(value.Serial, out _))
+                if (newItem is not null && !Player.Inventory.UserInventory.Items.TryGetValue(value.Serial, out _))
                     throw new InvalidOperationException("ev.NewItem cannot be set to an item they do not have.");
 
                 newItem = value;
