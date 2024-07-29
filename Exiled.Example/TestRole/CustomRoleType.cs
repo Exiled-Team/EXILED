5b22e39d4009afcbc8f481980d760b658e979427
Monald <63928518+Monaldcry7788@users.noreply.github.com>
2024-07-30 01:25:59 +0200

    Added test role (testing purposes)

---
+++ Exiled.Example/TestRole/CustomRoleType.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CustomRoleType.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Example.TestRole
+{
+    using Exiled.CustomModules.API.Enums;
+
+    /// <summary>
+    /// The custom role type.
+    /// </summary>
+    public class CustomRoleType : UUCustomRoleType
+    {
+        /// <summary>
+        /// Initializes a new custom role id.
+        /// </summary>
+        public static readonly CustomRoleType Scp999 = new();
+    }
+}
---
+++ Exiled.Example/TestRole/Scp999Behaviour.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Scp999Behaviour.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Example.TestRole
+{
+    using Exiled.CustomModules.API.Features.CustomRoles;
+
+    /// <inheritdoc />
+    public class Scp999Behaviour : RoleBehaviour
+    {
+        /// <inheritdoc />
+        protected override void PostInitialize()
+        {
+            base.PostInitialize();
+        }
+    }
+}
