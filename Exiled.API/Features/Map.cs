18c202d88a7407cd920c1a6e58a74089fa5cc358
NotZer0Two <lyonfan2010@gmail.com>
2024-08-11 08:17:50 +0200

    Fix Build error

--- Exiled.API/Features/Map.cs
+++ Exiled.API/Features/Map.cs
@@ -356,7 +356,7 @@ namespace Exiled.API.Features
         /// <summary>
         /// Spawns a Mice inside the <see cref="RoomType.EzShelter"/>.
         /// </summary>
-        /// <param name="mice">Amount of mice you want to spawn</param>
+        /// <param name="mice">Amount of mice you want to spawn.</param>
         public static void SpawnMice(byte mice = 1)
         {
             SqueakSpawner squeakSpawner = UnityEngine.Object.FindObjectOfType<SqueakSpawner>();
