040689de1774bf90c9d21e887de209f27bd11ced
Monald <63928518+Monaldcry7788@users.noreply.github.com>
2024-08-31 16:27:54 +0200

    fix file header.

--- Exiled.Events/Patches/Events/Player/ChangingRoleAndChangedRole.cs
+++ Exiled.Events/Patches/Events/Player/ChangingRoleAndChangedRole.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="ChangingRoleAndSpawned.cs" company="Exiled Team">
+// <copyright file="ChangingRoleAndChangedRole.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
