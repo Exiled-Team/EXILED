104c1130c09c740e4534e8bfcbf387a8d8ec6253
Valentin Arthur Thomas <warquis958@gmail.com>
2024-09-11 04:14:19 +0200

    Simplifying Singleton<T> implementation
    The code has been simplified to no longer use a dictionary to manage multiple instances of `Singleton<T>`. Now a single instance of `T` is used via a static `Instance` property.
    
    The `Create` method has been changed to set the instance if it is not already set, and a new `Destroy` method has been added to reset the instance to `null`.
    
    The `TryGet` and `Destroy` methods have been simplified to work with the new single instance implementation.
    
    The calls to `Singleton<VirtualPlugin>.Create(this)` and `Singleton<VirtualPlugin>.Destroy(this)` in `VirtualPlugin.cs` have been commented out, with annotations suggesting that use of the class itself as a singleton might require a different approach to avoid overwriting other instances.

--- Exiled.API/Features/Core/Generic/Singleton.cs
+++ Exiled.API/Features/Core/Generic/Singleton.cs
@@ -19,69 +19,53 @@ namespace Exiled.API.Features.Core.Generic
     public sealed class Singleton<T> : TypeCastObject<T>
         where T : class
     {
-        // @Nao, this is not realy a singleton if you have mutliple value.
-        // NB: each Singleton<T> ave a new instance of dictionary.
-        // exemple Singleton<int>.Instance is not the same Singleton<bool>.Instance
-        private static readonly Dictionary<T, Singleton<T>> Instances = new();
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Singleton{T}"/> class.
-        /// </summary>
-        /// <param name="value">The branch to instantiate.</param>
-        public Singleton(T value)
-        {
-            Destroy(value);
-            Value = value;
-            Instances.Add(value, this);
-        }
-
-        /// <summary>
-        /// Finalizes an instance of the <see cref="Singleton{T}"/> class.
-        /// </summary>
-        // @Nao Probably never call, it self reference. Maybe an future issue doing memory leek
-        // it will get call only after the instance get remove form Instances
-        ~Singleton() => Instances.Remove(Value);
-
         /// <summary>
         /// Gets the relative value.
         /// </summary>
-        public static T Instance => Instances.FirstOrDefault(@object => @object.Key.GetType() == typeof(T)).Value;
-
-        /// <summary>
-        /// Gets the singleton value.
-        /// </summary>
-        internal T Value { get; private set; }
-
-        /// <summary>
-        /// Converts the given <see cref="Singleton{T}"/> instance into <typeparamref name="T"/>.
-        /// </summary>
-        /// <param name="instance">The <see cref="Singleton{T}"/> instance to convert.</param>
-        public static implicit operator T(Singleton<T> instance) => instance?.Value;
+        public static T Instance { get; private set; }
 
         /// <summary>
         /// Tries to get the relative value.
         /// </summary>
-        /// <typeparam name="TObject">The type of the object.</typeparam>
         /// <param name="instance">The object instance.</param>
         /// <returns><see langword="true"/> if the object instance is not null and can be casted as the specified type; otherwise, <see langword="false"/>.</returns>
-        // @nao It will return the Instance.
-        // But with this curent class definition you can register derived class of T.
-        // They can be registred but not retrived. Also why make this method generic if this not to get
-        // other value than the instance.
-        public static bool TryGet<TObject>(out TObject instance)
-            where TObject : class => (instance = Instance as TObject) is not null;
-
-        /// <inheritdoc cref="Singleton{T}"/>
-        public static void Create(T @object) => new Singleton<T>(@object);
+        public static bool TryGet(out T instance)
+            => (instance = Instance) is not null;
 
         /// <summary>
-        /// Destroys the given <typeparamref name="T"/> instance.
+        /// Define the value of the <see cref="Instance"/> if not already defined.
+        /// If you want to replace first call <see cref="Destroy()"/>.
+        /// </summary>
+        /// <param name="object">The object use to create the singleton.</param>
+        public static void Create(T @object)
+        {
+            if (Instance is not null)
+                return;
+            Instance = @object;
+        }
+
+        /// <summary>
+        /// Set to null <see cref="Instance"/> if <paramref name="object"/> is the same.
         /// </summary>
         /// <param name="object">The object to destroy.</param>
         /// <returns><see langword="true"/> if the instance was destroyed; otherwise, <see langword="false"/>.</returns>
         public static bool Destroy(T @object)
         {
-            return Instances.Remove(@object);
+            if (Instance == @object)
+            {
+                Instance = null;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Set to null <see cref="Instance"/>.
+        /// </summary>
+        public static void Destroy()
+        {
+            Instance = null;
         }
     }
 }
